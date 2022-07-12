07bff2aba40adbbe7fdfa503f449fe432e18ee73
OswaldUnity <oswald@thedac.info>
2022-07-11 19:35:54 -0500

    Fixed missing new change part way

--- pages/api/getAccessories.ts
+++ pages/api/getAccessories.ts
@@ -1,11 +1,10 @@
 ﻿import {NextApiRequest, NextApiResponse} from "next";
 import {FirebaseUtil} from "../../utils/firebase.util";
-import {AccessoryPartTypeEnum} from "../../enums/common.enum";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if(req.method === 'GET') {
     const { campaign, type } = req.query;
-    const data = await FirebaseUtil.Instance().GetAccessories(campaign as string ?? 'base', type as AccessoryPartTypeEnum);
+    const data = await FirebaseUtil.Instance().GetAccessories(campaign as string ?? 'base', type as string);
 
     res
       .status(200)
--- utils/model.util.ts
+++ utils/model.util.ts
@@ -1,9 +1,8 @@
 ﻿import {GLTF} from "three/examples/jsm/loaders/GLTFLoader";
 import {ImporterUtil} from "./importer.util";
 import * as SkeletonUtils from "three/examples/jsm/utils/SkeletonUtils";
-import {Mesh, Object3D, SkinnedMesh} from "three";
-import {AccessoryPartTypeEnum, BodyPartTypeEnum} from "../enums/common.enum";
-import {AccessoryInfoInterface} from "../interfaces/accessory-parts.interface";
+import {Group, Mesh, Object3D, SkinnedMesh} from "three";
+import {AccessoryInfoInterface, BasicData, PartInfoInterface} from "../interfaces/common.interface";
 
 export async function ReplaceModelPart(baseModel: GLTF, partUrl: string, partIndex: number) {
   const partModel = await ImporterUtil.LoadGltfModel(partUrl);
@@ -18,29 +17,61 @@ export async function ReplaceModelPart(baseModel: GLTF, partUrl: string, partInd
   baseModel.scene.children[0].children[partIndex] = chest;
 }
 
-export async function ReplaceModelPartOnly(baseModel: GLTF, replaceModel: GLTF, partIndex: BodyPartTypeEnum) {
+export async function ReplaceModelPartOnly(baseModel: Object3D, replaceModel: GLTF, partInfo: PartInfoInterface, selectedPart?: BasicData) {
+  if(selectedPart == undefined) {
+    console.error('No selected Part', 'There is no selected part to replace on base model.');
+    return;
+  }
+  
+  let chestMesh: Object3D | undefined = undefined;
+  replaceModel.scene.traverse(m => {
+    if(m.name === selectedPart.value) {
+      chestMesh = m;
+    }
+  });
+  
+  if(chestMesh == undefined) {
+    console.error('Piece not found:', `${selectedPart.value} not found on replace model, please verify the glb file.`);
+    return;
+  }
+  
   // @ts-ignore
-  const chest = SkeletonUtils.clone(replaceModel.scene.children[0].children[1]) as Mesh;
-  const oldModel = baseModel.scene.children[0].children[partIndex] as SkinnedMesh;
+  const chest: Object3D = SkeletonUtils.clone(chestMesh);
+
+  const baseSkeleton = (baseModel.children[partInfo.partIndex] as SkinnedMesh).skeleton;
+  if((chest as Group).isGroup) {
+    chest.traverse(object => {
+      if((object as SkinnedMesh).isSkinnedMesh) {
+        (object as SkinnedMesh).skeleton = baseSkeleton.clone();
+      }
+    });
+  }
+  
+  if((chest as SkinnedMesh).isSkinnedMesh) {
+    (chest as SkinnedMesh).skeleton = baseSkeleton.clone();
+  }
   
   console.log('base', baseModel);
-  console.log('replace', replaceModel);
+  console.log('replace', chest);
+  console.log('partInfo', partInfo);
   
-  oldModel.geometry = chest.geometry;
-  oldModel.updateMatrix();
+  baseModel.children.splice(partInfo.partIndex, 1);
+  baseModel.add(chest);
 }
 
 
 export function ReplaceModelAccessory(bone: AccessoryInfoInterface, accessory: GLTF) {
   const accessoryMesh = accessory.scene.children[0].clone() as Mesh;
-  accessoryMesh.scale.set(0.1, 0.1, 0.1);
+  accessoryMesh.scale.set(0.8, 0.8, 0.8);
   
   if(bone.hasIt) {
     bone.bone.children.splice(bone.accessoryIndex!, 1);
+    // bone.bone.children[bone.accessoryIndex!] = accessoryMesh;
   }
   
   bone.accessoryIndex = bone.bone.children.length;
   bone.hasIt = true;
   bone.bone.add(accessoryMesh);
+  
   console.log(bone);
 }
