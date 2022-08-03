b43a31fbc74c9cbfdabae3186883f049c2eb9894
NaN <nan@thedac.info>
2022-08-02 19:54:35 -0500

    TEST-SCENE UTIL Updated

--- utils/test-scene.util.ts
+++ utils/test-scene.util.ts
@@ -21,13 +21,13 @@ export function GetTestAxis(size: number): AxesHelper {
 
 export function GetTestLights(): PointLight[] {
   let lights = [];
-  lights[ 0 ] = new PointLight( 0xffffff, 3, 0 );
-  lights[ 1 ] = new PointLight( 0xffffff, 3, 0 );
-  lights[ 2 ] = new PointLight( 0xffffff, 3, 0 );
+  lights[ 0 ] = new PointLight( 0xffffff, 1, 0 );
+  // lights[ 1 ] = new PointLight( 0xffffff, 3, 0 );
+  // lights[ 2 ] = new PointLight( 0xffffff, 3, 0 );
 
-  lights[ 0 ].position.set( 0, 200, 0 );
-  lights[ 1 ].position.set( 100, 200, 100 );
-  lights[ 2 ].position.set( - 100, - 200, - 100 );
+  lights[ 0 ].position.set( 10, 10, 10 );
+  // lights[ 1 ].position.set( 100, 200, 100 );
+  // lights[ 2 ].position.set( - 100, - 200, - 100 );
   
   return lights;
 }
