fe43a2b31d261fec687e99f3494fb5097c96f2f2
NaN <nan@thedac.info>
2022-08-03 15:28:27 -0500

    LOG COMPONENT Updated
    - Interfaces for props added
    - Screen resolution as title

--- components/log.component.tsx
+++ components/log.component.tsx
@@ -1,16 +1,22 @@
 import { WebGLInfo } from "three";
 
-function LogComponent(props: any) {
+interface ExampleProps {
+  logs: WebGLInfo | undefined;
+  resX: number | 'undefined'; 
+  resY: number | 'undefined'; 
+}
+
+function LogComponent(props: ExampleProps) {
   return (
     <div className="fixed right-0 bottom-0 bg-slate-600 bg-opacity-50 p-4 pointer-events-none">
-      <p className="text-white text-xs font-black">LOGS</p>
-          { props.logs ?
-            <>
-              <p className="text-white text-xs">Scene polycount: <span className="text-yellow-300">{props.logs.render.triangles}</span></p>
-              <p className="text-white text-xs">Active Drawcalls: <span className="text-yellow-300">{props.logs.render.calls}</span></p>
-              <p className="text-white text-xs">Textures in Memory: <span className="text-yellow-300">{props.logs.memory.textures}</span></p>
-              <p className="text-white text-xs">Geometries in Memory: <span className="text-yellow-300">{props.logs.memory.geometries}</span></p>
-            </>
+      <p className="text-white text-xs font-black">{'X:' + props.resX + ' Y:' + props.resY}</p>
+      { props.logs ?
+      <>
+        <p className="text-white text-xs">Scene polycount: <span className="text-yellow-300">{props.logs.render.triangles}</span></p>
+        <p className="text-white text-xs">Active Drawcalls: <span className="text-yellow-300">{props.logs.render.calls}</span></p>
+        <p className="text-white text-xs">Textures in Memory: <span className="text-yellow-300">{props.logs.memory.textures}</span></p>
+        <p className="text-white text-xs">Geometries in Memory: <span className="text-yellow-300">{props.logs.memory.geometries}</span></p>
+      </>
             : <p className="text-white text-xs">No logs</p>}
     </div>
   )
