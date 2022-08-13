92df8135b8859370acdab96dab9cc8d671e60b06
NaN <nan@thedac.info>
2022-08-12 19:30:15 -0500

    CATEGORY-CHILDREN COMPONENT Updated
    - HTML and CSS updated
    - Icons for buttons added

--- components/categoryChildren.component.tsx
+++ components/categoryChildren.component.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { BodyPartLocationApi } from "../interfaces/api.interface";
 
 interface ExampleProps {
@@ -8,11 +9,11 @@ interface ExampleProps {
 function optionList(props: ExampleProps) {
   return props.list.map((x: BodyPartLocationApi) => {
     return (
-      <div className="overflow-hidden w-20 h-20 flex justify-center flex-col items-center bg-gray-300 mx-2" key={x.id} onClick={() => {props.handleClick(x.id, x.path, x.name)}}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-          <path className="fill-gray-600" d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Z"/>
-        </svg>
-        <p className="text-[0.75rem]">{x.name}</p>
+      <div className="w-14 h-14 bg-gray-200 border-2 border-gray-100 rounded-[6px] drop-shadow-md mx-2 inline-block" key={x.id} onClick={() => {props.handleClick(x.id, x.path, x.name)}}>
+        <div className="flex flex-col items-center justify-center h-full">
+          <Image src={'/resources/icos/features/' + x.type + '.svg'} width={25} height={25} alt={x.type}/>
+          <p className="text-[0.75rem]">{x.name}</p>
+        </div>
       </div>
     )
   });
@@ -20,8 +21,10 @@ function optionList(props: ExampleProps) {
 
 function CategoryChildrenComponent(props: ExampleProps) {
   return (
-    <div className="fixed bottom-0 right-0 w-screen h-[20%] flex flex-row justify-center items-center" >
-      {optionList(props)}
+    <div className="fixed bottom-0 right-0 w-screen h-[20%] flex items-center" >
+      <div className="h-16 w-screen overflow-auto whitespace-nowrap px-4">
+        {optionList(props)}
+      </div>
     </div>
   )
 }
