0b7cd663d0b01748e7faa4c3e46aa9f753a4c82e
NaN <nan@thedac.info>
2022-08-12 19:43:30 -0500

    LOADING COMPONENT Updated
    - change BG color

--- components/ag-loading.component.tsx
+++ components/ag-loading.component.tsx
@@ -10,7 +10,7 @@ export default class AGLoading extends Component<AGLoadingProps> {
       <>
         {
           this.props.loading ?
-            <div className="fixed left-0 top-0 bg-opacity-50 backdrop-blur-sm w-full h-full bg-gray-400">
+            <div className="fixed left-0 top-0 w-full h-full bg-white">
               <div className="flex justify-center items-center h-full align-middle space-x-2">
                 <div style={{borderTopColor: "transparent"}}
                      className="w-20 h-20 border-4 border-blue-400 border-dotted rounded-full animate-spin">
