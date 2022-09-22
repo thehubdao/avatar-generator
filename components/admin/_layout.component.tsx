38d20b569d7681e54f33f5cc04c9df64557420ea
OswaldUnity <oswald@thedac.info>
2022-09-22 17:42:19 -0500

    Added layout component for admin

---
+++ components/admin/_layout.component.tsx
@@ -0,0 +1,21 @@
+﻿import {Component} from "react";
+
+interface LayoutProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+interface LayoutState {
+}
+
+export default class Layout extends Component<LayoutProps, LayoutState> {
+  render() {
+    return (
+      <div>
+        <div>
+          
+        </div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
