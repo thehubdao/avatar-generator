0736abb64f050114c3681f07bdcd274747270b92
OswaldUnity <oswald@thedac.info>
2022-11-03 15:23:52 -0500

    Added new api calls for features and accessories, iframe event on change now changes accessories as well

--- pages/api/getFeatures.ts
+++ pages/api/getFeatures.ts
@@ -8,9 +8,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { campaign } = req.query;
 
     const useCampaign = campaign != undefined ? campaign as string : GlobalValues.BaseCampaign;
-    const [data] = await GetParameters<BasicData[]>(useCampaign + GlobalValues.Acc);
+    const [data] = await GetParameters<BasicData[]>(useCampaign);
     const result = data.map(d => d.id);
-
+    
     res
       .status(200)
       .json(result);
