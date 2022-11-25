1adef0dd6440f47f6935fbd3c237b1232ce2883c
NaN <nan@thedac.info>
2022-11-25 16:42:12 -0500

    OPTION SELECTOR Updated

--- components/selectors/optionSelector.component.tsx
+++ components/selectors/optionSelector.component.tsx
@@ -2,21 +2,27 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from 'next/image';
 import { BodyPartLocationApi } from "../../interfaces/api.interface";
 import 'swiper/css';
+import { BasicData } from '../../interfaces/common.interface';
+import { useEffect, useState } from 'react';
 
 interface props {
   list: BodyPartLocationApi[];
+  activeOption?: BasicData;
   handleClick: Function;
 }
 
-function optionList(props: props) {
+function OptionList({list, activeOption, handleClick}: props) {
   function selectPart(e:React.MouseEvent, opt: BodyPartLocationApi) {
     e.preventDefault();
-    props.handleClick(opt.id, opt.path, opt.name);
+    handleClick(opt.id, opt.path, opt.name);
   }
-
-  return props.list.map((opt) => {
+  useEffect(() => {
+    console.log("active option: ", activeOption);
+  }, [activeOption]);
+  // console.log("active option: ", activeOption);
+  return list.map((opt) => {
     return (
-      <SwiperSlide className='flex justify-center items-center group hover:-translate-y-2 transition-transform' key={opt.id}>
+      <SwiperSlide className={'flex justify-center items-center transition-transform' + (activeOption?.val === opt.name?' -translate-y-2':'')} key={opt.id}>
         <p className='nm-flat-slate-100 hidden absolute'></p>
         <div className="rounded-md w-[75px] h-[75px] flex items-center justify-center relative overflow-hidden" onClick={(event:React.MouseEvent) => selectPart(event,opt)}>
           <div className='w-[75px] h-[75px] rounded-md overflow-hidden flex justify-center items-center'>
@@ -24,7 +30,7 @@ function optionList(props: props) {
             <Image src={'/resources/images/image.png'} width={75} height={75} alt={opt.name}/>
           </div>
           <p className='text-[10px] px-2 absolute bottom-0 right-0 bg-slate-100 max-w-full rounded-tl-md'>{opt.name}</p>
-          <div className='w-2 h-2 absolute top-1 right-1 rounded-full bg-green-400 border border-green-600 hidden group-hover:block'></div>
+          <div className={'w-2 h-2 absolute top-1 right-1 rounded-full bg-green-400 border border-green-600 opacity-0' + (activeOption?.val === opt.name?'  opacity-100':'')}></div>
         </div>
       </SwiperSlide>
     )
@@ -42,7 +48,7 @@ export default function OptionSelectorComponent( props: props) {
         onSwiper={(swiper) => console.log(swiper)}
         className='!py-2'
       >
-        { optionList(props) }
+        { OptionList(props) }
       </Swiper>
     </div>
   )
