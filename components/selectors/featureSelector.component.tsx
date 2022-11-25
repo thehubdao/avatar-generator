f3f31ee7a5efa32c59a63840c1eb5ee3b2e31371
NaN <nan@thedac.info>
2022-11-25 16:42:26 -0500

    FEATURE-SELECTOR COMPONENT Updated

--- components/selectors/featureSelector.component.tsx
+++ components/selectors/featureSelector.component.tsx
@@ -6,7 +6,7 @@ import 'swiper/css';
 
 interface props {
   list: BodyPartLocationApi[] | BasicData[];
-  activedPart: string;
+  activeFeature: string;
   handleClick: Function;
 }
 
@@ -20,10 +20,10 @@ function optionList(props: props) {
   return props.list.map((opt) => {
     return (
       <SwiperSlide className='flex flex-col items-center'  key={opt.id}>
-        <div className={'rounded-md transition duration-200 ease-in-out w-[40px] h-[40px] flex items-center justify-center' + (props.activedPart==opt.id?' nm-flat-slate-100':'')} onClick={(event:React.MouseEvent) => selectPart(event,opt.id)}>
+        <div className={'rounded-md transition duration-200 ease-in-out w-[40px] h-[40px] flex items-center justify-center' + (props.activeFeature==opt.id?' nm-flat-slate-100':'')} onClick={(event:React.MouseEvent) => selectPart(event,opt.id)}>
           <Image src={'/resources/icos/features/' + opt.id + '.svg'} width={25} height={25} alt={'test'} className='opacity-80'/>
         </div>
-        <p className={'text-[10px] pt-1 opacity-50' + (props.activedPart==opt.id?' opacity-90 text-slate-700':'')}>{opt.id}</p>
+        <p className={'text-[10px] pt-1 opacity-50' + (props.activeFeature==opt.id?' opacity-90 text-slate-700':'')}>{opt.id}</p>
       </SwiperSlide>
     )
   });
@@ -31,7 +31,8 @@ function optionList(props: props) {
 
 export default function FeatureSelectorComponent( props: props) {
   return (
-    <div className='w-full border-t-2 border-slate-50'>
+    <div className='w-full relative'>
+      <div className='absolute h-full w-[50px] top-0 left-0 bg-gradient-to-r from-slate-100 z-10 pointer-events-none'></div>
       <p className='nm-flat-slate-100 hidden absolute'></p>
       <Swiper
         slidesPerView={6}
@@ -44,6 +45,7 @@ export default function FeatureSelectorComponent( props: props) {
       >
         { optionList(props) }
       </Swiper>
+      <div className='absolute h-full w-[50px] top-0 right-0 bg-gradient-to-l from-slate-100 z-10 pointer-events-none'></div>
     </div>
   )
 }
