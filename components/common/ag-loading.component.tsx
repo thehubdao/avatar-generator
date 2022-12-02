56c6b7b2dbc2bda0a6f617051a7b81bc7b61ee01
OswaldUnity <oswald@thedac.info>
2022-12-02 10:38:18 -0500

    Fix wrong loading behavior

--- components/common/ag-loading.component.tsx
+++ components/common/ag-loading.component.tsx
@@ -1,26 +1,23 @@
-import {Component} from "react";
-
 interface AGLoadingProps {
   loading?: boolean;
   transparency?: boolean;
   bgColor?: string;
 }
 
-export default class AGLoading extends Component<AGLoadingProps> {
-  render() {
-    return (
-      <>
-        {
-          this.props.loading ?
-          <div className='fixed top-0 left-0 w-screen h-screen flex justify-center items-center' style={{backgroundColor: `#${this.props.bgColor ?? "FFFFFF"}`}}>
+export default function AGLoading({loading, bgColor, transparency}: AGLoadingProps) {
+  return (
+    <>
+      {
+        loading ?
+          <div className={'fixed z-50 top-0 left-0 w-screen h-screen flex justify-center items-center' + (transparency ? ' bg-opacity-50 backdrop-blur-sm' : '')}
+               style={{backgroundColor: `#${bgColor ?? "FFFFFF"}${transparency ? '80' : ''}`}}>
             <div className="leap-frog">
-            <div className="leap-frog__dot"></div>
-            <div className="leap-frog__dot"></div>
-            <div className="leap-frog__dot"></div>
-          </div>
+              <div className="leap-frog__dot"></div>
+              <div className="leap-frog__dot"></div>
+              <div className="leap-frog__dot"></div>
+            </div>
           </div>: ''
-        }
-      </>
-    );
-  }
+      }
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -144,6 +144,7 @@ export default class AvatarGenerator extends Component<AvatarGeneratorProps, Ava
     
     // this.setLoading(false);
     this.setLoading(false);
+    this.setLoading(false);
     // trigger event when component has all data to render
     this.props.onDataLoaded?.()
 
@@ -531,7 +532,7 @@ export default class AvatarGenerator extends Component<AvatarGeneratorProps, Ava
   private renderEditMode() {
     return (
       <>
-        <AGLoading loading={this.state.loading} bgColor={this.props.bgColor}/>
+        <AGLoading loading={this.state.loading} bgColor={this.props.bgColor} />
         {/* CANVAS WRAPPER */}
         <div className="fixed left-[50%] translate-x-[-50%] flex justify-center items-start !w-full !h-full overflow-hidden transition-width transition-height duration-300 ease-in-out">
           {/* CANVAS BACKGROUND */}
