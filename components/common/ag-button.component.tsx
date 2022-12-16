da4edd8a66f932f72408533170c2bf79a2c6c6a3
OswaldUnity <oswald@thedac.info>
2022-12-15 19:11:36 -0500

    Update AGButton component to react function

--- components/common/ag-button.component.tsx
+++ components/common/ag-button.component.tsx
@@ -1,8 +1,6 @@
-﻿import {Component} from "react";
-
-interface AGButtonProps {
+﻿interface AGButtonProps {
   type?: 'primary' | 'secondary' | 'alert' | 'danger';
-  side?: 'center' | 'start' | 'end';
+  align?: 'center' | 'start' | 'end';
   onClickEvent?: () => void;
   children?: string | JSX.Element;
   form?: boolean;
@@ -14,61 +12,76 @@ interface AGButtonState {
   borderColor: string;
   textColor: string;
   hover: string;
-  side?: string;
+  side: string;
 }
 
-export default class AGButton extends Component<AGButtonProps, AGButtonState>{
-  constructor(props: AGButtonProps) {
-    super(props);
-    this.state = {
-      ...this.getBtnType(props.type),
-      side: this.getSide(props.side),
-    };
-  }
-  
-  getBtnType(type?: string): AGButtonState {
+export default function AGButton({type, align, form, onClickEvent, tooltip, children}: AGButtonProps) {
+  const vD: AGButtonState = {
+    ...getBtnType(),
+    side: getSide()
+  };
+
+  function getBtnType() {
     switch (type) {
       case 'secondary':
-        return { color: 'bg-teal-600', textColor: 'text-white', borderColor: 'border-gray-600', hover: 'hover:bg-teal-500' };
+        return {
+          color: 'bg-teal-600',
+          textColor: 'text-white',
+          borderColor: 'border-gray-600',
+          hover: 'hover:bg-teal-500'
+        };
       case 'alert':
-        return { color: 'bg-yellow-500', textColor: 'text-white', borderColor: 'border-gray-600', hover: 'hover:bg-yellow-400' };
+        return {
+          color: 'bg-yellow-500',
+          textColor: 'text-white',
+          borderColor: 'border-gray-600',
+          hover: 'hover:bg-yellow-400'
+        };
       case 'danger':
-        return { color: 'bg-rose-600', textColor: 'text-white', borderColor: 'border-gray-600', hover: 'hover:bg-rose-500' };
+        return {
+          color: 'bg-rose-600',
+          textColor: 'text-white',
+          borderColor: 'border-gray-600',
+          hover: 'hover:bg-rose-500'
+        };
       default:
         // primary
-        return { color: 'bg-sky-600', textColor: 'text-white', borderColor: 'border-gray-600', hover: 'hover:bg-sky-500' };
+        return {
+          color: 'bg-sky-600',
+          textColor: 'text-white',
+          borderColor: 'border-gray-600',
+          hover: 'hover:bg-sky-500'
+        };
     }
   }
 
-  private getSide(side: "center" | "start" | "end" | undefined) {
-    switch(side) {
+  function getSide() {
+    switch (align) {
       case "start":
         return "justify-start";
       case "end":
         return "justify-end";
       default:
-        return 'justify-center';
+        return "justify-center";
     }
   }
-  
-  render() {
-    const { color, borderColor, textColor, hover, side} = this.state;
-    
-    return (
-      <div className={`my-2 flex ${side ?? ''}`}>
-        {
-          this.props.form ?
-            <button type="submit" className={`mx-2 w-auto my-auto rounded py-1 border-1 ${hover} ${textColor} ${color} ${borderColor}`}
-                 onClick={this.props.onClickEvent} title={this.props.tooltip}>
-              <span className='px-4'>{this.props.children}</span>
-            </button>
-            :
-            <div className={`mx-2 w-auto my-auto rounded py-1 border-1 cursor-pointer ${hover} ${textColor} ${color} ${borderColor}`}
-                 onClick={this.props.onClickEvent} title={this.props.tooltip}>
-              <span className='px-4'>{this.props.children}</span>
-            </div>
-        }
-      </div>
-    );
-  }
+
+  return (
+    <div className={`my-2 flex ${vD.side ?? ''}`}>
+      {
+        form ?
+          <button type="submit"
+                  className={`mx-2 w-auto my-auto rounded py-1 border-1 ${vD.hover} ${vD.textColor} ${vD.color} ${vD.borderColor}`}
+                  onClick={onClickEvent} title={tooltip}>
+            <span className='px-4'>{children}</span>
+          </button>
+          :
+          <div
+            className={`mx-2 w-auto my-auto rounded py-1 border-1 cursor-pointer ${vD.hover} ${vD.textColor} ${vD.color} ${vD.borderColor}`}
+            onClick={onClickEvent} title={tooltip}>
+            <span className='px-4'>{children}</span>
+          </div>
+      }
+    </div>
+  );
 }
