9f9c0ca15c78b0510306bcfa2420f826da334f22
OswaldUnity <oswald@thedac.info>
2023-02-01 17:30:50 -0500

    Fix models coming with wrong name

--- utils/importer.util.ts
+++ utils/importer.util.ts
@@ -67,7 +67,7 @@ export async function GetFeaturesData(baseModel: Group, featureList: BasicData[]
     let result: Record<string, FeatureInfoInterface> = {};
 
     for (const [index, feature] of baseFeatures.children.entries()) {
-      if (featureList.some(x => x.val === feature.name)) {
+      if (featureList.some(x => feature.name.startsWith(x.val))) {
         const foundFeature = featureList.find(x => x.val === feature.name);
         result[foundFeature!.id] = {
           featureIndex: index,
