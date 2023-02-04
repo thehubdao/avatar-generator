98b7b8da352965070ad6139a7c0d3848a06fb9f8
OswaldUnity <oswald@thedac.info>
2023-02-03 22:22:03 -0500

    Update collection page, now has layout

--- pages/admin/collection/[collection].tsx
+++ pages/admin/collection/[collection].tsx
@@ -5,6 +5,7 @@ import {FirestoreParameters} from "../../../enums/firebase.enum";
 import {CampaignParameters} from "../../../interfaces/common.interface";
 import {CampaignParameterName} from "../../../enums/common.enum";
 import {useEffect} from "react";
+import Layout from "../../../components/admin/_layout.component";
 
 interface AvatarCollectionPageProps {
   isCampaign: boolean;
@@ -23,15 +24,19 @@ export default function AvatarCollectionPage({isCampaign, campaign, campaignPara
   return (
     <>
       { isCampaign ?
-        <AvatarCollection start={start}
-                          end={end}
-                          single={single}
-                          campaign={campaign}
-                          avatarBasePath={campaignParams?.armature ?? ''}
-                          featureList={campaignParams?.features ?? []}
-                          defaultAnimation={campaignParams?.config?.defAnimation}
-                          skinColor={campaignParams?.config?.defSkinColor ?? '00ffff'}
-        />
+        <Layout setUserInfo={() => {}}
+                noCampaign
+                setCurrentCampaign={() => {}} >
+          <AvatarCollection start={start}
+                            end={end}
+                            single={single}
+                            campaign={campaign}
+                            avatarBasePath={campaignParams?.armature ?? ''}
+                            featureList={campaignParams?.features ?? []}
+                            defaultAnimation={campaignParams?.config?.defAnimation}
+                            skinColor={campaignParams?.config?.defSkinColor ?? '00ffff'}
+          />
+        </Layout>
         :
         <h1>Is not a Campaign</h1>
       }
