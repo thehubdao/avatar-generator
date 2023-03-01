66606724fb5021f6c607e45aa9e50893a35b4711
NaN <nan@thedac.info>
2023-03-01 16:58:03 -0500

    INDEX PAGE Updated

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,6 @@
-﻿import {useState} from "react";
+﻿import { useState } from "react";
 import Head from "next/head";
-import {UserInterface} from "../../interfaces/firebase.interface";
+import { UserInterface } from "../../interfaces/firebase.interface";
 import Layout from "../../components/admin/_layout.component";
 import AGLoading from "../../components/common/ag-loading.component";
 import Dashboard from "../../components/admin/dashboard.component";
@@ -8,12 +8,10 @@ import Dashboard from "../../components/admin/dashboard.component";
 export default function Admin() {
   const [loading, setLoading] = useState<boolean>(true);
   const [userInfo, setUserInfo] = useState<UserInterface>();
-  const [selectedCampaign, setSelectedCampaign] = useState<string>();
 
   function updateUserInfo(user?: UserInterface) {
     setLoading(false);
     setUserInfo(user);
-    setSelectedCampaign(user?.campaign?.at(0));
   }
 
   return (
@@ -21,18 +19,14 @@ export default function Admin() {
       <Head>
         <title>Admin Dashboard</title>
       </Head>
-      <div className="bg-bg">
-        <AGLoading loading={loading} transparency/>
-        <Layout userInfo={userInfo}
-                currentCampaign={selectedCampaign}
-                setUserInfo={(user) => updateUserInfo(user)}
-                setCurrentCampaign={(campaign) => setSelectedCampaign(campaign)}>
-          {!loading &&
-              <Dashboard userRole={userInfo?.role} campaign={selectedCampaign} campaignList={userInfo?.campaign}>
-              </Dashboard>
-          }
-        </Layout>
-      </div>
+      <AGLoading loading={loading} transparency />
+      <Layout userInfo={userInfo}
+        setUserInfo={(user) => updateUserInfo(user)}>
+        {!loading &&
+          <Dashboard userRole={userInfo?.role} campaignList={userInfo?.campaign}>
+          </Dashboard>
+        }
+      </Layout>
     </>
   );
 }
