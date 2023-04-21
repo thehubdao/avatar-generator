f26d2b6a5ae46e1f5302d3434b11998534abedce
OswaldUnity <oswald@thedac.info>
2023-04-21 12:02:41 -0500

    Added single component

---
+++ components/avatar/single.component.tsx
@@ -0,0 +1,84 @@
+﻿import {useRef} from "react";
+import {
+  GetAnimationByCampaignAndName,
+  GetAvatarSingleByCampaignCombination
+} from "../../utils/api.util";
+import AvatarEditor, {ChangeFeature, ChangeSkinColor, ChangeStartAnimation, SetFeaturesData} from "./editor.component";
+import {BasicData, LookAtVectors} from "../../interfaces/common.interface";
+import {AnimationInterface, SingleInterface} from "../../interfaces/api.interface";
+import {AGChangeCamPosition, AGChangeLookAtPosition} from "./viewer.component";
+import {LogError} from "../../utils/common.util";
+import {Module} from "../../enums/common.enum";
+
+interface AvatarSingleProps {
+  campaign: string;
+  combination?: number;
+  featureList: BasicData[];
+  avatarBasePath: string;
+  defaultAnimation?: string;
+  defaultSkinTone?: string;
+  defaultCameraPosition?: LookAtVectors;
+}
+
+export default function AvatarSingle({
+                                       campaign,
+                                       combination,
+                                       featureList,
+                                       avatarBasePath,
+                                       defaultSkinTone,
+                                       defaultAnimation,
+                                       defaultCameraPosition
+                                     }: AvatarSingleProps) {
+  const singleData = useRef<SingleInterface>();
+  const defaultAnimationData = useRef<AnimationInterface>();
+
+  async function onAvatarBuilderReady() {
+    await Promise.all([
+      getSingleData(),
+      getAnimation()
+    ]);
+    
+    await SetFeaturesData(featureList);
+
+    // Set features from single
+    await loadSingleData();
+
+    // Set skin tone
+    defaultSkinTone != undefined && await ChangeSkinColor(defaultSkinTone);
+    // Set camera position and lookAt
+    defaultCameraPosition?.pos != undefined && AGChangeCamPosition(defaultCameraPosition.pos);
+    defaultCameraPosition?.lookAt != undefined && AGChangeLookAtPosition(defaultCameraPosition.lookAt);
+    
+    // Set animation
+    await ChangeStartAnimation(defaultAnimationData.current?.path);
+  }
+
+  async function getSingleData() {
+    const {value: reqSingleData} = await GetAvatarSingleByCampaignCombination(campaign, combination);
+    singleData.current = reqSingleData;
+  }
+
+  async function getAnimation() {
+    const {value} = await GetAnimationByCampaignAndName(campaign, defaultAnimation);
+    defaultAnimationData.current = value?.at(0);
+  }
+  
+  async function loadSingleData() {
+    // Iterate the features
+    // Place the features on the model
+    if (singleData.current == undefined)
+      return void LogError(Module.Single, "Missing single data!");
+      
+    for (const {val: {id, path, type, name}} of singleData.current.features) {
+      // Set feature on model
+      await ChangeFeature(id, path, name, type, defaultSkinTone);
+    }
+  }
+  
+  return (
+    <>
+      <AvatarEditor avatarBasePath={avatarBasePath}
+                    onReady={() => onAvatarBuilderReady()}/>
+    </>
+  );
+}
