2f493e0ef82a58db585e28767dfeef91b413fbcd
OswaldUnity <oswald@thedac.info>
2023-04-21 00:12:23 -0500

    Update added better api request generic function

--- utils/api.util.ts
+++ utils/api.util.ts
@@ -12,13 +12,35 @@ import {Module} from "../enums/common.enum";
 import {ApiRoutesV1} from "../enums/api.enum";
 
 //#region Generic
+type QueryParams = {[p: string]: string | number | undefined | null};
+type UriParams = (string | number | undefined)[];
+
 const GET_PARAMS: RequestInit = {
   method: 'GET'
 };
 
-async function GetRequest<T>(url: string | ApiRoutesV1, query?: string): Promise<Result<T>> {
+function BuildQuery(uris: UriParams, queries: QueryParams) {
+  let newUris = '';
+  for (const uri of uris) {
+    if (uri == undefined) continue;
+    
+    newUris += `/${uri}`;
+  }
+  
+  const params = new URLSearchParams();
+  for (const key of Object.keys(queries)) {
+    if (queries[key] == undefined) continue;
+    
+    params.append(key, `${queries[key]}`);
+  }
+  
+  return newUris + (params.toString() != '' ? `?${params.toString()}` : '');
+}
+
+async function GetRequest<T>(url: string | ApiRoutesV1, extraUri?: UriParams, query?: QueryParams): Promise<Result<T>> {
   try {
-    const jsonResult: ApiResponse<T> = await fetch(url + (query ?? ''), GET_PARAMS).then(res => res.json());
+    const extraQuery = BuildQuery(extraUri ?? [], query ?? {});
+    const jsonResult: ApiResponse<T> = await fetch(url + extraQuery, GET_PARAMS).then(res => res.json());
     if (jsonResult.success)
       return {success: true, value: jsonResult.data};
     else
@@ -58,19 +80,23 @@ async function PostRequest<T>(url: string, obj?: object): Promise<Result<T>> {
 //#endregion
 
 export async function GetAssetsListByCampaign(campaign?: string | null) {
-  return GetRequest<FeatureInterface[]>(ApiRoutesV1.FeatureOptions,campaign ? ('?campaign=' + campaign) : undefined);
+  return GetRequest<FeatureInterface[]>(ApiRoutesV1.FeatureOptions, undefined, {campaign});
 }
 
 export async function GetAccessoryListByCampaign(campaign?: string | null) {
-  return GetRequest<AccessoryInterface[]>(ApiRoutesV1.AccessoryOptions, campaign ? ('?campaign=' + campaign) : undefined);
+  return GetRequest<AccessoryInterface[]>(ApiRoutesV1.AccessoryOptions, undefined, {campaign});
 }
 
 export async function GetAnimationListByCampaign(campaign?: string | null) {
-  return GetRequest<AnimationInterface[]>(ApiRoutesV1.Animations, campaign ? ('?campaign=' + campaign) : undefined);
+  return GetRequest<AnimationInterface[]>(ApiRoutesV1.Animations, undefined, {campaign});
+}
+
+export async function GetAnimationByCampaignAndName(campaign?: string | null, name?: string) {
+  return GetRequest<AnimationInterface[]>(ApiRoutesV1.Animations, undefined, {campaign, name});
 }
 
 export async function GetEnvironmentListByCampaign(campaign?: string | null) {
-  return GetRequest<EnvironmentInterface[]>(ApiRoutesV1.Environments, campaign ? ('?campaign=' + campaign) : undefined);
+  return GetRequest<EnvironmentInterface[]>(ApiRoutesV1.Environments, undefined, {campaign});
 }
 
 export async function PostUpdateAdminCampaigns() {
@@ -78,5 +104,5 @@ export async function PostUpdateAdminCampaigns() {
 }
 
 export async function GetAvatarSingleByCampaignCombination(campaign: string, combination?: number) {
-  return GetRequest<SingleInterface>(ApiRoutesV1.Single, `/${campaign}/${combination}`);
+  return GetRequest<SingleInterface>(ApiRoutesV1.Single, [campaign, combination]);
 }
