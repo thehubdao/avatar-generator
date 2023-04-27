e08fea26011980ce9e4b91e0c219cf00d22b85fe
OswaldUnity <oswald@thedac.info>
2023-04-27 14:54:50 -0500

    Removed code running chrome-aws-lambda

--- server/api-handler/v1/export.api-handler.ts
+++ server/api-handler/v1/export.api-handler.ts
@@ -1,25 +1,24 @@
 ﻿import {NextApiRequest, NextApiResponse} from "next";
 import {ApiResponse} from "../../../interfaces/api.interface";
-import chromium from "chrome-aws-lambda";
 import {RequestResponse} from "../request.api-handler";
 import {DefaultApiResponse} from "../../enums/api.enum";
 
 export async function GetApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<unknown>>) {
   const {combination} = req.query;
   
-  const browser = await chromium.puppeteer.launch({
-    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
-  });
-  
-  const page = await browser.newPage();
-  await page.setViewport({
-    height: 480,
-    width: 480
-  });
+  // const browser = await chromium.puppeteer.launch({
+  //   args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+  //   defaultViewport: chromium.defaultViewport,
+  //   executablePath: await chromium.executablePath,
+  //   headless: true,
+  //   ignoreHTTPSErrors: true,
+  // });
+  //
+  // const page = await browser.newPage();
+  // await page.setViewport({
+  //   height: 480,
+  //   width: 480
+  // });
   
   console.log(req.query.path);
   
