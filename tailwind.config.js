4f3175de0c0faac615d7e2c5cd0b055dbc55a53b
nan <nan@thedac.info>
2023-05-30 11:17:50 -0500

    TAILWIND CONFIG Updated
    - Adding new neomorphism styles from design team
    - Adding UI folder content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
     content: [
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
+        './ui/**/*.{js,ts,jsx,tsx}',
         // For the best performance and to avoid false positives,
         // be as specific as possible with your content configuration.
     ],
@@ -19,7 +20,8 @@
             normal: '#54575C',
             dark: '#111111'
           },
-          bg: '#F5F7FF',
+          bg: '#F1F5F9',
+          accent: '#19D8F3',
           purple: '#7f3eae',
           orange: '#d85b00',
           blue: '#006cad',
@@ -27,15 +29,15 @@
           light: 'rgba(255,255,255,0.4)'
         },
         boxShadow: {
-          'flat-soft': '2px 2px 4px #E9ECF6, -2px -2px 4px #ffffff',
-          'flat-medium': '4px 4px 8px #E9ECF6, -4px -4px 8px #ffffff',
-          'flat-hard': '6px 6px 12px #E9ECF6, -6px -6px 12px #ffffff',
-          'inset-soft': 'inset 2px 2px 4px #E9ECF6, inset -2px -2px 4px #ffffff',
-          'inset-medium': 'inset 4px 4px 8px #E9ECF6, inset -4px -4px 8px #ffffff',
-          'inset-hard': 'inset 6px 6px 12px #E9ECF6, inset -6px -6px 12px #ffffff',
-          'flat-inset-soft': '2px 2px 4px #E9ECF6, -2px -2px 4px #ffffff, inset 2px 2px 4px #E9ECF6, inset -2px -2px 4px #ffffff',
-          'flat-inset-medium': '4px 4px 8px #E9ECF6, -4px -4px 8px #ffffff, inset 4px 4px 8px #E9ECF6, inset -4px -4px 8px #ffffff',
-          'flat-inset-hard': '6px 6px 12px #E9ECF6, -6px -6px 12px #ffffff, inset 6px 6px 12px #E9ECF6, inset -6px -6px 12px #ffffff'
+          'flat-soft':'-2.81481px -2.81481px 6.6296px #FFFFFF, 2.81481px 2.81481px 6.6296px #D3D9E6',
+          'flat-medium':'-5.81481px -5.81481px 12.6296px #FFFFFF, 5.81481px 5.81481px 12.6296px #D3D9E6',
+          'flat-hard':'-8.78261px -8.78261px 17.5652px #FFFFFF, 8.78261px 8.78261px 17.5652px #D3D9E6',
+          'inset-soft':'inset 1.3913px 1.3913px 2.78261px rgba(0,0,0,0.15), inset -1.3913px -1.3913px 2.78261px rgba(255,255,255,0.7)',
+          'inset-medium':'inset 2.3913px 2.3913px 5.78261px rgba(0,0,0,0.15), inset -2.3913px -2.3913px 5.78261px rgba(255,255,255,0.7)',
+          'inset-hard':'inset 4.3913px 4.3913px 8.78261px rgba(0,0,0,0.15), inset -4.3913px -4.3913px 8.78261px rgba(255,255,255,0.7)',
+          'flat---inset-soft': '2px 2px 4px #E9ECF6, -2px -2px 4px #ffffff, inset 2px 2px 4px #E9ECF6, inset -2px -2px 4px #ffffff',
+          'flat---inset-medium': '4px 4px 8px #E9ECF6, -4px -4px 8px #ffffff, inset 4px 4px 8px #E9ECF6, inset -4px -4px 8px #ffffff',
+          'flat---inset-hard': '6px 6px 12px #E9ECF6, -6px -6px 12px #ffffff, inset 6px 6px 12px #E9ECF6, inset -6px -6px 12px #ffffff'
         }
       },
     }
