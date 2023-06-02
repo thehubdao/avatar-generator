5d05c912f38810d04061fb334a436595d4003649
Andres <andgonzalezcas@unal.edu.co>
2023-06-02 15:32:57 -0500

    a new infinite scroll has been added for the selection of the different features.

--- ui/avatar/selectors/featureSelector.component.tsx
+++ ui/avatar/selectors/featureSelector.component.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
-import { useEffect } from "react";
+import { Swiper, SwiperRef, SwiperSlide, useSwiper } from 'swiper/react';
+import { Mousewheel } from 'swiper'
 import { FeatureInterface } from "../../../interfaces/api.interface";
 import { BasicData } from "../../../interfaces/common.interface";
+import { useRef } from "react";
 
 interface FeatureSelectorProps {
   list?: FeatureInterface[] | BasicData[];
@@ -10,41 +12,95 @@ interface FeatureSelectorProps {
 }
 
 interface OptionSelectorProps {
-  item: FeatureInterface | BasicData,
-  isActive: boolean
+  list?: FeatureInterface[] | BasicData[];
+  activeOpc?: string
+  handleClick: (id: string) => void;
+  handleSlide: (id: number) => void;
 }
 
-function OptionSelector({ item, isActive }: OptionSelectorProps) {
+const ScrollButton = ({ position = 'top' }: { position?: 'top' | 'bottom' }) => {
+  const swiper = useSwiper();
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    position === 'top' ? swiper.slidePrev() : swiper.slideNext();
+  }
+
+  let boxStyle = ''
+  if (position === 'top') {
+    boxStyle = 'top-0'
+  } else if (position === 'bottom') {
+    boxStyle = 'bottom-0 rotate-180'
+  }
+
   return (
-    <div className={`w-[100px] h-[100px] flex justify-center items-center bg-bg rounded-xl cursor-pointer ${isActive ? 'bg-accent shadow-inset-hard' : 'shadow-flat-hard'}`} title={item.id}>
-      <Image alt={item.id} width={70} height={70} src={'/resources/icons/features/Chest.svg'} priority />
+    <div className={`absolute h-fit ${boxStyle} z-10`}>
+      <button
+        onClick={handleClick}
+        className="bg-bg flex justify-center items-center w-[164px] h-[80px]"
+      >
+        <div className={`-rotate-[135deg] w-4 h-4 border-r-[5px] border-b-[5px]`} />
+      </button>
+      <div className="bg-gradient-to-b from-slate-100 w-full h-[8px]" />
     </div>
   )
 }
 
-export default function FeatureSelector({ list, activeOpc, handleClick }: FeatureSelectorProps) {
-
-  const selectFeature = (e: React.MouseEvent, id: string) => {
+function optionSelector({ list, activeOpc, handleClick, handleSlide }: OptionSelectorProps) {
+  const selectFeature = (e: React.MouseEvent, id: string, index: number) => {
     e.preventDefault();
     handleClick(id);
+    handleSlide(index);
   }
 
+  return list?.map((opt: any, index: number) => {
+    const isActive: boolean = activeOpc && activeOpc === opt.id ? true : false
+    return (
+      <SwiperSlide key={opt.id} style={{ height: '110px' }} className="px-8">
+        <div
+          onClick={event => {
+            selectFeature(event, opt.id, index)
+          }}
+        >
+          <div className={`w-[100px] h-[100px] flex justify-center items-center bg-bg rounded-xl cursor-pointer ${isActive ? 'bg-accent shadow-inset-hard' : 'shadow-flat-hard'}`} title={opt.id}>
+            <Image alt={opt.id} width={70} height={70} src={'/resources/icons/features/Chest.svg'} priority />
+          </div>
+        </div>
+      </SwiperSlide>
+    )
+  })
+}
+
+export default function FeatureSelector({ list, activeOpc, handleClick }: FeatureSelectorProps) {
+  const swiperRef = useRef<SwiperRef | null>(null);
+
+  const handleSlide = (id: number) => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideToLoop(id, 1000);
+    }
+  };
+
+
   return (
-    <div>
-      {
-        list ?
-          list.map(opt => {
-            return (
-              <div key={opt.id} className="py-3 px-4">
-                <div onClick={event => selectFeature(event, opt.id)}>
-                  <OptionSelector item={opt} isActive={activeOpc && activeOpc === opt.id ? true : false} />
-                </div>
-              </div>
-            )
-          })
-          :
-          <p>no features</p>
-      }
+    <div className="relative h-fit">
+      <Swiper
+        slidesPerView='auto'
+        spaceBetween={10}
+        centeredSlides={true}
+        grabCursor={true}
+        direction={"vertical"}
+        loop={true}
+        modules={[Mousewheel]}
+        mousewheel={true}
+        ref={swiperRef}
+        // onSlideChange={() => console.log('slide change')}
+        // onSwiper={(swiper) => console.log(swiper)}
+        className='!pb-2 !pt-3 h-screen'
+      >
+        {optionSelector({ list, activeOpc, handleClick, handleSlide })}
+        <ScrollButton position="top" />
+        <ScrollButton position="bottom" />
+      </Swiper>
     </div>
   )
 }
