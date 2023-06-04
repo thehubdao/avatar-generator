851e71467639490ccc1b28631d6dd6eb20e0dd27
Andres <andgonzalezcas@unal.edu.co>
2023-06-04 10:31:26 -0500

    added comments on code

--- ui/avatar/selectors/colorSelector.component.tsx
+++ ui/avatar/selectors/colorSelector.component.tsx
@@ -13,20 +13,27 @@ interface ColorOptionProps {
   listStyle?: 'Oval' | 'Rectangular'
 }
 
+/** 
+ * @params the same as the parent component
+ * @param isActive A boolean value indicating whether the option is currently selected.
+ */
+
 function ColorOption({ option, isActive, listStyle = 'Rectangular' }: ColorOptionProps) {
 
+  // * Variables controlling the style of the selection buttons
   const [activeStyle, setActiveStyle] = useState<string>('')
   const [optionStyle, setOptionStyle] = useState<string>('')
+
+  //* List style control
   useEffect(() => {
     if (listStyle === 'Oval') {
-      // Oval Style on option buttons
       setOptionStyle('w-8 h-8 shadow-flat-medium rounded-full p-[.4rem]')
     } else {
-      // Rectangle Style on option buttons
       setOptionStyle('w-12 h-12 p-2 shadow-flat-medium rounded-lg')
     }
   }, [listStyle])
 
+  // * Is active style control
   useEffect(() => {
     const rectangleStyle = (listStyle === 'Rectangular') ? 'p-4' : ''
     isActive
@@ -41,13 +48,23 @@ function ColorOption({ option, isActive, listStyle = 'Rectangular' }: ColorOptio
   )
 }
 
+/**
+ * @param list An array of colors in hexadecimal format without the '#' symbol, e.g., ['FF0000'].
+ * @param activeColor 
+ * @param handleClick The function enables us to select the color for a specific feature.
+ * @param listStyle It allows us to choose between oval and rectangular styles from the color picker list.
+ * 
+ * @return color selector component
+ */
+
 export default function ColorSelector({ list, activeColor, handleClick, listStyle = 'Rectangular' }: ColorSelectorProps) {
   const selectFeature = (e: React.MouseEvent, color: string) => {
     e.preventDefault();
     handleClick(color);
   }
-
+  
   const [barStyle, setBarStyle] = useState<string>('')
+  // * List Style bar controller
   useEffect(() => {
     if (listStyle === 'Oval') {
       setBarStyle('shadow-inset-medium rounded-full px-4 py-2 w-fit')
