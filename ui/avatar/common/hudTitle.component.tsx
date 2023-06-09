b14f5a743fb540a0d4dcdff50a4c233008cbef78
Andres <andgonzalezcas@unal.edu.co>
2023-06-09 15:05:33 -0500

    added documentation on code

--- ui/avatar/common/hudTitle.component.tsx
+++ ui/avatar/common/hudTitle.component.tsx
@@ -4,28 +4,39 @@ interface hudTitleProps {
   selectedFeature: string
 }
 
+/**
+ * HUD Title component for controlling HUD title animation.
+ * 
+ * @param {string} selectedFeature - The selected feature to display as the HUD title.
+ */
 const HudTitle = ({ selectedFeature }: hudTitleProps) => {
-  const [currentTitle, setCurrentTitle] = useState<string>('HUD');
-  const [nextTitle, setNextTitle] = useState<string>('HUD');
+  // State for the current and next HUD titles
+  const [currentTitle, setCurrentTitle] = useState('HUD');
+  const [nextTitle, setNextTitle] = useState('HUD');
 
-  const [runAnimation, setRunAnimation] = useState<boolean>(false);
+  // State for controlling the animation
+  const [runAnimation, setRunAnimation] = useState(false);
 
   useEffect(() => {
+    // Update the nextTitle and trigger the animation
     setNextTitle(selectedFeature);
     setRunAnimation(true);
 
+    // After a delay, update the currentTitle and stop the animation
     setTimeout(() => {
       setCurrentTitle(selectedFeature);
       setRunAnimation(false);
-    }, 500)
-  }, [selectedFeature])
+    }, 500);
+  }, [selectedFeature]);
 
   return (
     <div className="w-full h-[60px] relative overflow-hidden">
+      {/* Current HUD title with animation */}
       <h2 className={`absolute font-work font-bold text-6xl uppercase truncate ${runAnimation ? '-top-[60px] duration-500 transition-all' : 'top-0'}`}>{currentTitle}</h2>
+      {/* Next HUD title with animation */}
       <h2 className={`absolute font-work font-bold text-6xl uppercase truncate ${runAnimation ? 'bottom-0 duration-500 transition-all' : '-bottom-[60px]'}`}>{nextTitle}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default HudTitle
+export default HudTitle;
