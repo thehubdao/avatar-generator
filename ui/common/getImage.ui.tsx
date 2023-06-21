54bd5335cca96c43e7c24a2d85514faa37785996
Andres <andgonzalezcas@unal.edu.co>
2023-06-21 11:52:51 -0500

    Added get image component documentation

--- ui/common/getImage.ui.tsx
+++ ui/common/getImage.ui.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
 
 interface GetImageUiInterface {
-  imageUrl?: string,
-  alt?: string,
+  imageUrl?: string;
+  alt?: string;
 }
 
-export default function GetImageUI({ imageUrl, alt = 'imagen' }: GetImageUiInterface) {
+/**
+ ** GetImageUI is a versatile component that displays images with fallback options.
+ *
+ * @param {string} imageUrl - The URL of the image to be displayed.
+ * @param {string} alt The alternative text for the image. @default 'imagen'
+ * 
+ * @returns {JSX.Element} - The rendered GetImageUI component.
+ */
+export default function GetImageUI({ imageUrl, alt = 'imagen' }: GetImageUiInterface): JSX.Element {
   return (
     <>
-      {imageUrl == undefined ?
+      {imageUrl === undefined ? (
+        // Render default image if no imageUrl is provided
         <div className="relative w-2/4 h-2/4">
           <Image src={'/resources/icons/features/default.svg'} fill alt={alt} />
         </div>
-        :
+      ) : (
+        // Render provided imageUrl as an Image component
         <Image src={imageUrl} fill alt={alt} />
-      }
+      )}
     </>
-  )
+  );
 }
