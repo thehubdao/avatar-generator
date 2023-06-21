b835fe1252accd8be6084c7250544d7aedddd622
Andres <andgonzalezcas@unal.edu.co>
2023-06-21 11:39:47 -0500

    Added AG text component documentation

--- ui/common/ag-text.component.tsx
+++ ui/common/ag-text.component.tsx
@@ -1,5 +1,5 @@
-﻿import {Component} from "react";
-import {Delay} from "../../utils/common.util";
+﻿import { Component } from "react";
+import { Delay } from "../../utils/common.util";
 
 type TextType = 'th1' | 'th1.5' | 'th2' | 'th3' | 'text' | 'code' | 'link' | 'end' | 'icon';
 type MarkType = 'bullet' | 'dash';
@@ -17,12 +17,16 @@ interface AGTextState {
   copied: boolean;
 }
 
+//* AGText is a text component with various styles and functionalities.
 export default class AGText extends Component<AGTextProps, AGTextState> {
+  /**
+   * @returns The CSS class for the text alignment based on the specified side.
+   */
   getSide() {
     const { side } = this.props;
-    if(!side)
+    if (!side)
       return '';
-    
+
     switch (side) {
       case "center":
         return ' text-center';
@@ -32,14 +36,17 @@ export default class AGText extends Component<AGTextProps, AGTextState> {
         return ' text-right';
     }
   }
-  
+
+  /**
+   * @returns The JSX element for the text mark based on the specified mark type.
+   */
   renderMark() {
     const { mark } = this.props;
-    if(!mark)
+    if (!mark)
       return;
-    
+
     let leMark = mark;
-    
+
     switch (mark) {
       case "bullet":
         leMark = '◉';
@@ -48,13 +55,16 @@ export default class AGText extends Component<AGTextProps, AGTextState> {
         leMark = '-';
         break;
     }
-    
+
     return <span className="ml-1 mr-3">{leMark}</span>;
   }
-  
+
+  /**
+   * @returns The JSX element for the text based on the specified text type.
+   */
   renderType() {
     const { children, type, href } = this.props;
-    
+
     switch (type) {
       case 'th1':
         return <p className={"mt-2 mb-10 font-bold text-5xl" + this.getSide()}>{this.renderMark()}{children}</p>;
@@ -69,37 +79,52 @@ export default class AGText extends Component<AGTextProps, AGTextState> {
       case 'code':
         return (
           <div>
-            <button title="Copy"
-                    className="bg-sky-300 border-cyan-500 border-2 rounded-lg p-2 float-right mr-3 -mt-2"
-                    onClick={() => void this.copyToClipboard(children as string)}>{this.state?.copied ? '✔' : '📄'}</button>
-            <p className={'my-2 mx-1 p-2 bg-slate-400 text-sm text-indigo-900 rounded' + (this.props.justText ? ' break-all' : ' whitespace-pre-wrap') }
-               style={{fontFamily: '"Lucida Console", Monaco'}}>{children}</p>
+            <button
+              title="Copy"
+              className="bg-sky-300 border-cyan-500 border-2 rounded-lg p-2 float-right mr-3 -mt-2"
+              onClick={() => void this.copyToClipboard(children as string)}
+            >
+              {this.state?.copied ? '✔' : '📄'}
+            </button>
+            <p
+              className={'my-2 mx-1 p-2 bg-slate-400 text-sm text-indigo-900 rounded' + (this.props.justText ? ' break-all' : ' whitespace-pre-wrap')}
+              style={{ fontFamily: '"Lucida Console", Monaco' }}
+            >
+              {children}
+            </p>
           </div>
         );
       case "link":
-        return <> <a target="_blank" rel='noreferrer' href={href} className="underline">{children}</a></>
+        return (
+          <>
+            {" "}
+            <a target="_blank" rel='noreferrer' href={href} className="underline">{children}</a>
+          </>
+        );
       case "end":
         return <div className="mb-20"></div>;
       case "icon":
-        return <span className="text-4xl">{children}</span>
+        return <span className="text-4xl">{children}</span>;
     }
   }
-  
+
+  /**
+   * Copies the specified text to the clipboard.
+   * @param {string} text - The text to be copied.
+   */
   async copyToClipboard(text: string) {
     try {
       await navigator.clipboard.writeText(text);
 
-      this.setState({copied: true});
+      this.setState({ copied: true });
       await Delay(1000);
-      this.setState({copied: false});
+      this.setState({ copied: false });
     } catch (e) {
       console.error("Error copying to clipboard: ", e);
     }
   }
-  
+
   render() {
-    return (
-      this.renderType()
-    );
+    return this.renderType();
   }
 }
