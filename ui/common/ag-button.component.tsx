c6412d7c22e95ae0ab939fb44eaded8eef79ef72
Andres <andgonzalezcas@unal.edu.co>
2023-06-26 11:38:44 -0500

    update avatar generator button selected prop to extend functionality

--- ui/common/ag-button.component.tsx
+++ ui/common/ag-button.component.tsx
@@ -5,7 +5,11 @@
   children?: string | JSX.Element;
   form?: boolean;
   nm?: boolean;
-  selected?: boolean;
+  selected?: {
+    isSelected: boolean,
+    color?: string,
+    textColor?: string
+  };
   fit?: boolean;
   full?: boolean;
   circle?: boolean;
@@ -33,7 +37,7 @@ export default function AGButton({ type, align, form, nm, selected, fit, full, c
           color: 'bg-teal-600',
           textColor: 'text-white',
           borderColor: 'border-gray-600',
-          hover: 'hover:bg-teal-500'
+          hover: 'hover:bg-teal-500',
         };
       case 'alert':
         return {
@@ -76,9 +80,9 @@ export default function AGButton({ type, align, form, nm, selected, fit, full, c
       <button type={form ? 'submit' : undefined}
         className={`mx-2 w-auto min-h-[32px] my-auto rounded py-1 border-1 
         ${vD.hover} ${vD.textColor} ${vD.color} ${vD.borderColor}
-        ${selected ? 'bg-cyan-300' : ''}
+        ${selected?.isSelected ? `${selected.textColor} ${selected.color}` : ''}
         ${nm ? 'shadow-flat-soft rounded-lg' : ''}
-        ${nm && selected ? '!shadow-inset-soft rounded-lg' : ''}
+        ${nm && selected?.isSelected ? '!shadow-inset-soft rounded-lg' : ''}
         ${fit ? 'w-fit' : 'min-w-[100px]'}
         ${full ? '!w-full mx-0' : ''}
         ${circle ? 'rounded-full' : ''}
