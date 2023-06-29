3881a85fbd70b87b7ccf932988e08f893b008bd9
Andres <andgonzalezcas@unal.edu.co>
2023-06-29 12:24:25 -0500

    update we are working... message, and add enum data object to have better code implementation.

--- ui/admin/common/mobileBuildAlert.ui.tsx
+++ ui/admin/common/mobileBuildAlert.ui.tsx
@@ -1,5 +1,5 @@
 import AGButton from "../../common/ag-button.component";
-
+import { SocialMedia } from "../../../enums/socialmedia.enum";
 import {
   FaLinkedin,
   FaTelegramPlane,
@@ -11,6 +11,39 @@ import {
   FaEnvelope
 } from "react-icons/fa";
 
+interface SocialMediaDataProps {
+  link: string;
+  icon: React.ReactNode;
+}
+
+//* Social Media Data as a Object
+const socialMediaData: Record<SocialMedia, SocialMediaDataProps> = {
+  [SocialMedia.Medium]: { link: "https://medium.com/@THEHUB_DAO", icon: <FaMedium className="social-media-icon" />, },
+  [SocialMedia.Instagram]: { link: "https://www.instagram.com/thehub_dao/", icon: <FaInstagram className="social-media-icon" />, },
+  [SocialMedia.Linkedin]: { link: 'https://www.linkedin.com/company/the-hub-dao/', icon: <FaLinkedin className="social-media-icon" /> },
+  [SocialMedia.Twitter]: { link: 'https://twitter.com/thehub_dao', icon: <FaTwitter className="social-media-icon" /> },
+  [SocialMedia.Telegram]: { link: 'https://t.me/thehub_dao', icon: <FaTelegramPlane className="social-media-icon" /> },
+  [SocialMedia.Discord]: { link: 'https://discord.com/invite/J35NGdPWgq', icon: <FaDiscord className="social-media-icon" /> },
+  [SocialMedia.Etherscan]: { link: 'https://etherscan.io/token/0x8765b1a0eb57ca49be7eacd35b24a574d0203656', icon: <FaEthereum className="social-media-icon" /> },
+  [SocialMedia.Mail]: { link: 'mailto:info@thedac.info', icon: <FaEnvelope className="social-media-icon" /> },
+};
+
+/**
+ ** Represents Social Media Button that redirect us to a link
+ * @returns {TSX.Element} The social media button component.
+ */
+const SocialMediaButton = ({ link, icon }: SocialMediaDataProps) => {
+  return (
+    <AGButton nm fit>
+      <a
+        href={link}
+        className="cursor-pointer"
+        target="_blank"
+      >{icon}</a>
+    </AGButton>
+  )
+}
+
 /**
  ** Represents the mobile is building alert UI.
  * @returns {TSX.Element} The mobile build alert UI component.
@@ -19,68 +52,13 @@ export default function MobileBuildAlert() {
   return (
     <div className="w-full min-h-[calc(100vh-176px)] flex flex-col items-center max-w-lg m-auto justify-between">
       <h1 className="font-humane text-9xl text-gray-normal">COMMING SOON</h1>
-      <p className="text-center">We are working on the mobile version, while we finalize the details we invite you to visit the Hub DAO website. If you want to learn more about us, we invite you to contact us through our social networks.</p>
+      <p className="text-center">We are working on the mobile version, while we finalize the details we invite you to visit us on desktop version. If you want to learn more about us, we invite you to contact us through our social networks.</p>
       <div className="w-fit">
         <AGButton nm full>
           <p className="font-bold">Go to The HUB Website</p>
         </AGButton>
         <div className="flex flex-row items-center justify-center flex-wrap pt-3 sm:pt-6">
-          <AGButton nm fit>
-            <a
-              href="https://medium.com/@THEHUB_DAO"
-              className="cursor-pointer"
-              target="_blank"
-            ><FaMedium className="social-media-icon" /></a>
-          </AGButton>
-          <AGButton nm fit>
-            <a
-              href="https://www.instagram.com/thehub_dao/"
-              className="cursor-pointer"
-              target="_blank"
-            ><FaInstagram className="social-media-icon" /></a>
-          </AGButton>
-          <AGButton nm fit>
-            <a
-              href="https://www.linkedin.com/company/the-hub-dao/"
-              className="cursor-pointer"
-              target="_blank"
-            ><FaLinkedin className="social-media-icon" /></a>
-          </AGButton>
-          <AGButton nm fit>
-            <a
-              href="https://twitter.com/thehub_dao"
-              className="cursor-pointer"
-              target="_blank"
-            ><FaTwitter className="social-media-icon" /></a>
-          </AGButton>
-          <AGButton nm fit>
-            <a
-              href="https://t.me/thehub_dao"
-              className="cursor-pointer"
-              target="_blank"
-            ><FaTelegramPlane className="social-media-icon" /></a>
-          </AGButton>
-          <AGButton nm fit>
-            <a
-              href="https://discord.com/invite/J35NGdPWgq"
-              className="cursor-pointer"
-              target="_blank"
-            ><FaDiscord className="social-media-icon" /></a>
-          </AGButton>
-          <AGButton nm fit>
-            <a
-              href="https://etherscan.io/token/0x8765b1a0eb57ca49be7eacd35b24a574d0203656"
-              className="cursor-pointer"
-              target="_blank"
-            ><FaEthereum className="social-media-icon" /></a>
-          </AGButton>
-          <AGButton nm fit>
-            <a
-              href="mailto:info@thedac.info"
-              className="cursor-pointer"
-              target="_blank"
-            ><FaEnvelope className="social-media-icon" /></a>
-          </AGButton>
+          {Object.values(SocialMedia).map(item => <SocialMediaButton link={socialMediaData[item].link} icon={socialMediaData[item].icon} />)}
         </div>
       </div>
     </div>
