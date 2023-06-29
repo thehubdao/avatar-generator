eabe2fae51febcaf2c949e68489bd3dcb1f2396c
Andres <andgonzalezcas@unal.edu.co>
2023-06-29 11:12:57 -0500

    added no data toshow message and button to create new asset on edit campaing section

--- ui/admin/campaign/editCampaign/assetList.ui.tsx
+++ ui/admin/campaign/editCampaign/assetList.ui.tsx
@@ -2,6 +2,10 @@ import { useAppSelector } from "../../../../store/hooks";
 import AssetCard from "./assetCard.ui";
 import { AssetType } from "../../../../types/asset.type";
 import { FirestoreLocation } from "../../../../enums/firebase.enum";
+import AGButton from "../../../common/ag-button.component";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { GoToPage } from "../../../../utils/router.util";
+import { PageLocation } from "../../../../enums/common.enum";
 
 interface AssetListInterface {
   activedOption: FirestoreLocation,
@@ -20,16 +24,24 @@ export default function AssetList({ activedOption }: AssetListInterface) {
   return (
     <div className="flex flex-wrap gap-6">
       {
-        campaignAssets[activedOption as keyof CampaignAssetsInterface] ?
-        (campaignAssets[activedOption as keyof CampaignAssetsInterface] as AssetType[]).map((asset: AssetType) => {
-          return (
-            <div key={asset.id}>
-              <AssetCard id={asset.id} name={asset.name} thumb={asset.thumb} location={activedOption} />
-            </div>
-          )
-        })
-        :
-        <p>No Data to show</p>
+        campaignAssets[activedOption as keyof CampaignAssetsInterface] && (campaignAssets[activedOption as keyof CampaignAssetsInterface] as AssetType[]).length > 0 ?
+          (campaignAssets[activedOption as keyof CampaignAssetsInterface] as AssetType[]).map((asset: AssetType) => {
+            return (
+              <div key={asset.id}>
+                <AssetCard id={asset.id} name={asset.name} thumb={asset.thumb} location={activedOption} />
+              </div>
+            )
+          })
+          :
+          <div className="w-full mx-2 flex items-center gap-5">
+            <p>{`No ${activedOption} to show`}</p>
+            <AGButton nm onClickEvent={() => void GoToPage(PageLocation.AssetCreate)}>
+              <div className={`flex items-center gap-2 p-2 font-poppins text-blue`}>
+                <IoMdAddCircleOutline className="text-2xl" />
+                <p>Create a new asset</p>
+              </div>
+            </AGButton>
+          </div>
       }
     </div>
   )
