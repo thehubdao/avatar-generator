d587547e7f7c453e2b3c2e097ba9a86ff8cf0ed7
nan <nan@thedac.info>
2023-06-29 10:07:47 -0500

    Add downloadFile function and send it via props to editCampaign UI component

--- pages/admin/campaign/index.tsx
+++ pages/admin/campaign/index.tsx
@@ -2,6 +2,13 @@ import Head from "next/head"
 import Layout from "../../../ui/admin/admin.layout"
 import AGLoading from "../../../ui/common/ag-loading.component"
 import EditCampaignUI from "../../../ui/admin/campaign/editCampaign/editCampaign.ui"
+import { GetFileUrl } from "../../../utils/firebase.util";
+
+async function downloadFIle(path: string) {
+  const fileLink = await GetFileUrl(path);
+  if (fileLink === undefined) return alert('error on file download, file link is undefined.')
+  window.open(fileLink, '_blank');
+}
 
 export default function Campaign() {
   return (
@@ -10,7 +17,7 @@ export default function Campaign() {
         <title>Admin - Campaign</title>
       </Head>
       <Layout>
-        <EditCampaignUI />
+        <EditCampaignUI downloadFile={(path: string) => void downloadFIle(path)} />
       </Layout>
       <AGLoading loading={false} bgColor="F1F5F9" />
     </>
