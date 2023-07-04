2651d5cb0fee170b1e93b5c1b00c22fb30bd787c
nan <nan@thedac.info>
2023-07-04 17:00:27 -0500

    Edit props type allowing to downloadFile function be a promise too on editCampaign UI

--- ui/admin/campaign/editCampaign/editCampaign.ui.tsx
+++ ui/admin/campaign/editCampaign/editCampaign.ui.tsx
@@ -13,7 +13,7 @@ import ConfigCampaign from "./configCampaign.ui";
 import CampaignStats from "./campaignStats.ui";
 
 interface EditCampaignUIProps {
-  downloadFile: (path: string) => void;
+  downloadFile: ((path: string) => void) | ((path: string) => Promise<void>);
 }
 
 export default function EditCampaignUI({ downloadFile }: EditCampaignUIProps) {
