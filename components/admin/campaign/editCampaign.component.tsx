6466ebd6f4386309f44b85528f131ac9374b6872
nan <nan@thedac.info>
2023-07-04 17:00:42 -0500

    Create editCampaign component

---
+++ components/admin/campaign/editCampaign.component.tsx
@@ -0,0 +1,18 @@
+import EditCampaignUI from "../../../ui/admin/campaign/editCampaign/editCampaign.ui";
+import { GetFileUrl } from "../../../utils/firebase.util";
+import { modal } from "../../../utils/modal.util";
+
+export default function EditCampaign() {
+
+  async function downloadFile(path: string) {
+    const fileLink = await GetFileUrl(path);
+    if (fileLink === undefined) return modal('error on file download, file link is undefined.');
+    window.open(fileLink, '_blank');
+  }
+  
+  return (
+    <>
+    <EditCampaignUI downloadFile={(path: string) => downloadFile(path)} />
+    </>
+  )
+}
