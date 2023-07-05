838d316b52fd516b2fa592cb5b74b40b460430c4
nan <nan@thedac.info>
2023-07-05 16:31:37 -0500

    Replace environment references by scenario on api-handler

--- server/api-handler/v1/scenarios.api-handler.ts
+++ server/api-handler/v1/scenarios.api-handler.ts
@@ -1,16 +1,16 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {GlobalValues} from "../../../enums/common.enum";
 import {GetInfoDB} from "../../../utils/firebase.util";
-import {ApiResponse, EnvironmentInterface} from "../../../interfaces/api.interface";
+import {ApiResponse, ScenarioInterface} from "../../../interfaces/api.interface";
 import {FirestoreLocation} from "../../../enums/firebase.enum";
 import {RequestResponse} from "../request.api-handler";
 import {DefaultApiResponse} from "../../enums/api.enum";
 
-export async function GetApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<EnvironmentInterface[]>>) {
+export async function GetApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<ScenarioInterface[]>>) {
   const {campaign} = req.query;
   const realCampaign = campaign as string ?? GlobalValues.BaseCampaign;
 
-  const data = await GetInfoDB<EnvironmentInterface>(FirestoreLocation.Environments, realCampaign, {
+  const data = await GetInfoDB<ScenarioInterface>(FirestoreLocation.Scenarios, realCampaign, {
     campaign: realCampaign
   });
 
