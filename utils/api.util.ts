1da3609933597b74608779fac8cf8d2e2db08440
nan <nan@thedac.info>
2023-07-05 16:28:47 -0500

    Replace environment references by scenario on api util

--- utils/api.util.ts
+++ utils/api.util.ts
@@ -2,7 +2,7 @@ import {
   AccessoryInterface,
   AnimationInterface,
   ApiResponse,
-  EnvironmentInterface,
+  ScenarioInterface,
   EnvMapInterface,
   FeatureInterface,
   SingleInterface,
@@ -96,8 +96,8 @@ export async function GetAnimationByCampaignAndName(campaign?: string | null, na
   return GetRequest<AnimationInterface[]>(ApiRoutesV1.Animations, undefined, {campaign, name});
 }
 
-export async function GetEnvironmentListByCampaign(campaign?: string | null) {
-  return GetRequest<EnvironmentInterface[]>(ApiRoutesV1.Environments, undefined, {campaign});
+export async function GetScenarioListByCampaign(campaign?: string | null) {
+  return GetRequest<ScenarioInterface[]>(ApiRoutesV1.Scenarios, undefined, {campaign});
 }
 
 export async function GetEnvMapListByCampaign(campaign: string) {
