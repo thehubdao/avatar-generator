38e00c08089e7accab18299292053fea593c1015
nan <nan@thedac.info>
2023-07-05 16:28:20 -0500

    Replace environment references by scenario on types

--- types/asset.type.ts
+++ types/asset.type.ts
@@ -1,9 +1,9 @@
 import {
   AccessoryInterface,
   AnimationInterface,
-  EnvironmentInterface,
+  ScenarioInterface,
   EnvMapInterface,
   FeatureInterface
 } from "../interfaces/api.interface";
 
-export type AssetType = FeatureInterface | AccessoryInterface | AnimationInterface| EnvironmentInterface | EnvMapInterface;
+export type AssetType = FeatureInterface | AccessoryInterface | AnimationInterface| ScenarioInterface | EnvMapInterface;
