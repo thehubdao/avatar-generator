dbd218e3046e5bcbf462eb35870f4cf20bacf8cf
Andres <andgonzalezcas@unal.edu.co>
2023-07-06 15:51:11 -0500

    Change head title on Account view

--- pages/admin/account/index.tsx
+++ pages/admin/account/index.tsx
@@ -2,9 +2,6 @@ import Head from "next/head";
 import Layout from "../../../ui/admin/admin.layout";
 import AGLoading from "../../../ui/common/ag-loading.component";
 import AdminAccount from "../../../ui/admin/admin.account";
-import { useEffect, useState } from "react";
-import { UserInterface } from "../../../interfaces/firebase.interface";
-import { GetCurrentUserInfo, HandleNotLoggedIn } from "../../../utils/firebase.util";
 
 
 export default function AccountView() {
@@ -12,7 +9,7 @@ export default function AccountView() {
   return (
     <>
       <Head>
-        <title>Admin Dashboard</title>
+        <title>Admin Account</title>
       </Head>
       <Layout>
         <AdminAccount />
