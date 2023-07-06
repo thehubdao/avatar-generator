582a2df189cdbbc842c0b1567b46a267c49c4179
Andres <andgonzalezcas@unal.edu.co>
2023-07-06 15:51:36 -0500

    Change style on account view ui, and delete a console logs

--- ui/admin/admin.account.tsx
+++ ui/admin/admin.account.tsx
@@ -5,22 +5,19 @@ import { UserRol } from "../../enums/common.enum";
 export default function AdminAccount() {
   const userData: AuthStateInterface = useAppSelector(state => state.auth)
 
-  console.log({ userData })
-
   return (
     <>
       <h1 className="font-humane text-9xl text-gray-normal">ACCOUNT:</h1>
 
-
       <div className="mt-10">
-        <p className="font-humane text-6xl text-gray-dark tracking-wide">
-          Name: {userData.userInfo?.name ?? 'User'}
-          {userData.address && <span className="ml-6">#{userData.address}</span>}
+        <p className="font-poppins font-bold text-3xl text-gray-normal mb-4">
+          {userData.userInfo?.name ?? 'User'}
+          {userData.address && <span className="ml-6">({userData.address})</span>}
         </p>
-        {userData.userInfo && <div>
-          <p className="font-humane text-5xl text-gray-normal tracking-wide">Role: {UserRol[userData.userInfo.role ?? 1]}</p>
-          <p className="font-humane text-5xl text-gray-normal tracking-wide">Account: {userData.userInfo.account}</p>
-          <p className="font-humane text-5xl text-gray-normal tracking-wide">Email: {userData.userInfo.email}</p>
+        {userData.userInfo && <div className="text-base text-gray-normal">
+          <p><b>Rol:</b> {UserRol[userData.userInfo.role ?? 1]}</p>
+          <p><b>Account:</b> {userData.userInfo.account}</p>
+          <p><b>Email:</b> {userData.userInfo.email}</p>
         </div>}
       </div>
     </>
