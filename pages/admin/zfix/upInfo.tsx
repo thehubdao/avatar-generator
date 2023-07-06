58e02a74c77d3255c9abab3eb9292151840c26a3
OswaldUnity <oswald@thedac.info>
2023-07-06 11:20:15 -0500

    Update commented the logic in order to avoid unnecessary usage

--- pages/admin/zfix/upInfo.tsx
+++ pages/admin/zfix/upInfo.tsx
@@ -63,7 +63,7 @@ export default function UpInfoPage() {
       if (isNotLogIn)
         return;
       
-      await onInit();
+      // await onInit();
     })().catch(err => console.error(err));
   }, []);
   
