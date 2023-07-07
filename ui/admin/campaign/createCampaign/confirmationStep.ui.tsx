0452f2a424854e7146630ce3dfd3dd645c3a0054
nan <nan@thedac.info>
2023-07-06 19:19:46 -0500

    Change name: endStep UI to confirmationStep

--- ui/admin/campaign/createCampaign/confirmationStep.ui.tsx
+++ ui/admin/campaign/createCampaign/confirmationStep.ui.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 import { useSwiper } from "swiper/react";
 import { IoMdArrowBack } from "react-icons/io";
 
-interface EndStepProps {
+interface ConfirmationStepUIProps {
   ready: boolean;
   handleNextStep: () => void;
 }
 
-export default function EndStep({ ready = false, handleNextStep }: EndStepProps) {
+export default function ConfirmationStepUI({ ready = false, handleNextStep }: ConfirmationStepUIProps) {
   const swiper = useSwiper();
   return (
     <div className="flex flex-col justify-between px-5">
