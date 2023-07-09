46dfb05ef736ee3be37d586bc6368d7d0b65bacc
Andres <andgonzalezcas@unal.edu.co>
2023-07-08 21:34:20 -0500

    Update PageLocaton Enum to add /admin/account/userControl

--- enums/common.enum.ts
+++ enums/common.enum.ts
@@ -38,7 +38,8 @@ export enum PageLocation {
   FirstSteps = '/admin/firstSteps',
   AdminCampaign = '/admin/campaign',
   AssetCreate = '/admin/campaign/createAsset',
-  Account = '/admin/account'
+  Account = '/admin/account',
+  UserControl = '/admin/account/userControl'
 }
 
 export enum AdminComponents {
