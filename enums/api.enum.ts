4c3ed0e22786d52fea0d394beef6e467f725d041
nan <nan@thedac.info>
2023-07-11 12:29:31 -0500

    Fix url for stages API on API enum

--- enums/api.enum.ts
+++ enums/api.enum.ts
@@ -2,7 +2,7 @@
   FeatureOptions = '/api/v1/featureOptions',
   AccessoryOptions = '/api/v1/accessoryOptions',
   Animations = '/api/v1/animations',
-  Stages = '/api/v1/Stages',
+  Stages = '/api/v1/stages',
   AdminCampaigns = 'api/v1/adminCampaigns',
   Single = '/api/v1/single',
   EnvMaps = '/api/v1/envMaps',
