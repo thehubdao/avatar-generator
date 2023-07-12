a6933da67de2f19eed356bb9378c92cc7b1abcf0
nan <nan@thedac.info>
2023-07-12 15:33:40 -0500

    Create new ColorConfig interface and update CampaignConfig on common interfaces

--- interfaces/common.interface.ts
+++ interfaces/common.interface.ts
@@ -29,6 +29,13 @@ export interface ExportInterface {
   model?: Blob;
 }
 
+export interface ColorConfig {
+  materialName?: string;
+  defColor?: string;
+  colorPalette?: string[];
+  usePalette?: boolean;
+}
+
 export interface LookAtVectors {
   lookAt?: AGVector3;
   pos?: AGVector3;
@@ -43,19 +50,21 @@ export interface AGVector3 {
 export interface CampaignConfig {
   clientRequirements?: ClientQuestion[];
   changeMaterial?: ChangeMaterialOption;
-  defStart?: number;
-  defEyesColor?: string;
-  defSkinColor?: string;
-  defSkin?: string;
-  defCam?: LookAtVectors;
+  defAvatarCombination?: number;
   defBg?: string;
   lights?: ConfigLight[],
+  defCam?: LookAtVectors;
   defAnimation?: string;
   defStage?: string;
   defEnvMap?: string;
+  skin?: ColorConfig;
   featuresCamPos?: Record<string, LookAtVectors>;
   accCamPos?: Record<string, LookAtVectors>;
+  featuresSkin?: Record<string, ColorConfig>;
+  accSkin?: Record<string, ColorConfig>;
 }
+
+
 
 /***
  * Update CampaignParameterName enum as well, when changing names on this interface
