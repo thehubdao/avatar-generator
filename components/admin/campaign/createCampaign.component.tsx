73d364b525cf6cc16177a7b8a25b75a119428eb3
nan <nan@thedac.info>
2023-07-12 15:41:05 -0500

    remove unused props and send submitCampaign to UI component on createCampaign component

--- components/admin/campaign/createCampaign.component.tsx
+++ components/admin/campaign/createCampaign.component.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks";
 import { GoToPage } from "../../../utils/router.util";
 import { Module, PageLocation } from "../../../enums/common.enum";
@@ -11,67 +10,60 @@ import { UserInterface } from "../../../interfaces/firebase.interface";
 import { setUserInfo } from "../../../store/authSlice";
 import { ShowModal } from "../../../utils/modal.util";
 
-interface CreateCampaignProps {
-  baseMeshFile?: File | null;
-}
+import CreateCampaignUI from "../../../ui/admin/campaign/createCampaign/createCampaign.ui"
 
-export default function CreateCampaign({ baseMeshFile }: CreateCampaignProps) {
+export default function CreateCampaign() {
   const campaignName = useAppSelector(state => state.addCampaign.name);
   const campaignFeatures = useAppSelector(state => state.addCampaign.features);
-  const campaignReady = useAppSelector(state => state.addCampaign.ready);
+  const campaignConfig = useAppSelector(state => state.addCampaign.config);
   const campaignList = useAppSelector(state => state.auth.userInfo?.campaign);
   const dispatch = useAppDispatch();
 
-  async function submitCampaign() {
-    if (campaignReady) {
-      const userInfo = await GetCurrentUser();
-      if (userInfo == null) {
-        dispatch(reset());
-        return void HandleNotLoggedIn();
-      } else {
-        const baseMeshUploaded = await UploadFile(baseMeshFile, StorageLocation.AvatarBase, undefined, campaignName);
+  async function submitCampaign(avatarBaseFile: File | null) {
+    const userInfo = await GetCurrentUser();
+    if (userInfo == null) {
+      dispatch(reset());
+      return void HandleNotLoggedIn();
+    } else {
+      const baseMeshUploaded = await UploadFile(avatarBaseFile, StorageLocation.AvatarBase, undefined, campaignName);
+      if (baseMeshUploaded == undefined) return LogError(Module.CampaignAdd, "Error uploading armature");
 
-        if (baseMeshUploaded == undefined) return LogError(Module.CampaignAdd, "Error uploading armature");
+      // Make doc campaign object
+      const docData: CampaignParameters = {
+        owner: userInfo.uid,
+        armature: baseMeshUploaded,
+        features: campaignFeatures,
+        accessories: [],
+        config: campaignConfig
+      };
 
-        // Make doc campaign object
-        const docData: CampaignParameters = {
-          owner: userInfo.uid,
-          armature: baseMeshUploaded,
-          features: campaignFeatures,
-          accessories: [],
-        };
+      // Upload information to DB
+      const result = await InsertDocWithId(campaignName, docData, FirestoreGlobalLocation.Campaign);
+      dispatch(reset());
+      void GoToPage(PageLocation.Admin);
 
-        // Upload information to DB
-        const result = await InsertDocWithId(campaignName, docData, FirestoreGlobalLocation.Campaign);
-        dispatch(reset());
-        void GoToPage(PageLocation.Admin);
+      // Update user info
+      const newUserInfo: Partial<UserInterface> = {
+        campaign: campaignList ? [...campaignList, campaignName] : [campaignName]
+      };
 
-        // Update user info
-        const newUserInfo: Partial<UserInterface> = {
-          campaign: campaignList ? [...campaignList, campaignName] : [campaignName]
-        };
+      await UpdateDocObject(FirestoreGlobalLocation.User, newUserInfo, undefined, userInfo.uid);
 
-        await UpdateDocObject(FirestoreGlobalLocation.User, newUserInfo, undefined, userInfo.uid);
-
-        // // Send user to admin dashboard
-        if (result.success) {
-          const uInfo = await GetCurrentUserInfo(true);
-          if(uInfo) {
-            const {role, name, account, email, campaign} = uInfo;
-            void dispatch(setUserInfo({role, name, account, email, campaign}));
-          }
-          ShowModal("Campaign created successfully");
+      // // Send user to admin dashboard
+      if (result.success) {
+        const uInfo = await GetCurrentUserInfo(true);
+        if (uInfo) {
+          const { role, name, account, email, campaign } = uInfo;
+          void dispatch(setUserInfo({ role, name, account, email, campaign }));
         }
-        else
-          void LogError(Module.CampaignAdd, result.errMessage ?? 'Unknow error.');
+        ShowModal("Campaign created successfully");
       }
+      else
+        void LogError(Module.CampaignAdd, result.errMessage ?? 'Unknow error.');
     }
   }
 
-  useEffect(() => {
-    void submitCampaign();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [campaignReady]);
-
-  return (<></>)
+  return (
+    <CreateCampaignUI setAvatarBaseFile={(file: File | null) => submitCampaign(file)} />
+  )
 }
