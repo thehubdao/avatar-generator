f29cfd50c0f1567143ae65be69ead618596447be
nan <nan@thedac.info>
2023-07-13 16:17:28 -0500

    Add icon selector based on featuresIcons font on featuresConfigStep UI

--- ui/admin/campaign/createCampaign/featuresConfigStep.ui.tsx
+++ ui/admin/campaign/createCampaign/featuresConfigStep.ui.tsx
@@ -33,11 +33,11 @@ export default function FeaturesConfigStepUI({ ready = false, featuresCount }: F
   const dispatch = useAppDispatch();
   const [featureList, setFeatureList] = useState<FeatureBasic[]>(features || []);
 
-  function updateStrings(e: ChangeEvent<HTMLInputElement>, key: FeatureStringOption) {
+  function updateStrings(e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>, key: FeatureStringOption) {
     setFeatureList((prevState) => {
       const oldState = [...prevState];
       if (oldState[featureSelected] == undefined)
-        oldState[featureSelected] = {...featureBase};
+        oldState[featureSelected] = { ...featureBase };
 
       oldState[featureSelected] = { ...oldState[featureSelected], [key]: e.target.value.trim() };
       return oldState;
@@ -48,7 +48,7 @@ export default function FeaturesConfigStepUI({ ready = false, featuresCount }: F
     setFeatureList((prevState) => {
       const oldState = [...prevState];
       if (oldState[featureSelected] == undefined)
-        oldState[featureSelected] = {...featureBase};
+        oldState[featureSelected] = { ...featureBase };
 
       oldState[featureSelected] = { ...oldState[featureSelected], [key]: e.target.checked };
       return oldState;
@@ -59,7 +59,7 @@ export default function FeaturesConfigStepUI({ ready = false, featuresCount }: F
     setFeatureList((prevState) => {
       const oldState = [...prevState];
       if (oldState[featureSelected] == undefined)
-        oldState[featureSelected] = {...featureBase};
+        oldState[featureSelected] = { ...featureBase };
 
       oldState[featureSelected] = { ...oldState[featureSelected], [key]: e.target.value };
       return oldState;
@@ -90,7 +90,7 @@ export default function FeaturesConfigStepUI({ ready = false, featuresCount }: F
           <div className="absolute top-2/4 right-4 -translate-y-2/4 pointer-events-none">
             <MdKeyboardArrowDown />
           </div>
-          <select name="" id="" className="bg-bg shadow-flat-medium hover:shadow-flat-hard w-full h-[48px] py-2 px-4 rounded-lg cursor-pointer" value={featureSelected} onChange={e => {
+          <select className="bg-bg shadow-flat-medium hover:shadow-flat-hard w-full h-[48px] py-2 px-4 rounded-lg cursor-pointer" value={featureSelected} onChange={e => {
             setFeatureSelected(parseInt(e.target.value))
           }}>
             {
@@ -122,26 +122,41 @@ export default function FeaturesConfigStepUI({ ready = false, featuresCount }: F
                 onChange={(e) => updateNumbers(e, 'index')} />
             </div>
           </div>
-          <div className="grid grid-cols-2 w-full py-4">
-            <div>
+          <div className="grid grid-cols-2 items-center w-full py-4">
+            {/* TODO add custom icon to features via URL or uploading file */}
+            {/* <div>
               <label htmlFor={'feature-checkbox-1'} className="relative w-6 h-6 shadow-inset-soft bg-bg rounded-lg flex justify-center items-center cursor-pointer">
                 <div className={`w-3 h-3 bg-orange rounded-full ${featureList[featureSelected]?.hasCustomIcon ? '' : 'hidden'}`}></div>
                 <p className="absolute left-[120%] whitespace-nowrap select-none">Custom icon</p>
               </label>
               <input type="checkbox" id={'feature-checkbox-1'} checked={featureList[featureSelected]?.hasCustomIcon || false} onChange={(e) => updateBooleans(e, 'hasCustomIcon')} className="absolute hidden" />
+            </div> */}
+            <div className="flex gap-2 items-center">
+              <p className="whitespace-nowrap">Icon:</p>
+              <select
+                value={featureList[featureSelected]?.iconUrl || 'a'}
+                onChange={e => {
+                  updateStrings(e, 'iconUrl')
+                }}
+                className="font-featuresIcons text-4xl text-center leading-none text-gray-normal bg-bg shadow-flat-medium hover:shadow-flat-hard w-16 h-16 rounded-lg cursor-pointer"
+              >
+                <option value="a">a</option>
+                <option value="b">b</option>
+                <option value="c">c</option>
+                <option value="d">d</option>
+                <option value="e">e</option>
+                <option value="f">f</option>
+                <option value="g">g</option>
+              </select>
             </div>
-            <div>
+            {/* TODO multicolor selection on features */}
+            {/* <div>
               <label htmlFor={'feature-checkbox-2'} className="relative w-6 h-6 shadow-inset-soft bg-bg rounded-lg flex justify-center items-center cursor-pointer">
                 <div className={`w-3 h-3 bg-orange rounded-full ${featureList[featureSelected]?.isMulticolor ? '' : 'hidden'}`}></div>
                 <p className="absolute left-[120%] whitespace-nowrap select-none">Multicolor</p>
               </label>
               <input type="checkbox" id={'feature-checkbox-2'} checked={featureList[featureSelected]?.isMulticolor || false} onChange={(e) => updateBooleans(e, 'isMulticolor')} className="absolute hidden" />
-            </div>
-          </div>
-          <div className="flex items-center gap-2 w-full">
-            <p className="whitespace-nowrap">Icon url:</p>
-            <input type="text" value={featureList[featureSelected]?.iconUrl || ''} className="shadow-inset-soft px-4 py-2 my-2 min-h-[48px] w-full rounded-lg bg-bg"
-              onChange={(e) => updateStrings(e, 'iconUrl')} />
+            </div> */}
           </div>
         </div>
       </div>
