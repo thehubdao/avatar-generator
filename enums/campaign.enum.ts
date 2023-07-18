82e5529678129b6898a391e1cb0ac1c559261c90
nan <nan@thedac.info>
2023-07-18 14:22:28 -0500

    Create CampaignConfigOption for config menu option on Campaign enum

--- enums/campaign.enum.ts
+++ enums/campaign.enum.ts
@@ -3,3 +3,10 @@ export enum ClientQuestion {
     BGColor = 2,
     Gender = 3,
 }
+
+export enum CampaignConfigOption {
+    Base = 'base',
+    Color = 'color',
+    Camera = 'camera',
+    Light = 'light',
+}
