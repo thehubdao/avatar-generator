5e31c600ef00d40b6f6d53f6c0d4f1d49bb2d0ba
nan <nan@thedac.info>
2023-07-20 18:15:44 -0500

    Create header Modal UI

---
+++ ui/admin/common/header/modal.ui.tsx
@@ -0,0 +1,28 @@
+import { IoMdLogOut } from "react-icons/io";
+import AGButton from "../../../common/ag-button.component";
+import { LogOut } from "../../../../utils/firebase.util";
+
+interface ModalUIProps {
+  closeModal: () => void;
+}
+
+export default function ModalUI({closeModal}: ModalUIProps) {
+  return (
+    <div className="bg-gray-dark w-80 p-6 rounded-2xl">
+      <div className="flex items-center pb-4 gap-4">
+        <div className="bg-white rounded-full w-10 h-10 text-2xl flex justify-center items-center">
+          <IoMdLogOut />
+        </div>
+        <p className="text-white">Are you sure you want to<br />log out?</p>
+      </div>
+      <div className="flex w-full justify-end">
+        <AGButton fit onClickEvent={() => closeModal()}>
+          NO
+        </AGButton>
+        <AGButton fit onClickEvent={() => LogOut()}>
+          YES
+        </AGButton>
+      </div>
+    </div>
+  )
+}
