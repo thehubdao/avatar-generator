e0c2a862f2d8df1498b1114db697848b651e38fe
Andres <andgonzalezcas@unal.edu.co>
2023-09-05 14:05:17 -0500

    Update lukso view styles

--- ui/lukso/lukso.ui.tsx
+++ ui/lukso/lukso.ui.tsx
@@ -1,13 +1,13 @@
-import EditLuksoSectionUI from "./sections/editSection";
+import EditLuksoSectionUI from "./sections/editSection.ui";
 import MainLuksoSectionUI from "./sections/mainSection.ui";
 import MintLuksoSectionUI from "./sections/mintSection.ui";
 
 export default function LuksoUI() {
   return (
-    <div className="w-full grow">
-      <MainLuksoSectionUI />
-      <MintLuksoSectionUI />
-      <EditLuksoSectionUI />
+    <div className="w-full grow text-white text-center">
+      <MainLuksoSectionUI hidden/>
+      <MintLuksoSectionUI hidden />
+      <EditLuksoSectionUI  />
     </div>
   )
 }
--- ui/lukso/sections/mintSection.ui.tsx
+++ ui/lukso/sections/mintSection.ui.tsx
@@ -1,9 +1,12 @@
 import { Fragment } from "react"
 import TransparentBox from "../../common/transparentBox.ui"
+import Image from "next/image"
 
-export default function MintLuksoSectionUI() {
+export default function MintLuksoSectionUI({ hidden }: { hidden?: boolean }) {
+  const hiddenClass = hidden ? 'hidden' : 'flex'
+
   return (
-    <section className="h-full flex items-center justify-between hidden">
+    <section className={`${hiddenClass} h-full items-center justify-between`}>
       {/* Mint features */}
       <div className="w-[461px] h-full flex flex-col gap-3">
         <TransparentBox
@@ -13,8 +16,13 @@ export default function MintLuksoSectionUI() {
           heightClass="grow"
           borderSizeClass="border-t-0"
         >
-          <h2>Image</h2>
-          <div className="grid grid-cols-2">
+          <Image
+            src={'/resources/images/campaings/close-avatar-lukso.png'}
+            width={347}
+            height={301}
+            alt="lukso avatar selfie view"
+          />
+          <div className="grid grid-cols-2 mt-20 gap-4 gap-x-14">
             {Array.from({ length: 4 }).map((_, index) => {
               return (<Fragment key={index}>
                 <div>
@@ -28,31 +36,33 @@ export default function MintLuksoSectionUI() {
               </Fragment>)
             })}
           </div>
-          <TransparentBox fullWidth border backgroundColorClass="bg-white" heightClass="h-12" >
-            <p>World Color <span>0x00f</span></p>
+          <TransparentBox fullWidth border backgroundColorClass="bg-white" heightClass="h-12" aditionalClass="mt-10" >
+            <p className="text-black text-xl ">World Color <span>(0x00f)</span></p>
           </TransparentBox>
         </TransparentBox>
       </div>
 
       {/* Buttons */}
-      <div className="flex flex-col w-[240px] h-[70%] justify-end gap-3">
+      <div className="flex flex-col w-[240px] h-[70%] justify-end gap-3 text-black text-xl">
         <TransparentBox fullWidth border backgroundColorClass="bg-white" heightClass="h-12" >
-          <p>Reroll <span>icon</span></p>
+          <p>Reroll <span>(icon)</span></p>
         </TransparentBox>
         <TransparentBox fullWidth border backgroundColorClass="bg-white" heightClass="h-12" >
-          <p>Claim <span>icon</span></p>
+          <p>Claim <span>(icon)</span></p>
         </TransparentBox>
       </div>
 
       {/* Mint your avatar */}
       <div className="w-[564px] h-[70%]">
-        <TransparentBox fullWidth border backgroundColorClass="bg-[#FFCBDE]" >
-          <h3>MINT YOUR AVATAR</h3>
-          <p>Heroes is THE HUB's genesis PFP collection. 5,000 Unique Interoperable Avatars on the Ethereum blockchain.
+        <TransparentBox fullWidth border backgroundColorClass="bg-[#FFCBDE]" paddingClass="px-28" aditionalClass="gap-8">
+          <h3 className="font-semibold text-2xl mb-20">MINT YOUR AVATAR</h3>
+          <p className="text-lg">Heroes is THE HUB's genesis PFP collection. 5,000 Unique Interoperable Avatars on the Ethereum blockchain.
             Heroes is THE HUB's genesis PFP collection. 5,000 Unique Interoperable Avatars on the Ethereum blockchain.</p>
-          <div>Social media section</div>
-          <p>PUBLIC MINT</p>
-          <p>0.03 ETH</p>
+          <div>(Social media section)</div>
+          <div className="font-semibold">
+            <p>PUBLIC MINT</p>
+            <p>0.03 ETH</p>
+          </div>
         </TransparentBox>
       </div>
 
