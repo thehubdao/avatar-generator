677a3b4b31ac139a620937a75128ba3a648d8959
Andres <andgonzalezcas@unal.edu.co>
2023-09-07 14:56:23 -0500

    Create a mobile control layout

--- layouts/mobile.layout.tsx
+++ layouts/mobile.layout.tsx
@@ -3,10 +3,12 @@ import store from "../store/store";
 import MobileBuildAlert from "../ui/admin/common/mobileBuildAlert.ui";
 
 export default function MobileLayout({ children }: { children: JSX.Element | JSX.Element[] | boolean }) {
-  <Provider store={store}>
-    <main className="max-w-screen-xl min-h-screen m-auto py-[88px] px-8 bg-bg">
-      <div className="hidden xl:block">{children}</div>
-      <div className="block xl:hidden"><MobileBuildAlert /></div>
-    </main>
-  </Provider>
+  return (
+    <Provider store={store}>
+      <main className="max-w-screen-xl min-h-screen m-auto py-[88px] px-8 bg-bg">
+        <div className="hidden xl:block">{children}</div>
+        <div className="block xl:hidden"><MobileBuildAlert /></div>
+      </main>
+    </Provider>
+  )
 }
