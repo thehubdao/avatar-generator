15022520d069f717a24a017821d1b4f69fe90d31
Andres <andgonzalezcas@unal.edu.co>
2023-09-07 11:05:37 -0500

    Update animation and hidden function on lukso sections, and shaders

--- ui/lukso/sections/mintSection.ui.tsx
+++ ui/lukso/sections/mintSection.ui.tsx
@@ -1,14 +1,38 @@
-import { Fragment } from "react"
-import TransparentBox from "../../common/transparentBox.ui"
 import Image from "next/image"
+import { Fragment, useEffect, useRef } from "react"
+import TransparentBox from "../../common/transparentBox.ui"
+import { moveHorizontalBlocks, moveVerticalBlocks } from "../../../utils/gsap/block_in_out";
 
-export default function MintLuksoSectionUI({ hidden }: { hidden?: boolean }) {
-  const hiddenClass = hidden ? 'hidden' : 'flex'
+interface MintLuksoSectionUIProps {
+  setCurrentSection: React.Dispatch<React.SetStateAction<number>>
+}
+
+export default function MintLuksoSectionUI({ setCurrentSection }: MintLuksoSectionUIProps) {
+  const mainFeatureRef = useRef<HTMLDivElement>(null);
+  const mintAvatarRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLDivElement>(null);
+  const ANIMATION_DURATION = 0.5;
+
+  const gsapEnterBlocks = () => {
+    if (!mainFeatureRef.current || !mintAvatarRef.current || !buttonRef.current) return
+    moveHorizontalBlocks(mainFeatureRef.current, ANIMATION_DURATION, 'left', 'in');
+    moveHorizontalBlocks(mintAvatarRef.current, ANIMATION_DURATION, 'right', 'in');
+    moveVerticalBlocks(buttonRef.current, ANIMATION_DURATION, 'bottom', 'in');
+  }
+
+  const gsapOutBlocks = () => {
+    if (!mainFeatureRef.current || !mintAvatarRef.current || !buttonRef.current) return
+    moveHorizontalBlocks(mainFeatureRef.current, ANIMATION_DURATION, 'left', 'out');
+    moveHorizontalBlocks(mintAvatarRef.current, ANIMATION_DURATION, 'right', 'out', () => setCurrentSection(2));
+    moveVerticalBlocks(buttonRef.current, ANIMATION_DURATION, 'bottom', 'out');
+  }
+
+  useEffect(() => { gsapEnterBlocks(); }, [])
 
   return (
-    <section className={`${hiddenClass} h-full items-center justify-between`}>
+    <section className={`flex h-full items-center justify-between`}>
       {/* Mint features */}
-      <div className="w-[461px] h-full flex flex-col gap-3">
+      <div className="w-[461px] h-full flex flex-col gap-3" ref={mainFeatureRef}>
         <TransparentBox
           fullWidth
           border
@@ -43,17 +67,19 @@ export default function MintLuksoSectionUI({ hidden }: { hidden?: boolean }) {
       </div>
 
       {/* Buttons */}
-      <div className="flex flex-col w-[240px] h-[70%] justify-end gap-3 text-black text-xl">
+      <div className="flex flex-col w-[240px] h-[70%] justify-end gap-3 text-black text-xl" ref={buttonRef}>
         <TransparentBox fullWidth border backgroundColorClass="bg-white" heightClass="h-12" >
           <p>Reroll <span>(icon)</span></p>
         </TransparentBox>
-        <TransparentBox fullWidth border backgroundColorClass="bg-white" heightClass="h-12" >
-          <p>Claim <span>(icon)</span></p>
-        </TransparentBox>
+        <button className="w-full h-fit" onClick={() => gsapOutBlocks()}>
+          <TransparentBox fullWidth border backgroundColorClass="bg-white" heightClass="h-12" >
+            <p>Claim <span>(icon)</span></p>
+          </TransparentBox>
+        </button>
       </div>
 
       {/* Mint your avatar */}
-      <div className="w-[564px] h-[70%]">
+      <div className="w-[564px] h-[70%]" ref={mintAvatarRef}>
         <TransparentBox fullWidth border backgroundColorClass="bg-[#FFCBDE]" paddingClass="px-28" aditionalClass="gap-8">
           <h3 className="font-semibold text-2xl mb-20">MINT YOUR AVATAR</h3>
           <p className="text-lg">Heroes is THE HUB's genesis PFP collection. 5,000 Unique Interoperable Avatars on the Ethereum blockchain.
