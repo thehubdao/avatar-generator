013cff4980fa462664f47016958d4a3b8c7f77e4
nan <nan@thedac.info>
2023-09-28 18:47:21 -0500

    Create GeneratedShadowLight function on light util
    - GeneratedShadowLight util allows to create a no-lighting light to generate a dynamic shadow

--- utils/threejs/light.util.ts
+++ utils/threejs/light.util.ts
@@ -1,13 +1,14 @@
-﻿import {AmbientLight, Light, PointLight, RectAreaLight} from "three";
-import {ConfigLight} from "../../interfaces/light.interface";
-import {LightType} from "../../enums/light.enum";
-import {ColorStringToHexString} from "../common.util";
+﻿import { AmbientLight, Light, PointLight, RectAreaLight } from "three";
+import { ConfigLight } from "../../interfaces/light.interface";
+import { LightType } from "../../enums/light.enum";
+import { ColorStringToHexString } from "../common.util";
+import { AGVector3 } from "../../interfaces/common.interface";
 
 type GenerateLightFunction = (light: ConfigLight) => Light;
 
 function DefaultLights(): Light[] {
   const ambient = new AmbientLight( 0x404040 );
-  
+
   const pointOne = new PointLight( 0xffffff, 1, 0 );
   pointOne.position.set(1, 1, 10);
   
@@ -16,14 +17,14 @@ function DefaultLights(): Light[] {
 
 export function GetLights(toDoLights: ConfigLight[] | undefined) {
   if (toDoLights == undefined) return DefaultLights();
-  
+
   const newLights: Light[] = [];
   for (const light of toDoLights) {
     const generatedLight = GenerateLight(light);
     if (generatedLight != undefined)
       newLights.push(generatedLight);
   }
-  
+
   return newLights;
 }
 
@@ -43,7 +44,7 @@ function GetGenerateLightFunction(type: LightType): GenerateLightFunction | unde
 function GenerateLight(light: ConfigLight) {
   const generateLightFunction = GetGenerateLightFunction(light.type);
   if (generateLightFunction == undefined) return undefined;
-  
+
   return generateLightFunction(light);
 }
 
@@ -55,11 +56,11 @@ function GeneratePointLight(light: ConfigLight) {
     decay,
     pos,
   } = light.params;
-  
+
   const newLight = new PointLight(ColorStringToHexString(color), intst, dist, decay);
   if (pos != undefined)
     newLight.position.set(pos.x, pos.y, pos.z);
-  
+
   return newLight;
 }
 
@@ -68,7 +69,7 @@ function GenerateAmbientLight(light: ConfigLight) {
     color,
     intst,
   } = light.params;
-  
+
   const newLight = new AmbientLight(ColorStringToHexString(color), intst);
   return newLight;
 }
@@ -82,10 +83,22 @@ function GenerateRectAreaLight(light: ConfigLight) {
     pos,
     lAt,
   } = light.params;
-  
+
   const newLight = new RectAreaLight(ColorStringToHexString(color), intst, width, height);
   if (pos != undefined) newLight.position.set(pos.x, pos.y, pos.z);
   if (lAt != undefined) newLight.lookAt(lAt.x, lAt.y, lAt.z);
-  
+
   return newLight;
+}
+
+export function GenerateShadowLight(lightPos: AGVector3) {
+  const shadowLight = new PointLight(0xffffff, 0, 0);
+  shadowLight.position.set(lightPos.x, lightPos.y, lightPos.z);
+  shadowLight.castShadow = true;
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+  shadowLight.shadow.radius = 3;
+  shadowLight.shadow.blurSamples = 25;
+  shadowLight.shadow.bias = 0.0001;
+  return shadowLight;
 }
