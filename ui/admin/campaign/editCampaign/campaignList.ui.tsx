c3a251854dc400f8604665c5240728675307c412
Andres <andgonzalezcas@unal.edu.co>
2023-09-29 12:56:51 -0500

    solve ESLINT build command errors

--- ui/admin/campaign/editCampaign/campaignList.ui.tsx
+++ ui/admin/campaign/editCampaign/campaignList.ui.tsx
@@ -34,7 +34,7 @@ export default function CampaignListUI({ deleteCampaign }: CampaignListUIProps) 
                   dispatch(setName(x));
                   void GoToPage(PageLocation.AdminCampaign);
                 }}
-                deleteCampaign={() => { deleteCampaign(x); }}
+                deleteCampaign={() => { void deleteCampaign(x); }}
               />
             )
           })
