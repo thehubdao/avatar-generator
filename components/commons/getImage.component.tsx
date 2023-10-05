209138a116c8a4aa953503eef8b236a65f57e2b0
nan <nan@thedac.info>
2023-10-05 18:52:17 -0500

    Update GetImage component
    - Create getData async function
    - Update imports
    - Add sizes to Image filled component

--- components/commons/getImage.component.tsx
+++ components/commons/getImage.component.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import GetImageUI from "../../ui/common/getImage.ui";
 import { GetFileUrl } from "../../utils/firebase.util";
+import { Module } from "../../enums/common.enum";
+import { LogError } from "../../utils/common.util";
 
 interface GetImageProps {
   url?: string;
@@ -10,11 +12,16 @@ interface GetImageProps {
 export default function GetImage({ url, alt }: GetImageProps) {
   const [imageUrl, setImageUrl] = useState<string | undefined>(undefined);
 
+  const getData = async () => {
+    const result = await GetFileUrl(url);
+    if (!result.success)
+      void LogError(Module.OptionSelector, `${result.errCode}: ${result.errMessage}`)
+
+    setImageUrl(result.success ? result.value : undefined);
+  }
+
   useEffect(() => {
-    (async () => {
-      const newImage = await GetFileUrl(url);
-      setImageUrl(newImage ?? undefined);
-    })().catch(err => console.error(err));
+    getData().catch(console.error);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [url])
 
