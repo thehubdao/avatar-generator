0fb2b2e384fda756c10a47b983fea1d6054ff17b
nan <nan@thedac.info>
2023-10-05 18:48:02 -0500

    Create getTexture util & Update getTextureFromFile util from texture utils

--- utils/threejs/texture.util.ts
+++ utils/threejs/texture.util.ts
@@ -1,8 +1,9 @@
 import {NearestFilter, Texture, TextureLoader} from "three";
 import {IsWebUrl, LogError} from "../common.util";
 import {GetFileUrl} from "../firebase.util";
-import {Module} from "../../enums/common.enum";
+import {CommonErrorCode, Module} from "../../enums/common.enum";
 import {TextureTone} from "../../types/texture.type";
+import { Result } from "../../types/common.type";
 
 class TextureUtil {
   private static _instance: TextureUtil;
@@ -41,17 +42,57 @@ class TextureUtil {
     
     return toneTexture;
   }
+
+  public async GetTexture(url: string): Promise<Result<Texture>> {
+    try {
+      const newTexture = await TextureUtil.Instance().TextureLoader().loadAsync(url);
+      return {success: true, value: newTexture};
+    }
+    catch(e) {
+      const msg = `Couldn't make a texture for url: ${url}!`;
+      void LogError(Module.TextureUtil, msg, e);
+      return {success: false, errMessage: msg, errCode: CommonErrorCode.CouldntProcess};
+    }
+  }
+
+  public async GetTextureMemory(url: string): Promise<Result<Texture>> {
+    if (this._textureDb && this._textureDb[url])
+      return {success: true, value: this._textureDb[url].clone()};
+
+    if (this._textureDb === undefined) this._textureDb = {};
+
+    try {
+      const newTexture = await TextureUtil.Instance().TextureLoader().loadAsync(url);
+
+      this._textureDb[url] = newTexture.clone();
+      return {success: true, value: newTexture};
+    }
+    catch(e) {
+      const msg = `Couldn't make a texture for url: ${url}!`;
+      void LogError(Module.TextureUtil, msg, e);
+      return {success: false, errMessage: msg, errCode: CommonErrorCode.CouldntProcess};
+    }
+  }
 }
 
 export async function GetToneTexture(tone: TextureTone = 'threeTone' ) {
   return TextureUtil.Instance().GetToneTexture(tone);
 }
 
-export async function GetTextureFromFile(url: string | undefined) {
-  let realUrl: string | undefined = url;
-  if (url != undefined && !IsWebUrl(url)) realUrl = await GetFileUrl(url);
-  
-  if (realUrl == undefined) return void LogError(Module.TextureUtil, `Couldn't get texture from url: ${url ?? ''}`);
-  
-  return TextureUtil.Instance().TextureLoader().loadAsync(realUrl);
+export async function GetTextureFromFile(url: string | undefined): Promise<Result<Texture>> {
+  if (url == undefined) {
+    const msg =  "Missing path to get URL!";
+    void LogError(Module.TextureUtil, msg);
+    return {success: false, errMessage: msg, errCode: CommonErrorCode.MissingInfo};
+  }
+
+  let realUrl = url;
+
+  if (!IsWebUrl(url)) {
+    const result = await GetFileUrl(url);
+    if (result.success)
+      realUrl = result.value;
+  }
+
+  return TextureUtil.Instance().GetTexture(realUrl);
 }
