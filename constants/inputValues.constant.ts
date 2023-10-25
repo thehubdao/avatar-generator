6c82ca04b1d60bd7430e9aa3944e70bebeed8258
Andres <andgonzalezcas@unal.edu.co>
2023-10-25 10:36:42 -0500

    add max file size constant to use on input type file.

---
+++ constants/inputValues.constant.ts
@@ -0,0 +1,14 @@
+
+export const Kilobytes = 1024
+export const Megabytes = Math.pow(Kilobytes, 2);
+
+export const MAXIMUM_FILE_SIZE = {
+  model: {
+    sizeOnMb: 10,
+    scale: 'Mb',
+  },
+  image: {
+    sizeOnMb: 2,
+    scale: 'Mb',
+  },
+}
