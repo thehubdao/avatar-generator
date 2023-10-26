02640d448c8578abb59995a9da83a841ebf04398
Andres <andgonzalezcas@unal.edu.co>
2023-10-26 13:59:17 -0500

    add max number of lights constants on constants lights

--- constants/lightType.constant.ts
+++ constants/lightType.constant.ts
@@ -1,11 +1,14 @@
 import { LightType } from "../enums/light.enum";
 
+
+// TODO: TO ENUM
 export const LIGHT_TYPE_LABELS: Record<LightType, string> = {
   [LightType.PointLight]: 'Point Light',
   [LightType.AmbientLight]: 'Ambient Light',
   [LightType.RectAreaLight]: 'Rect Area Light',
 };
 
+// TODO: TO INTERFACE
 export const LIGHT_PROPERTIES = {
   [LightType.PointLight]: ['position', 'lookAt', 'color', 'intensity', 'distance', 'decay'],
   [LightType.RectAreaLight]: ['position', 'lookAt', 'color', 'intensity', 'size'],
@@ -17,3 +20,7 @@ export const INDEX_OUT_LIGHT_ARRAY = {
   create_light: -2,
   no_lights: -1
 }
+
+export const MAX_NUMBER_OF_POINT_LIGHTS = 5;
+export const MAX_NUMBER_OF_RECT_AREA_LIGHT = 2;
+export const MAX_NUMBER_OF_AMBIENT_LIGHT = 1;
