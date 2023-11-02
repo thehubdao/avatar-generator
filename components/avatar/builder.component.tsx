c8cef9f7c0ae44648cacdfb4b8b80b5fe785576d
Andres <andgonzalezcas@unal.edu.co>
2023-11-02 17:25:11 -0500

    Change files to .component to .ui on ui folder, and start with mobile refactor.

--- components/avatar/builder.component.tsx
+++ components/avatar/builder.component.tsx
@@ -29,7 +29,7 @@ import {
 } from "../../utils/api.util";
 import { SaveFile } from "../../utils/exporter.util";
 import AGLoading from "../../ui/common/ag-loading.component";
-import HudComponent from "../../ui/avatar/hud.component";
+import HudComponent from "../../ui/avatar/hud.ui";
 import AvatarEditor, {
   ChangeAccessory,
   ChangeFeature, ChangeSkinColor,
