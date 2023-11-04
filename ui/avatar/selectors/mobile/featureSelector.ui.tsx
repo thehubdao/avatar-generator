235484e2121c802518b38fffe7a79429e1bd5803
Andres <andgonzalezcas@unal.edu.co>
2023-11-03 19:20:21 -0500

    Update mobile ui select feature and options on viewer view.

--- ui/avatar/selectors/mobile/featureSelector.ui.tsx
+++ ui/avatar/selectors/mobile/featureSelector.ui.tsx
@@ -1,52 +1,44 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { MouseEvent } from "react";
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
+import { useRef } from "react";
 import 'swiper/css';
 import { FeatureBasic } from '../../../../interfaces/common.interface';
+import FeatureOptionList from './featureOptionList.ui';
 
-interface Props {
+interface FeatureSelectorComponentProps {
   list: FeatureBasic[];
   activeOpc: string;
   handleClick: (id: string) => void;
 }
 
-function OptionList({ list, activeOpc, handleClick }: Props) {
+export default function FeatureSelectorComponent(props: FeatureSelectorComponentProps) {
+  const swiperRef = useRef<SwiperRef>(null);
 
-  function selectFeature(e: MouseEvent, id: string) {
-    e.preventDefault();
-    handleClick(id);
-  }
-
-  return list?.map((opt: FeatureBasic, index: number) => {
-    return (
-      <SwiperSlide className='flex flex-col items-center justify-center' key={index} onClick={(event: MouseEvent) => selectFeature(event, opt.displayName)}>
-        <p className={'text-xs text-center p-2 opacity-50' + (activeOpc == opt.displayName ? ' opacity-90 text-slate-700 border-b-[1px] border-slate-700' : '')}>
-          {opt.displayName.toUpperCase()}
-        </p>
-      </SwiperSlide>
-    )
-  });
-}
-
-export default function FeatureSelectorComponent(props: Props) {
-  const itemsLength = props.list?.length ? props.list.length : 1;
   const itemsPerView = 4;
+
+  const handleSlide = (id: string, index: number) => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideToLoop(index, 1000);
+    }
+
+    props.handleClick(id);
+  };
+
   return (
     <div className='w-full relative'>
       <Swiper
         slidesPerView={itemsPerView}
         centeredSlides={true}
-        autoFocus
         grabCursor={true}
-        loop={itemsLength < itemsPerView ? false : true} //! error with this prop on swipper lib (loop)
+        // loop={itemsLength < itemsPerView ? false : true} //! error with this prop on swipper lib (loop)
         // onSlideChange={() => console.log('slide change')}
-        // onSwiper={(swiper) => console.log(swiper)}
         className='!pt-1'
+        ref={swiperRef}
       >
-        {OptionList(props)}
+        {FeatureOptionList({ ...props, handleClick: handleSlide })}
       </Swiper>
       <div className='absolute w-full px-3' >
         <div className='w-full border-b-[1px] border-slate-300' />
       </div>
-    </div>
+    </div >
   )
-}
+} 
