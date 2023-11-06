3729ca7cd4e19b4ee6b681bc41167d5ee1094489
Andres <andgonzalezcas@unal.edu.co>
2023-11-06 17:07:53 -0500

    Update Lukso ui with new small componets new type of props

--- ui/lukso/lukso.ui.tsx
+++ ui/lukso/lukso.ui.tsx
@@ -4,21 +4,22 @@ import MintLuksoSectionUI from "./sections/mintSection.ui";
 import LuksoLoadingUI from "./sections/loadingSection.ui";
 
 interface LuksoUIProps {
-  reRoll: () => Promise<void>;
-  setIsEditModeSelected: React.Dispatch<React.SetStateAction<boolean>>;
+  reRoll: () => void | Promise<void>;
+  setIsEditModeSelected: (value: boolean) => void;
   isLoading: boolean;
   currentSection: number;
-  setCurrentSection: React.Dispatch<React.SetStateAction<number>>;
+  setCurrentSection: (value: number) => void;
   getloaderDivElement: (elementReference: HTMLDivElement) => void;
+  exportModel: () => void;
 }
 
-export default function LuksoUI({ reRoll, setIsEditModeSelected, isLoading, currentSection, setCurrentSection, getloaderDivElement }: LuksoUIProps) {
+export default function LuksoUI({ reRoll, setIsEditModeSelected, isLoading, currentSection, setCurrentSection, getloaderDivElement, exportModel }: LuksoUIProps) {
   return (
     <div className="w-full grow text-white text-center">
       <LuksoLoadingUI loading={isLoading} getloaderDivElement={getloaderDivElement} />
-      {(currentSection === 0) && <MainLuksoSectionUI setCurrentSection={setCurrentSection} />}
-      {(currentSection === 1) && <MintLuksoSectionUI setCurrentSection={setCurrentSection} reRoll={reRoll} />}
-      {(currentSection === 2) && <EditLuksoSectionUI setIsEditModeSelected={setIsEditModeSelected} />}
+      {(currentSection === 0) && <MainLuksoSectionUI setCurrentSection={(newSection: number) => setCurrentSection(newSection)} />}
+      {(currentSection === 1) && <MintLuksoSectionUI setCurrentSection={(newSection: number) => setCurrentSection(newSection)} reRoll={reRoll} />}
+      {(currentSection === 2) && <EditLuksoSectionUI setIsEditModeSelected={setIsEditModeSelected} exportModel={exportModel}/>}
     </div>
   )
 }
