d6e78bfc504b7d3a2a88c774112f69e476ccbb47
Andres <andgonzalezcas@unal.edu.co>
2023-11-07 20:01:15 -0500

    Improve readibility on lukso ui and added luksoSection enum.

--- ui/lukso/lukso.ui.tsx
+++ ui/lukso/lukso.ui.tsx
@@ -1,25 +1,34 @@
-import EditLuksoSectionUI from "./sections/editSection.ui";
-import MainLuksoSectionUI from "./sections/mainSection.ui";
-import MintLuksoSectionUI from "./sections/mintSection.ui";
-import LuksoLoadingUI from "./sections/loadingSection.ui";
+import EditSectionUI from "./sections/editSection.ui";
+import MainSectionUI from "./sections/mainSection.ui";
+import MintSectionUI from "./sections/mintSection.ui";
+import LoadingUI from "./sections/loadingSection.ui";
+import { LuksoSections } from "../../enums/lukso/common.enum";
 
 interface LuksoUIProps {
-  reRoll: () => void | Promise<void>;
-  setIsEditModeSelected: (value: boolean) => void;
   isLoading: boolean;
-  currentSection: number;
-  setCurrentSection: (value: number) => void;
+  currentSection: LuksoSections;
+  reRoll: () => Promise<void>;
+  exportModel: () => Promise<void>;
+  setIsEditModeSelected: (value: boolean) => void;
+  setCurrentSection: (value: LuksoSections) => void;
   getloaderDivElement: (elementReference: HTMLDivElement) => void;
-  exportModel: () => void | Promise<void>;
 }
 
-export default function LuksoUI({ reRoll, setIsEditModeSelected, isLoading, currentSection, setCurrentSection, getloaderDivElement, exportModel }: LuksoUIProps) {
+export default function LuksoUI({
+  reRoll,
+  setIsEditModeSelected,
+  isLoading,
+  currentSection,
+  setCurrentSection,
+  getloaderDivElement,
+  exportModel
+}: LuksoUIProps) {
   return (
     <div className="w-full grow text-white text-center">
-      <LuksoLoadingUI loading={isLoading} getloaderDivElement={getloaderDivElement} />
-      {(currentSection === 0) && <MainLuksoSectionUI setCurrentSection={(newSection: number) => setCurrentSection(newSection)} />}
-      {(currentSection === 1) && <MintLuksoSectionUI setCurrentSection={(newSection: number) => setCurrentSection(newSection)} reRoll={reRoll} />}
-      {(currentSection === 2) && <EditLuksoSectionUI setIsEditModeSelected={setIsEditModeSelected} exportModel={exportModel}/>}
+      {isLoading && <LoadingUI getloaderDivElement={getloaderDivElement} />}
+      {(currentSection === LuksoSections.Main) && <MainSectionUI setCurrentSection={(newSection) => setCurrentSection(newSection)} />}
+      {(currentSection === LuksoSections.Mint) && <MintSectionUI setCurrentSection={(newSection) => setCurrentSection(newSection)} reRoll={reRoll} />}
+      {(currentSection === LuksoSections.Edit) && <EditSectionUI setIsEditModeSelected={setIsEditModeSelected} exportModel={exportModel} />}
     </div>
   )
 }
