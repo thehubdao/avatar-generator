d1e257a7d58f34465b1f9cd30830b7cdebb8a448
Andres <andgonzalezcas@unal.edu.co>
2023-11-07 19:55:56 -0500

    Improved lukso loading section component
    - Change social button component import to UI component
    - Add log errors behavior
    - createShader function created and use on useEffect.

--- ui/lukso/sections/loadingSection.ui.tsx
+++ ui/lukso/sections/loadingSection.ui.tsx
@@ -1,31 +1,45 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 
 // Fragment shader
 import { fragmentShaderSource } from '../shader/background.shader'
-import SocialMediaButtonsLukso from "../common/socialMediaButtons.ui";
+import SocialButtonsUI from "../common/socialButtons.ui";
+import { LogError } from "../../../utils/common.util";
+import { Module } from "../../../enums/common.enum";
 
-interface AGLoadingProps {
-  loading: boolean;
+interface LoadingUIProps {
   getloaderDivElement: (elementReference: HTMLDivElement) => void;
 }
 
-export default function LuksoLoadingUI({ loading, getloaderDivElement }: AGLoadingProps) {
+export default function LoadingUI({ getloaderDivElement }: LoadingUIProps) {
   const parentRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const memoizedGetLoaderDivElement = useCallback(getloaderDivElement, [getloaderDivElement]);
+  const compileShader = (gl: WebGLRenderingContext, source: string, type: number) => {
+    const shader = gl.createShader(type);
 
-  useEffect(() => {
+    if (!shader) return LogError(Module.Lukso, 'Shader doesn\'t exist on compile shader function');
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      LogError(Module.Lukso, 'Error on compile shader:', gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+      return null;
+    }
+
+    return shader;
+  };
+
+  const createShader = () => {
     if (parentRef.current) getloaderDivElement(parentRef.current);
-    if (!canvasRef.current) return;
+    if (!canvasRef.current) return LogError(Module.Lukso, 'Canvas doesn\'t exist on create shader function');
 
     const canvas = canvasRef.current;
     const gl = canvas.getContext('webgl');
 
     if (!gl) {
-      console.error('WebGL is not support on your browser');
-      return;
+      return LogError(Module.Lukso, 'WebGL is not support on your browser on create shader function');
     }
 
     // Vertex shader (optional)
@@ -41,7 +55,8 @@ export default function LuksoLoadingUI({ loading, getloaderDivElement }: AGLoadi
 
     const shaderProgram = gl.createProgram();
 
-    if (!shaderProgram || !vertexShader || !fragmentShader) return;
+    if (!shaderProgram || !vertexShader || !fragmentShader)
+      return LogError(Module.Lukso, 'Shader Program | Vertex Shader | Fragment Shader doesn\'t exist on create shader function');
 
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
@@ -70,61 +85,46 @@ export default function LuksoLoadingUI({ loading, getloaderDivElement }: AGLoadi
     };
 
     requestAnimationFrame(render);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [memoizedGetLoaderDivElement]);
+  }
 
-  const compileShader = (gl: WebGLRenderingContext, source: string, type: number) => {
-    const shader = gl.createShader(type);
-
-    if (!shader) return;
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      console.error('Error on compile shader:', gl.getShaderInfoLog(shader));
-      gl.deleteShader(shader);
-      return null;
-    }
-
-    return shader;
-  };
+  useEffect(() => {
+    createShader();
+  }, []);
 
   return (
-    <>{loading
-      ? <div className={'fixed z-50 top-0 left-0 w-screen h-screen flex justify-center items-center bg-[#FABCE2]'} ref={parentRef}>
-        <canvas ref={canvasRef} className="w-full h-screen fixed top-0 left-0"></canvas>
-        <div className="w-full h-screen flex flex-col justify-center items-center">
-          <div className="fixed top-0 w-full h-14 flex items-center px-4 justify-between">
-            <Image
-              src='/resources/icons/campaigns/lukso.svg'
-              width={106}
-              height={24}
-              alt="Lukso icon"
-            />
-            <SocialMediaButtonsLukso />
-          </div>
-          <div className="fixed">
-            <Image
-              src='/resources/icons/campaigns/lukso.svg'
-              width={596}
-              height={138}
-              alt="Lukso icon"
-            />
-            <div className="w-[596px]">
-              <p className="text-4xl mt-7 tracking-[1.21em] text-center">AVATAR HU<span className="tracking-[0em]">B</span></p>
-            </div>
-          </div>
-          <div className="fixed bottom-0 right-0 m-5">
-            <p className="text-xl tracking-[0.21em]">Powered b<span className="tracking-[0em]">y</span></p>
-            <Image
-              src='/resources/images/the-hub-logo-white.svg'
-              alt="the hub icon"
-              width={182}
-              height={32}
-            />
+    <div ref={parentRef} className={'fixed z-50 top-0 left-0 w-screen h-screen flex justify-center items-center bg-[#FABCE2]'}>
+      <canvas ref={canvasRef} className="w-full h-screen fixed top-0 left-0"></canvas>
+      <div className="w-full h-screen flex flex-col justify-center items-center">
+        <div className="fixed top-0 w-full h-14 flex items-center px-4 justify-between">
+          <Image
+            src='/resources/icons/campaigns/lukso.svg'
+            width={106}
+            height={24}
+            alt="Lukso icon"
+          />
+          <SocialButtonsUI />
+        </div>
+        <div className="fixed">
+          <Image
+            src='/resources/icons/campaigns/lukso.svg'
+            width={596}
+            height={138}
+            alt="Lukso icon"
+          />
+          <div className="w-[596px]">
+            <p className="text-4xl mt-7 tracking-[1.21em] text-center">AVATAR HU<span className="tracking-[0em]">B</span></p>
           </div>
         </div>
-      </div> : ''
-    }</>
+        <div className="fixed bottom-0 right-0 m-5">
+          <p className="text-xl tracking-[0.21em]">Powered b<span className="tracking-[0em]">y</span></p>
+          <Image
+            src='/resources/images/the-hub-logo-white.svg'
+            alt="the hub icon"
+            width={182}
+            height={32}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
