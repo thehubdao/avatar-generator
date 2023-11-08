2e9526a8beeb8bb7bed3849c7c0582c75e664432
Andres <andgonzalezcas@unal.edu.co>
2023-11-07 19:34:58 -0500

    Created common lukso enums with sections and social links.

---
+++ enums/lukso/common.enum.ts
@@ -0,0 +1,12 @@
+export enum LuksoSections {
+  Loading,
+  Main,
+  Mint,
+  Edit
+}
+
+export enum LuksoSocialLinks {
+  Instagram = 'https://www.instagram.com/lukso/',
+  Twitter = 'https://twitter.com/lukso_io',
+  Discord = 'https://discord.com/invite/lukso'
+}
