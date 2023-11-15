657df9fb503cb5150e42a9d276692fb8dda78d3e
OswaldUnity <oswald@thedac.info>
2023-11-15 16:17:08 -0500

    Added vrm export types, interfaces, constant & enum

---
+++ constants/export.constant.ts
@@ -0,0 +1,363 @@
+﻿import {VrmMetadata, VrmStructure} from "../interfaces/export.interface";
+
+export const VRM_SPEC_VERSION = [ "0.0" ] as const;
+
+export const VRM_META_AUTHOR = [
+  "OnlyAuthor",
+  "ExplicitlyLicensedPerson",
+  "Everyone"
+] as const;
+
+export const VRM_META_ALLOW = [
+  "Disallow",
+  "Allow"
+] as const;
+
+export const VRM_META_LICENSE = [
+  "Redistribution_Prohibited",
+  "CC0",
+  "CC_BY",
+  "CC_BY_NC",
+  "CC_BY_SA",
+  "CC_BY_NC_SA",
+  "CC_BY_ND",
+  "CC_BY_NC_ND",
+  "Other"
+] as const;
+
+export const VRM_HUMANOID_BONES = [
+  "hips",
+  "spine",
+  "chest",
+  "neck",
+  "head",
+  "leftUpperLeg",
+  "leftLowerLeg",
+  "leftFoot",
+  "rightUpperLeg",
+  "rightLowerLeg",
+  "rightFoot",
+  "leftUpperArm",
+  "leftLowerArm",
+  "leftHand",
+  "rightUpperArm",
+  "rightLowerArm",
+  "rightHand"
+] as const;
+
+// ^ obligatory list of bones for vrm0
+// Whole list of bones for vrm0
+// "rightUpperLeg","leftLowerLeg","rightLowerLeg","leftFoot","rightFoot","spine","chest","neck","head","leftShoulder","rightShoulder","leftUpperArm","rightUpperArm","leftLowerArm","rightLowerArm","leftHand","rightHand","leftToes","rightToes","leftEye","rightEye","jaw","leftThumbProximal","leftThumbIntermediate","leftThumbDistal","leftIndexProximal","leftIndexIntermediate","leftIndexDistal","leftMiddleProximal","leftMiddleIntermediate","leftMiddleDistal","leftRingProximal","leftRingIntermediate","leftRingDistal","leftLittleProximal","leftLittleIntermediate","leftLittleDistal","rightThumbProximal","rightThumbIntermediate","rightThumbDistal","rightIndexProximal","rightIndexIntermediate","rightIndexDistal","rightMiddleProximal","rightMiddleIntermediate","rightMiddleDistal","rightRingProximal","rightRingIntermediate","rightRingDistal","rightLittleProximal","rightLittleIntermediate","rightLittleDistal","upperChest"
+
+export const VRM_FIRST_PERSON_LOOK_AT = [
+  "Bone",
+  "BlendShape"
+] as const;
+
+export const VRM_BLEND_SHAPE_PRESET = [
+  "unknown",
+  "neutral",
+  "a",
+  "i",
+  "u",
+  "e",
+  "o",
+  "blink",
+  "joy",
+  "angry",
+  "sorrow",
+  "fun",
+  "lookup",
+  "lookdown",
+  "lookleft",
+  "lookright",
+  "blink_l",
+  "blink_r"
+] as const;
+
+export const VRM_META_DEFAULT: VrmMetadata = {
+  title: "",
+  version: "",
+  author: "",
+  contactInformation: "",
+  reference: "",
+  allowedUserName: "OnlyAuthor",
+  violentUssageName: "Disallow",
+  sexualUssageName: "Disallow",
+  commercialUssageName: "Disallow",
+  otherPermissionUrl: "",
+  licenseName: "Redistribution_Prohibited",
+  otherLicenseUrl: ""
+} as const;
+
+export const VRM_BASE: VrmStructure = {
+  extensions: {
+    VRM: {
+      exporterVersion: "avatarhub_vrm_exporter_experimental_0.3",
+      specVersion: "0.0",
+      humanoid: {
+        // TODO: Can be build on the spot
+        humanBones: [
+          {
+            bone: "hips",
+            node: 64,
+            useDefaultValues: true
+          },
+          {
+            bone: "spine",
+            node: 53,
+            useDefaultValues: true
+          },
+          {
+            bone: "chest",
+            node: 51,
+            useDefaultValues: true
+          },
+          {
+            bone: "neck",
+            node: 2,
+            useDefaultValues: true
+          },
+          {
+            bone: "head",
+            node: 1,
+            useDefaultValues: true
+          },
+          {
+            bone: "leftUpperLeg",
+            node: 58,
+            useDefaultValues: true
+          },
+          {
+            bone: "leftLowerLeg",
+            node: 57,
+            useDefaultValues: true
+          },
+          {
+            bone: "leftFoot",
+            node: 56,
+            useDefaultValues: true
+          },
+          {
+            bone: "rightUpperLeg",
+            node: 63,
+            useDefaultValues: true
+          },
+          {
+            bone: "rightLowerLeg",
+            node: 62,
+            useDefaultValues: true
+          },
+          {
+            bone: "rightFoot",
+            node: 61,
+            useDefaultValues: true
+          },
+          {
+            bone: "leftUpperArm",
+            node: 25,
+            useDefaultValues: true
+          },
+          {
+            bone: "leftLowerArm",
+            node: 24,
+            useDefaultValues: true
+          },
+          {
+            bone: "leftHand",
+            node: 23,
+            useDefaultValues: true
+          },
+          {
+            bone: "rightUpperArm",
+            node: 49,
+            useDefaultValues: true
+          },
+          {
+            bone: "rightLowerArm",
+            node: 48,
+            useDefaultValues: true
+          },
+          {
+            bone: "rightHand",
+            node: 47,
+            useDefaultValues: true
+          }
+        ],
+        armStretch: 0.05000000074505806,
+        legStretch: 0.05000000074505806,
+        upperArmTwist: 0.5,
+        lowerArmTwist: 0.5,
+        upperLegTwist: 0.5,
+        lowerLegTwist: 0.5,
+        feetSpacing: 0,
+        hasTranslationDoF: false
+      },
+      firstPerson: {
+        firstPersonBone: 5,
+        firstPersonBoneOffset: {
+          x: 0,
+          y: 0,
+          z: 0
+        },
+        meshAnnotations: [],
+        lookAtTypeName: "Bone",
+        lookAtHorizontalInner: {
+          curve: [
+            0,
+            0,
+            0,
+            1,
+            1,
+            1,
+            1,
+            0
+          ],
+          xRange: 90,
+          yRange: 10
+        },
+        lookAtHorizontalOuter: {
+          curve: [
+            0,
+            0,
+            0,
+            1,
+            1,
+            1,
+            1,
+            0
+          ],
+          xRange: 90,
+          yRange: 10
+        },
+        lookAtVerticalDown: {
+          curve: [
+            0,
+            0,
+            0,
+            1,
+            1,
+            1,
+            1,
+            0
+          ],
+          xRange: 90,
+          yRange: 10
+        },
+        lookAtVerticalUp: {
+          curve: [
+            0,
+            0,
+            0,
+            1,
+            1,
+            1,
+            1,
+            0
+          ],
+          xRange: 90,
+          yRange: 10
+        }
+      },
+      blendShapeMaster: {
+        blendShapeGroups: [
+          {
+            name: "Neutral",
+            presetName: "neutral",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "A",
+            presetName: "a",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "I",
+            presetName: "i",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "U",
+            presetName: "u",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "E",
+            presetName: "e",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "O",
+            presetName: "o",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "Blink",
+            presetName: "blink",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "Joy",
+            presetName: "joy",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "Angry",
+            presetName: "angry",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "Sorrow",
+            presetName: "sorrow",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "Fun",
+            presetName: "fun",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "Blink_L",
+            presetName: "blink_l",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          },
+          {
+            name: "Blink_R",
+            presetName: "blink_r",
+            binds: [],
+            materialValues: [],
+            isBinary: false
+          }
+        ]
+      },
+      secondaryAnimation: {
+        colliderGroups: [],
+        boneGroups: []
+      }
+    }
+  }
+};
---
+++ interfaces/export.interface.ts
@@ -0,0 +1,160 @@
+﻿import {VrmTextureShader} from "../enums/export.enum";
+import {
+  VrmBlendShapePreset,
+  VrmFirstPersonLookAtType,
+  VrmHumanoidBones,
+  VrmMetaAllow,
+  VrmMetaAuthor,
+  VrmMetaLicense,
+  VrmSpecVersion
+} from "../types/export.type";
+
+interface DegreeMap {
+  curve: number[];
+  xRange: number;
+  yRange: number;
+}
+
+export interface GltfMaterial {
+  name: string;
+}
+
+export interface GltfSkin {
+  inverseBindMatrices: number;
+  joints: number[];
+  skeleton: number;
+}
+
+export interface GltfNode {
+  mesh?: number;
+  children?: number[];
+}
+
+export interface GltfScene {
+  nodes: number[];
+}
+
+// TODO: fix type?
+export interface VrmMaterialProperty {
+  name: string;
+  shader: VrmTextureShader;
+  keywordMap: {};
+  tagMap: {};
+  floatProperties: {};
+  vectorProperties: {};
+  textureProperties: {};
+}
+
+export interface VrmMetadata {
+  title: string;
+  version: string;
+  author: string;
+  contactInformation: string;
+  reference: string;
+  allowedUserName: VrmMetaAuthor;
+  violentUssageName: VrmMetaAllow;
+  sexualUssageName: VrmMetaAllow;
+  commercialUssageName: VrmMetaAllow;
+  otherPermissionUrl: string;
+  licenseName: VrmMetaLicense;
+  otherLicenseUrl?: string;
+}
+
+export interface VrmStructure {
+  // TODO: probably wont go
+  scenes?: GltfScene[];
+  nodes?: GltfNode[];
+  skins?: GltfSkin[];
+  materials?: GltfMaterial[];
+  buffers?: {
+    byteLength: number;
+    uri?: string;
+  }[];
+  
+  extensionsUsed?: string[];
+  extensions: {
+    VRM: {
+      materialProperties?: VrmMaterialProperty[];
+      exporterVersion: "avatarhub_vrm_exporter_experimental_0.3";
+      specVersion: VrmSpecVersion;
+      meta?: VrmMetadata;
+      humanoid: {
+        humanBones: {
+          bone: VrmHumanoidBones,
+          node: number;
+          useDefaultValues: boolean;
+        }[];
+        armStretch: number;
+        legStretch: number;
+        upperArmTwist: number;
+        lowerArmTwist: number;
+        upperLegTwist: number;
+        lowerLegTwist: number;
+        feetSpacing: number;
+        hasTranslationDoF: boolean;
+      };
+      firstPerson: {
+        firstPersonBone: number;
+        firstPersonBoneOffset: {
+          x: number;
+          y: number;
+          z: number;
+        };
+        meshAnnotations: {
+          mesh: number;
+          firstPersonFlag: string;
+        }[];
+        lookAtTypeName: VrmFirstPersonLookAtType;
+        lookAtHorizontalInner: DegreeMap;
+        lookAtHorizontalOuter: DegreeMap;
+        lookAtVerticalDown: DegreeMap;
+        lookAtVerticalUp: DegreeMap;
+      };
+      blendShapeMaster: {
+        blendShapeGroups: {
+          name: string;
+          presetName: VrmBlendShapePreset;
+          binds: {
+            mesh: number;
+            index: number;
+            weight: number;
+          }[];
+          materialValues: {
+            materialName: string;
+            propertyName: string;
+            targetValue: number[];
+          }[];
+          isBinary: boolean;
+        }[];
+      };
+      secondaryAnimation: {
+        colliderGroups: {
+          node: number;
+          colliders: {
+            offset: {
+              x: number;
+              y: number;
+              z: number;
+            };
+            radius: number;
+          }[];
+        }[];
+        boneGroups: {
+          comment: string;
+          stiffiness: number;
+          gravityPower: number;
+          gravityDir: {
+            x: number;
+            y: number;
+            z: number;
+          };
+          dragForce: number;
+          center: number;
+          hitRadius: number;
+          bones: number[];
+          colliderGroups: number[];
+        }[];
+      }
+    }
+  }
+}
