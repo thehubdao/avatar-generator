2ed3677a46e6110284c71df6661005631b5f244d
Andres <andgonzalezcas@unal.edu.co>
2023-11-16 10:56:11 -0500

    change .constants to .constant on swiper item per view file

--- ui/avatar/selectors/mobile/featureSelector.ui.tsx
+++ ui/avatar/selectors/mobile/featureSelector.ui.tsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import 'swiper/css';
 import { FeatureBasic } from '../../../../interfaces/common.interface';
 import FeatureOptionListUI from './featureOptionList.ui';
-import { MOBILE_ITEMS_PER_VIEW } from '../../../../constants/mobile/swiperItemsPerView.constants';
+import { MOBILE_ITEMS_PER_VIEW } from '../../../../constants/mobile/swiperItemsPerView.constant';
 
 interface FeatureSelectorUIProps {
   list: FeatureBasic[];
