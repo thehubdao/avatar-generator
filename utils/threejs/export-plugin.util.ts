38fe6e3d5e5cdbfb3b47bab02e90a2c765e4bf18
OswaldUnity <oswald@thedac.info>
2023-11-17 10:34:55 -0500

    Update gltfVrmPlugin, generates and fill the data before binarize

--- utils/threejs/export-plugin.util.ts
+++ utils/threejs/export-plugin.util.ts
@@ -1,17 +1,10 @@
 ﻿import {GLTFExporterPlugin, GLTFWriter} from "three/examples/jsm/exporters/GLTFExporter";
-import {Object3D, SkinnedMesh} from "three";
-import {IsSkinnedMesh} from "../model.util";
-import {CastStringToInteger} from "../common.util";
-import {GltfMaterial} from "../../interfaces/export.interface";
-
-interface WriterProps {
-  json: {
-    material: GltfMaterial[]
-  }
-}
+import {Object3D} from "three";
+import {VrmStructure} from "../../interfaces/export.interface";
+import {GenerateVrmBoneData, GenerateVrmMaterialData, GetVrmData} from "./vrm.util";
 
 export default class GLTFExporterAddVrmData implements GLTFExporterPlugin {
-  public writer: GLTFWriter;
+  public writer: GLTFWriter & { json?: VrmStructure };
   public name: string;
 
   constructor(writer: GLTFWriter) {
@@ -19,53 +12,28 @@ export default class GLTFExporterAddVrmData implements GLTFExporterPlugin {
     this.name = 'AB_add_vrm_data';
   }
   
-  writeNode(object: Object3D, nodeDef: { [key: string]: any }) {
-    console.log("Write Node!");
-    const writer = this.writer;
-    const json = {...writer.json};
-    const skins = [...writer.skins] as SkinnedMesh[];
-    
-    console.log("JSON", json);
-    
-    if (!IsSkinnedMesh(object)) return;
-
-    console.log("Skins", skins);    
-    
-    const skinRef = ObjectEntries(skins).find(([key, val]) => val === object);
-    
-    console.log(skinRef);
-    
-    // if (CastStringToInteger(skinRef[0]) !== 0) {
-    //   console.log("Remove!", skinRef[0], 0, skinRef[0] !== 0);
-    //   delete writer.skins[skinRef[0]];
-    // }
-    // else {
-    //   console.log("Not!", skinRef[0], skinRef[0] !== 0)
-    //   console.log(writer.skins[0]);
-    // }
-    
-    // nodeDef.skin = 0;
-    
-    console.log(object, nodeDef);
-  }
-  
   afterParse(input: Object3D | Object3D[]) {
-    const writer = this.writer;
+    const writer = this.writer;    
     const json = writer.json;
-    
-    console.log(json);
-    
-    // TODO: Generate Bones
+    if (json == undefined) return;
+        
+    // Get nodes
+    const nodes = json.nodes;
+    GenerateVrmBoneData(nodes);
     
     // GetMaterial
+    const materials = json?.materials;
     // Add material
+    GenerateVrmMaterialData(materials);
+    
+    // Add VRM extension used
+    json.extensionsUsed = json.extensionsUsed || [];
+    json.extensionsUsed.push("VRM");
     
     // Get VRM Values
-    // Add to Json
+    const vrmData = GetVrmData();
     
+    // Add to Json
+    json.extensions = vrmData.extensions;
   }
-}
-
-export function ObjectEntries<K extends (string | number | symbol), V>(obj: Record<K, V>) {
-  return Object.entries(obj) as unknown as [K, V][];
 }
