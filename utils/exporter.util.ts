18889621cca7113f3cc808bc0cad090fab8e8122
OswaldUnity <oswald@thedac.info>
2023-11-17 13:03:31 -0500

    Moved vrm scene generation to vrm.util

--- utils/exporter.util.ts
+++ utils/exporter.util.ts
@@ -10,6 +10,7 @@ import GLTFExporterAddVrmData from "./threejs/export-plugin.util";
 import {Bone, BufferGeometry, Material, Matrix4, Scene, Skeleton, SkinnedMesh} from "three";
 import {mergeGeometries} from "three/examples/jsm/utils/BufferGeometryUtils";
 import {degToRad} from "three/src/math/MathUtils";
+import {GenerateVrmScene} from "./threejs/vrm.util";
 
 class ExporterUtil {
   private static _instance: ExporterUtil;
@@ -88,147 +89,19 @@ export async function ExportModelVrm(model: GLTF | undefined, pose?: Record<stri
     if (model == undefined)
       Raise("Model can't be undefined if trying to export!");
 
-    // TODO: Check how to force scale on sub objects without changing scale on those subobjects
-    
     const sceneClone = clone(model.scene);
     SetPose(sceneClone, pose);
 
-    const sceneReal = new Scene();
-    sceneReal.name = "Scene";
-    sceneReal.userData.name = "Scene";
+    const vrmAvatar = GenerateVrmScene(sceneClone);
     
-    try {
-      const skinnedMeshArray: SkinnedMesh[] = [];
-      const geometryArray: BufferGeometry[] = [];
-      const materialArray: Material[] = [];
-
-      sceneClone.traverse(obj => {
-        if (IsSkinnedMesh(obj)) {
-          skinnedMeshArray.push(obj);
-          geometryArray.push(obj.geometry.clone());
-          const mat = obj.material;
-          if (mat instanceof Material) {
-            materialArray.push(mat.clone());
-          } else {
-            const lel = mat.map(m => m.clone());
-            materialArray.push(...lel);
-          }
-        }
-      });
-    
-      const allowedProps = Object.keys(geometryArray[0].attributes);
-      
-      for (const geom of geometryArray) {
-        for (const key of Object.keys(geom.attributes)) {
-          if (!allowedProps.includes(key))
-            delete geom.attributes[key];
-        }
-      }
-
-      const firstSM = skinnedMeshArray[0];
-      const matrixCopy = firstSM.skeleton.boneInverses.map(b => b.clone());
-      const actualBones = firstSM.skeleton.bones[0].clone(true);
-      const boneArray: Bone[] = [];
-      
-      actualBones.traverse(obj => {
-        if (IsBone(obj)) {
-          boneArray.push(obj);
-        }
-      });
-      
-      const avatarGeom = mergeGeometries(geometryArray, true);
-
-      const leSkinnedMesh = new SkinnedMesh(avatarGeom, materialArray);
-      leSkinnedMesh.name = "Avatar";
-      leSkinnedMesh.userData.name = "Avatar";
-      
-      // leSkinnedMesh.scale.setScalar(0.01);
-      // leSkinnedMesh.rotateX(degToRad(90));
-      // leSkinnedMesh.position.setScalar(0);
-      // leSkinnedMesh.updateMatrix();
-      // leSkinnedMesh.geometry.applyMatrix4(leSkinnedMesh.matrix);
-      // leSkinnedMesh.scale.setScalar(1);
-      // leSkinnedMesh.rotation.set(0, 0, 0);
-      // leSkinnedMesh.position.setScalar(0);
-      // leSkinnedMesh.updateMatrix();
-
-      actualBones.scale.setScalar(0.01);
-      actualBones.rotateX(degToRad(90));
-      actualBones.position.setScalar(0);
-      actualBones.updateMatrix();
-      
-      const newSkeleton = new Skeleton(boneArray, matrixCopy);
-      const identity = new Matrix4();
-      leSkinnedMesh.bind(newSkeleton, identity);
-      
-      leSkinnedMesh.scale.setScalar(0.01);
-      leSkinnedMesh.rotateX(degToRad(90));
-      leSkinnedMesh.updateMatrix();
-      leSkinnedMesh.geometry.applyMatrix4(leSkinnedMesh.matrix);
-
-      // leSkinnedMesh.updateMatrix();
-      
-      // actualBones.scale.setScalar(0.01);
-      // actualBones.rotateX(degToRad(90));
-      // actualBones.position.setScalar(0);
-      // actualBones.updateMatrix();
-
-      // actualBones.scale.setScalar(0.01);
-      // actualBones.rotateX(degToRad(90));
-      // actualBones.position.setScalar(0);
-      // actualBones.updateMatrix();
-
-      // leSkinnedMesh.updateMatrix();
-      // leSkinnedMesh.geometry.applyMatrix4(leSkinnedMesh.matrix);
-      // leSkinnedMesh.scale.setScalar(1);
-      // leSkinnedMesh.position.setScalar(0);
-      // leSkinnedMesh.rotation.set(0, 0, 0);
-      // leSkinnedMesh.updateMatrix();
-      
-      // const armatureGroup = new Group();
-      // armatureGroup.name = "Main";
-      // armatureGroup.userData.name = "Main";
-      // armatureGroup.scale.setScalar(0.01);
-      // armatureGroup.rotateX(degToRad(90));
-      // armatureGroup.rotateZ(degToRad(180));
-      
-      // armatureGroup.children.push(actualBones);
-      // armatureGroup.children.push(leSkinnedMesh);
-      // armatureGroup.add(actualBones);
-      // armatureGroup.add(leSkinnedMesh);
-
-      // sceneReal.children.push(armatureGroup);
-
-      // sceneReal.children.push(actualBones);
-      // sceneReal.children.push(leSkinnedMesh);
-
-      // sceneReal.add(actualBones);
-      // sceneReal.add(leSkinnedMesh);
-
-      
-      // actualBones.geometry.applyMatrix4(leSkinnedMesh.matrix);
-      // actualBones.scale.setScalar(1);
-      // actualBones.updateMatrix();
-
-      sceneReal.children.push(actualBones);
-      sceneReal.children.push(leSkinnedMesh);
-      
-      // sceneReal.position.setScalar(0);
-      // sceneReal.rotation.set(0, 0, 0);
-      // sceneReal.scale.setScalar(1);
-      // sceneReal.updateMatrix();
-    } catch (e) {
-      console.error(e);
-    }
-
     // CleanModelForExport(model);
-    let glb = await exporter.parseAsync(sceneReal, {
+    let vrm = await exporter.parseAsync(vrmAvatar, {
       animations: [],
       binary: true,
       trs: true,
     }) as ArrayBuffer;
-    
-    return {success: true, value: new Blob([glb], { type: 'application/octet-stream' })};
+
+    return {success: true, value: new Blob([vrm], {type: 'application/octet-stream'})};
   } catch (err) {
     const msg = "Error while exporting to VRM!";
     void LogError(Module.ExporterUtil, msg, err);
