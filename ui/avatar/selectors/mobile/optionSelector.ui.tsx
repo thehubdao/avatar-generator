716b3d0c75d7acea6016b5cde0c38b59fb52ed2b
nan <nan@thedac.info>
2023-11-17 12:13:16 -0500

    make the list as required

--- ui/avatar/selectors/mobile/optionSelector.ui.tsx
+++ ui/avatar/selectors/mobile/optionSelector.ui.tsx
@@ -4,18 +4,17 @@ import { BasicData } from '../../../../interfaces/common.interface';
 import { OptionItemUI } from "./optionItem.ui";
 
 interface OptionSelectorUIProps {
-  list?: FeatureInterface[];
+  list: FeatureInterface[];
   activeOption?: BasicData;
   handleClick: (id: string, path: string, name: string) => void;
 }
 
 export default function OptionSelectorUI({ list, activeOption, handleClick }: OptionSelectorUIProps) {
   return <>
-    {list && <div className='w-full px-2 flex flex-wrap overflow-y-scroll gap-2 justify-center h-fit items-center'>
+    <div className='w-full px-2 flex flex-wrap overflow-y-scroll gap-2 justify-center h-fit items-center'>
       {list.map((opt, index) => (
         <OptionItemUI key={index} opt={opt} isActive={activeOption?.val === opt.name} handleClick={(id, path, name) => handleClick(id, path, name)} />
       ))}
-    </div>}
+    </div>
   </>
 }
-
