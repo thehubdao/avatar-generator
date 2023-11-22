2ad8dd635cfbbac8d5326ac1b011ae068d9df120
OswaldUnity <oswald@thedac.info>
2023-11-22 11:16:16 -0500

    Added new way to rotate bones for vrm generation

--- utils/threejs/vrm.util.ts
+++ utils/threejs/vrm.util.ts
@@ -11,13 +11,25 @@ import {VrmTextureShader} from "../../enums/export.enum";
 import {
   VRM_BASE,
   VRM_HUMAN_BONES_DEFAULT,
-  VRM_HUMAN_BONES_DEFAULT_LENGHT,
-  VRM_HUMANOID_BONES, VRM_MAP_MIXAMO, VRM_META_DEFAULT
+  VRM_HUMAN_BONES_DEFAULT_LENGTH,
+  VRM_HUMANOID_BONES,
+  VRM_MAP_MIXAMO,
+  VRM_META_DEFAULT
 } from "../../constants/export.constant";
-import {Bone, BufferGeometry, Material, Matrix4, Object3D, Scene, Skeleton, SkinnedMesh, Vector3} from "three";
+import {
+  Bone,
+  BufferGeometry,
+  Material,
+  Matrix4,
+  Object3D,
+  Quaternion,
+  Scene,
+  Skeleton,
+  SkinnedMesh,
+  Vector3
+} from "three";
 import {IsBone, IsSkinnedMesh} from "../model.util";
 import {mergeGeometries} from "three/examples/jsm/utils/BufferGeometryUtils";
-import {degToRad} from "three/src/math/MathUtils";
 
 class VrmUtil {
   private static _instance: VrmUtil;
@@ -82,7 +94,7 @@ export function GenerateVrmBoneData(nodes: GltfNode[] | undefined) {
   
   try {    
     if (nodes == undefined)
-      Raise("Need skin and node list in order to generate human bones properly!");
+      Raise("Need node list in order to generate human bones properly!");
     
     for (let i = 0; i < VRM_HUMANOID_BONES.length; i++) {
       const vrmBone = VRM_HUMANOID_BONES[i];
@@ -105,7 +117,7 @@ export function GenerateVrmBoneData(nodes: GltfNode[] | undefined) {
     extensions: {
       VRM: {
         humanoid: {
-          humanBones: generatedBones.length === VRM_HUMAN_BONES_DEFAULT_LENGHT
+          humanBones: generatedBones.length === VRM_HUMAN_BONES_DEFAULT_LENGTH
             ? generatedBones : VRM_HUMAN_BONES_DEFAULT
         }
       }
@@ -179,10 +191,7 @@ export function GenerateVrmScene(model: Object3D) {
     }
   }
 
-  const firstSM = skinnedMeshArray[0]
-  firstSM.scale.setScalar(scaleFactor);
-  firstSM.updateMatrix();
-  firstSM.geometry.applyMatrix4(firstSM.matrix);
+  const firstSM = skinnedMeshArray[0];
   const matrixCopy = firstSM.skeleton.boneInverses.map(b => b.clone());
   const actualBones = firstSM.skeleton.bones[0].clone(true);
   const boneArray: Bone[] = [];
@@ -192,36 +201,97 @@ export function GenerateVrmScene(model: Object3D) {
       boneArray.push(obj);
     }
   });
-
+  
   const avatarGeom = mergeGeometries(geometryArray, true);
 
   const leSkinnedMesh = new SkinnedMesh(avatarGeom, materialArray);
   leSkinnedMesh.name = "Avatar";
   leSkinnedMesh.userData.name = "Avatar";
-
-  // leSkinnedMesh.scale.setScalar(0.01);
+  
+  // actualBones.matrixAutoUpdate = false;
+  // leSkinnedMesh.matrixAutoUpdate = false;
+  
+  // leSkinnedMesh.scale.setScalar(scaleFactor);
   // leSkinnedMesh.rotateX(degToRad(90));
   // leSkinnedMesh.position.setScalar(0);
   // leSkinnedMesh.updateMatrix();
+  // leSkinnedMesh.updateWorldMatrix(true, true)
+  //
+  // const pos1 = new Vector3();
+  // const rot1 = new Quaternion();
+  // const sca1 = new Vector3();
+  // leSkinnedMesh.matrixWorld.clone().decompose(pos1, rot1, sca1);
+  // console.log("Tranform: ", {pos1, rot1, sca1});
+  //
+  //
   // leSkinnedMesh.geometry.applyMatrix4(leSkinnedMesh.matrix);
   // leSkinnedMesh.scale.setScalar(1);
   // leSkinnedMesh.rotation.set(0, 0, 0);
   // leSkinnedMesh.position.setScalar(0);
   // leSkinnedMesh.updateMatrix();
+  //
+  // leSkinnedMesh.updateWorldMatrix(true, true)
+  // const pos3 = new Vector3();
+  // const rot3 = new Quaternion();
+  // const sca3 = new Vector3();
+  // leSkinnedMesh.matrixWorld.clone().decompose(pos3, rot3, sca3);
+  // console.log("KEK: ", {pos3, rot3, sca3});
+  // leSkinnedMesh.matrixAutoUpdate = false;
   
-  // actualBones.scale.setScalar(scaleFactor);
-  // actualBones.rotateX(degToRad(90));
-  // actualBones.position.setScalar(0);
+  // actualBones.rotateOnWorldAxis(new Vector3(1, 0, 0), degToRad(90));
   // actualBones.updateMatrix();
+  // actualBones.scale.setScalar(scaleFactor);
+  // actualBones.position.setX(0);
+  // actualBones.position.setZ(0);
+  // actualBones.position.setY();
+  // actualBones.updateMatrix();
+
+  // const scull = 0.01;
+  // const matrixScaleOG = new Matrix4();
+  // matrixScaleOG.makeScale(scull, scull, scull);
+  // leSkinnedMesh.scale.applyMatrix4(matrixScaleOG);
 
   const newSkeleton = new Skeleton(boneArray, matrixCopy);
   const identity = new Matrix4();
   leSkinnedMesh.bind(newSkeleton, identity);
+  
+  //Create a matrix
+  const matrix = new Matrix4();
+  //Rotate the matrix
+  matrix.makeRotationX(Math.PI / 2);
 
-  leSkinnedMesh.scale.setScalar(scaleFactor);
-  leSkinnedMesh.rotateX(degToRad(90));
-  leSkinnedMesh.updateMatrix();
-  leSkinnedMesh.geometry.applyMatrix4(leSkinnedMesh.matrix);
+  //rotate the object using the matrix
+  actualBones.position.applyMatrix4(matrix);
+
+  leSkinnedMesh.geometry.computeBoundingBox();
+  console.log(leSkinnedMesh.geometry.boundingBox?.clone());
+  
+  const matrixScale = new Matrix4();
+  matrixScale.makeScale(0.01, 0.01, 0.01);
+  actualBones.scale.applyMatrix4(matrixScale);
+  actualBones.position.applyMatrix4(matrixScale);
+  // leSkinnedMesh.scale.applyMatrix4(matrixScale);
+  
+  // leSkinnedMesh.geometry.computeBoundingBox();
+  // console.log(leSkinnedMesh.geometry.);
+  
+  actualBones.rotateX(Math.PI / 2);
+  // actualBones.rotateY(Math.PI);
+  
+  // const pivot = new Object3D();
+  // pivot.add(actualBones);
+  // pivot.add(leSkinnedMesh);
+  //
+  // pivot.scale.setScalar(0.01);
+  // pivot.rotateX(degToRad(90));
+  // pivot.updateMatrix();
+  // pivot.updateMatrixWorld(true);
+  // pivot.updateWorldMatrix(true, true);
+  
+  // leSkinnedMesh.scale.setScalar(scaleFactor);
+  // leSkinnedMesh.rotateX(degToRad(90));
+  // leSkinnedMesh.updateMatrix();
+  // leSkinnedMesh.geometry.applyMatrix4(leSkinnedMesh.matrix);
 
   // leSkinnedMesh.updateMatrix();
 
@@ -269,11 +339,6 @@ export function GenerateVrmScene(model: Object3D) {
 
   sceneReal.children.push(actualBones);
   sceneReal.children.push(leSkinnedMesh);
-
-  // sceneReal.position.setScalar(0);
-  // sceneReal.rotation.set(0, 0, 0);
-  // sceneReal.scale.setScalar(1);
-  // sceneReal.updateMatrix();
   
   return sceneReal;
 }
