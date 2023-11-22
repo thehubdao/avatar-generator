e624c6565c1c039d10435f5d05e701e4ea4a86de
OswaldUnity <oswald@thedac.info>
2023-11-22 11:15:05 -0500

    Fix error adding VRM on extensions used, gltf plugin

--- utils/threejs/export-plugin.util.ts
+++ utils/threejs/export-plugin.util.ts
@@ -4,19 +4,19 @@ import {VrmStructure} from "../../interfaces/export.interface";
 import {GenerateVrmBoneData, GenerateVrmMaterialData, GetVrmData} from "./vrm.util";
 
 export default class GLTFExporterAddVrmData implements GLTFExporterPlugin {
-  public writer: GLTFWriter & { json?: VrmStructure };
+  public writer: GLTFWriter & { json?: VrmStructure, extensionsUsed?: Record<string, boolean> };
   public name: string;
 
   constructor(writer: GLTFWriter) {
     this.writer = writer;
-    this.name = 'AB_add_vrm_data';
+    this.name = "VRM";
   }
   
   afterParse(input: Object3D | Object3D[]) {
     const writer = this.writer;    
-    const json = writer.json;
-    if (json == undefined) return;
-        
+    const json = writer.json!;
+    const extensionsUsed = writer.extensionsUsed!;
+    
     // Get nodes
     const nodes = json.nodes;
     GenerateVrmBoneData(nodes);
@@ -26,14 +26,16 @@ export default class GLTFExporterAddVrmData implements GLTFExporterPlugin {
     // Add material
     GenerateVrmMaterialData(materials);
     
-    // Add VRM extension used
-    json.extensionsUsed = json.extensionsUsed || [];
-    json.extensionsUsed.push("VRM");
-    
     // Get VRM Values
     const vrmData = GetVrmData();
     
-    // Add to Json
-    json.extensions = vrmData.extensions;
+    if (!extensionsUsed[this.name]) {
+      // Add VRM extension used
+      json.extensions = json.extensions || {};
+      // Add to Json
+      json.extensions = vrmData.extensions;
+      
+      extensionsUsed[this.name] = true;
+    }
   }
 }
