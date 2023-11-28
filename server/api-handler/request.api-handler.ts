9c49e0b84d61e20eea1ae49430a3585bc7e5d576
OswaldUnity <oswald@thedac.info>
2023-11-27 21:27:39 -0500

    Expand RequestHandler interface with multiple types
    The RequestHandler interface and RequestApiHandler function now support multiple types by expanding the generic parameters. This allows a different type for each CRUD operation (Get, Post, Put, Delete), providing more flexibility in handling different data types for different operations. This change improves type safety and makes API handling more robust.

--- server/api-handler/request.api-handler.ts
+++ server/api-handler/request.api-handler.ts
@@ -5,7 +5,7 @@ import {RequestMessage, RequestStatus} from "../types/api.type";
 import {REQUEST_STATUS, REQUEST_METHOD} from "../constants/api.constant";
 import {DefaultApiResponse} from "../enums/api.enum";
 
-export async function RequestApiHandler<T>(req: NextApiRequest, res: NextApiResponse<ApiResponse<T>>, requests: RequestHandler<T>) {
+export async function RequestApiHandler<T, U, V, W>(req: NextApiRequest, res: NextApiResponse<ApiResponse<T | U | V | W>>, requests: RequestHandler<T, U, V, W>) {
   switch (req.method) {
     case REQUEST_METHOD.Get:
       if (requests.Get == undefined)
--- server/interfaces/api-handler.interface.ts
+++ server/interfaces/api-handler.interface.ts
@@ -1,11 +1,11 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {ApiResponse} from "../../interfaces/api.interface";
 
-export interface RequestHandler<T> {
+export interface RequestHandler<T, U, V, W> {
   Get?: RequestFunction<T>,
-  Post?: RequestFunction<T>,
-  Put?: RequestFunction<T>,
-  Delete?: RequestFunction<T>,
+  Post?: RequestFunction<U>,
+  Put?: RequestFunction<V>,
+  Delete?: RequestFunction<W>,
 }
 
 type RequestFunction<T> = (req: NextApiRequest, res: NextApiResponse<ApiResponse<T>>) => Promise<void>;
