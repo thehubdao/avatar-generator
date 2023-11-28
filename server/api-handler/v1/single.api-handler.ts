c76331dbd779c06668ac9c9d1d6d537ae0b33c8c
OswaldUnity <oswald@thedac.info>
2023-11-27 21:29:38 -0500

    Added implementation single post, returns combination number by campaign & attributes

--- server/api-handler/v1/single.api-handler.ts
+++ server/api-handler/v1/single.api-handler.ts
@@ -9,11 +9,14 @@ import {
   GetCombinationValues,
   GetMaxCombinationNum,
   GetMaxIndexValues,
-  NumberToIndexValues,
-  IndexValuesStringToNumber
+  IndexValuesStringToNumber,
+  NumberToIndexValues
 } from "../../../utils/collection.util";
-import {CastStringToInteger, RandomIntMax} from "../../../utils/common.util";
-import {GLOBAL_VALUES} from "../../../constants/common.constant";
+import {Base64ToObj, CastStringToInteger, LogError, Raise, RandomIntMax} from "../../../utils/common.util";
+import {EXPORT_ATTRIBUTE, GLOBAL_VALUES} from "../../../constants/common.constant";
+import {SinglePostBody} from "../../interfaces/single.interface";
+import {BasicData} from "../../../interfaces/common.interface";
+import {Module} from "../../../enums/common.enum";
 
 async function CheckCampaign(campaign: string) {
   const campaignsResult = await GetParameter<string[]>(undefined, FirestoreParameters.Campaigns);
@@ -86,4 +89,49 @@ export async function GetUriApiHandler(req: NextApiRequest, res: NextApiResponse
   const [campaign, combination] = single as string[];
 
   return await ProcessAndGetData(res, campaign, combination);
+}
+
+export async function PostApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<number>>) {
+  const {campaign, attributes} = req.body as SinglePostBody;
+  
+  if (campaign == undefined || attributes == undefined)
+    return RequestResponse(res, "BadRequest", false, DefaultApiResponse.MissingInfo);
+  
+  try {
+    // On campaign not exist return bad request
+    const isCampaign = await CheckCampaign(campaign);
+    if (!isCampaign)
+      return RequestResponse(res, "BadRequest", false, DefaultApiResponse.WrongInput);
+
+    const realAttributes = Base64ToObj<BasicData[]>(attributes);
+    if (realAttributes == undefined || !Array.isArray(realAttributes))
+      return RequestResponse(res, "BadRequest", false,  DefaultApiResponse.WrongInput);
+
+    // Update index on campaign
+    const featureValues = await FindAndReadjustFeatureIndexes(campaign);
+    if (featureValues == undefined)
+      return RequestResponse(res, "ServerError", false, DefaultApiResponse.ErrorProcessingInfo);
+
+    // On invalid number use random combination
+    const maxIndexValues = GetMaxIndexValues(featureValues.featureList, featureValues.featureOptionListData);
+
+    const indexValues = realAttributes
+      .filter(a => a.id !== EXPORT_ATTRIBUTE.Campaign)
+      .map(a => {
+        const featureIndex = featureValues.featureList.find(f => f.displayName === a.id)?.index;
+        if (featureIndex == undefined) return;
+        
+        return featureValues.featureOptionListData.get(featureIndex)?.find(o => o.name === a.val)?.index;
+      });
+    
+    const combination = IndexValuesStringToNumber(indexValues.join(GLOBAL_VALUES.CollectorIndexSeparator), maxIndexValues);
+    if (combination == undefined)
+      Raise("Error getting combination number from index values!");
+
+    return RequestResponse(res, "Successful", true, DefaultApiResponse.PostSuccess, combination);
+  } catch (e) {
+    const err = e as Error;
+    void LogError(Module.ApiUtil, err.message, e);
+    return RequestResponse(res, "ServerError", false, DefaultApiResponse.ErrorProcessingInfo);
+  }
 }
