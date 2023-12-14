ff6c43e2b7a282cd8ccc62c0a98adc8b4da65fda
OswaldUnity <oswald@thedac.info>
2023-12-14 18:01:14 -0500

    Added collectionItem interface representing items on collection generated

--- server/interfaces/collection.interface.ts
+++ server/interfaces/collection.interface.ts
@@ -1,4 +1,13 @@
+import {CollectionStatus} from "../enums/collection.enum";
+
 export interface CollectionPostBody {
   campaign?: string;
   update: boolean;
+}
+
+export interface CollectionItem {
+  id: number;
+  indexValues: string;
+  status: CollectionStatus;
+  chance: number;
 }
