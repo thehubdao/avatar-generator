5f24c9cbe4313834599ef6dbf8735c0e3eec7bd7
GabCh15 <info@thedac.info>
2024-02-05 16:45:03 -0500

    fix mint whitelist

--- utils/phase.util.ts
+++ utils/phase.util.ts
@@ -5,7 +5,7 @@ import { Phase, phaseMap } from '../types/avatar.type'
 export const currentPhase: Phase | undefined = process.env.NEXT_PUBLIC_MINTING_PHASE as Phase
 
 export const isAddressWhitelisted = (address: string | undefined) => {
-    if (mintWhitelist[address as keyof typeof mintWhitelist]) return true
+    if (mintWhitelist[address?.toLowerCase() as keyof typeof mintWhitelist]) return true
     return false
 }
 
