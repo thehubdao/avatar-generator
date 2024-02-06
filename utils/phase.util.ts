165352940fe5405f808d5a2bb6a62cc75421a6ca
GabCh15 <info@thedac.info>
2024-02-06 13:35:26 -0500

    fix whitelist

--- utils/phase.util.ts
+++ utils/phase.util.ts
@@ -1,19 +1,21 @@
+import { getAddress } from 'ethers'
 import mintWhitelist from '../constants/lukso/mintWhitelist.json'
 import { Phase, phaseMap } from '../types/avatar.type'
 
 export const currentPhase: Phase | undefined = process.env.NEXT_PUBLIC_MINTING_PHASE as Phase
 
 export const isAddressWhitelisted = (address: string | undefined) => {
-    return true
     if (mintWhitelist[address?.toLowerCase() as keyof typeof mintWhitelist]) return true
     return false
 }
 
 export const canMint = (address: string | undefined) => {
     if (!address) return false
-    
+    const formattedAddress = getAddress(address)
+    if (!formattedAddress) return false
+
     if (currentPhase === phaseMap.publicMintPhase) return true
-    if (currentPhase === phaseMap.whitelistPhase && isAddressWhitelisted(address)) return true
+    if (currentPhase === phaseMap.whitelistPhase && isAddressWhitelisted(formattedAddress)) return true
 
     return false
 }
