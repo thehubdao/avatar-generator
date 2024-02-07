2ade6e86767da3567495cf573c6a12fa67448061
GabCh15 <info@thedac.info>
2024-02-06 23:44:45 -0500

    upload pics changes

--- ui/lukso/lukso.ui.tsx
+++ ui/lukso/lukso.ui.tsx
@@ -44,8 +44,8 @@ export default function LuksoUI({
     <div className="w-full grow text-white text-center">
       {isLoading && <LoadingUI getloaderDivElement={getloaderDivElement} />}
       {(currentSection === LuksoSections.Main) && <MainSectionUI combination={combination} picture={picture} setCurrentSection={(newSection) => setCurrentSection(newSection)} hasMinted={hasMinted} provider={provider} isGettingInfoAboutHasMinted={isGettingInfoAboutHasMinted}/>}
-      {(currentSection === LuksoSections.Mint) && <MintSectionUI picture={`https://gateway.pinata.cloud/ipfs/${process.env.NEXT_PUBLIC_IPFS_IMAGE_URL}/${combination}.png`} features={features} setCurrentSection={(newSection) => setCurrentSection(newSection)} reRoll={reRoll} handleClaim={handleClaim} provider={provider} />}
-      {(currentSection === LuksoSections.Edit) && <EditSectionUI combination={combination} features={features} setIsEditModeSelected={setIsEditModeSelected} exportModel={exportModel} />}
+      {(currentSection === LuksoSections.Mint) && <MintSectionUI picture={picture} features={features} setCurrentSection={(newSection) => setCurrentSection(newSection)} reRoll={reRoll} handleClaim={handleClaim} provider={provider} />}
+      {(currentSection === LuksoSections.Edit) && <EditSectionUI picture={picture} combination={combination} features={features} setIsEditModeSelected={setIsEditModeSelected} exportModel={exportModel} />}
     </div>
   )
 }
--- ui/lukso/sections/mainSection.ui.tsx
+++ ui/lukso/sections/mainSection.ui.tsx
@@ -27,7 +27,7 @@ interface MainSectionUIProps {
 
 
 
-export default function MainSectionUI({ setCurrentSection, hasMinted, provider, isGettingInfoAboutHasMinted, picture, combination }: MainSectionUIProps) {
+export default function MainSectionUI({ setCurrentSection, hasMinted, provider, isGettingInfoAboutHasMinted, picture }: MainSectionUIProps) {
   const luksoAvatarRef = useRef<HTMLDivElement>(null);
   const exclusiveCollectionRef = useRef<HTMLDivElement>(null);
 
@@ -214,7 +214,7 @@ export default function MainSectionUI({ setCurrentSection, hasMinted, provider, 
             flex-col bg-[#d59fba]">
               {isLoading && <LoadingUI getloaderDivElement={()=>{}} />}
               <Image
-                src={`https://gateway.pinata.cloud/ipfs/${process.env.NEXT_PUBLIC_IPFS_IMAGE_URL}/${combination}.png`}
+                src={picture}
                 alt="lukso avatar selfie view"
 /*                 width={384}
                 height={384} */
