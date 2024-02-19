dccbdc0e71dcfd7030febdc8336f292bae3ac8cf
GabCh15 <info@thedac.info>
2024-02-19 16:06:04 -0500

    test deployment

--- ui/lukso/common/conectModal.tsx
+++ ui/lukso/common/conectModal.tsx
@@ -20,7 +20,7 @@ function ConnectModalUI({ setIsConnecting }: ConnectModalUIProps) {
             <ConnectWeb3Button onConnect={onConnect} classStyles="h-fit" >
               <TransparentBoxUI fullWidth border backgroundColorClass="bg-white" heightClass="h-12" >
                 <div className="flex items-center gap-3">
-                  <p className="text-black">Mint starting at 4:20PM EST</p>
+                  <p className="text-black">Login</p>
                 </div>
               </TransparentBoxUI>
             </ConnectWeb3Button>
