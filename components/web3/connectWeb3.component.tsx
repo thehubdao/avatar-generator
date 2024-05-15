08779ff1854a18e6fe907a084199edbaa1585baf
GabCh15 <info@thedac.info>
2024-05-14 20:50:49 -0500

    Delete console log

--- components/web3/connectWeb3.component.tsx
+++ components/web3/connectWeb3.component.tsx
@@ -14,10 +14,9 @@ export default function ConnectWeb3Button({ classStyles, children, onConnect }: 
     return <>
         <button className={classStyles} onClick={ () => {
             void (async ()=>{ 
-            try{
-                console.log("Connect")
+           
                 await connect()
-            console.log("COnnectTT")}catch(err){console.log(err)}})()
+            })()
             onConnect()
         }}>
             <div className="flex items-center gap-3 mx-4">
