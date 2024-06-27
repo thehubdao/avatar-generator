79ad57349221a5d447f2df13b9ac91053df955c3
GabCh15 <info@thedac.info>
2024-06-26 22:04:25 -0500

    Fix build

--- components/lukso/lukso.component.tsx
+++ components/lukso/lukso.component.tsx
@@ -18,7 +18,6 @@ import AvatarEditor, {
 import {
   AGChangeCamPosition,
   AGChangeLookAtPosition,
-  GetCanvasImageUrl,
   TakeCanvasPicture,
 } from '../avatar/viewer.component'
 import HudComponent from '../../ui/avatar/hud.ui'
@@ -32,7 +31,7 @@ import { Module } from '../../enums/common.enum'
 import { LuksoSections } from '../../enums/lukso/common.enum'
 
 // Utils
-import { Delay, FilterList, LogError, MixArrays } from '../../utils/common.util'
+import { FilterList, LogError, MixArrays } from '../../utils/common.util'
 import {
   GetAccessoryListByCampaign,
   GetAnimationByCampaignAndName,
@@ -132,12 +131,7 @@ export default function LuksoComponent({
     ''
   )
   const [isAccountModalOpen, setIsAccountModalOpen] = useState<boolean>(false)
-  const [isLoadingMintedData, setIsLoadingMintedData] = useState<boolean>(
-    false
-  )
   const [{ wallet }] = useConnectWallet()
-
-  const [picture, setPicture] = useState<string>('');
 
   useEffect(() => {
     if (!wallet) return setProvider(undefined)
@@ -212,11 +206,6 @@ export default function LuksoComponent({
 
   }
 
-  async function takePicture() {
-    await Delay(2500);
-    setPicture(GetCanvasImageUrl());
-  }
-
   async function getFeatureList() {
     const result = await GetAssetsListByCampaign(campaignParams?.campaign)
     featureList = result.success ? result.value : undefined
@@ -288,9 +277,9 @@ export default function LuksoComponent({
   }
 
   async function loadSingleData() {
+    setIsLoading(true)
     // Iterate the features
     // Place the features on the model
-    setIsLoadingMintedData(true)
     if (singleInitData === undefined)
       return void LogError(Module.Lukso, 'Missing single data!!!!!')
 
@@ -309,9 +298,7 @@ export default function LuksoComponent({
         campaignParams?.config.skin?.defColor ?? 'ffffff'
       )
     }
-
-    await takePicture();
-    setIsLoadingMintedData(false)
+    setIsLoading(false)
 
   }
 
@@ -590,7 +577,6 @@ export default function LuksoComponent({
                 exportModel={() => exportModel()}
                 provider={provider}
                 features={singleInitData?.features}
-                picture={picture}
                 combination={selectedCombination} combinationPictureUrl={combinationPictureUrl}
                 onClickBackButton={() => {
                   setSelectedCombination(undefined)
