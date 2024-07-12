c02d7d0b6fb35c686269ce5ca95ff7c55a52f34a
nan <ccmolinac@gmail.com>
2024-07-11 19:17:00 -0500

    Add reveal animation to onMount behavior on featureList UI

--- ui/lukso/common/featureList.ui.tsx
+++ ui/lukso/common/featureList.ui.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useRef } from "react";
 import { RandomTier } from "../../../enums/common.enum";
-import { IndexFeatureInterface } from "../../../interfaces/api.interface"
+import { IndexFeatureInterface } from "../../../interfaces/api.interface";
+import { gsap } from "gsap";
 
 interface FeatureListUIProps {
   features: IndexFeatureInterface[]
@@ -25,12 +27,27 @@ function GetRaretyColor(opc: RandomTier) {
 }
 
 export default function FeatureListUI({features}: FeatureListUIProps) {
+  const DOM = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (DOM.current) {
+      const arrayEl = gsap.utils.toArray(DOM.current.children);
+      console.log(arrayEl);
+      
+      gsap.to(arrayEl, {
+        opacity: 1,
+        duration: 1,
+        stagger: 0.2
+      })
+    }
+  }, [])
   return (
-    <div className="grid grid-cols-2 py-10 gap-4 gap-x-14 text-sm 2xl:text-base w-full">
+    <div ref={DOM} className={`grid grid-cols-2 py-10 gap-4 gap-x-14 text-sm 2xl:text-base w-full`}> 
+
       {
         features.map((feature, index) => {
           return (
-            <div key={index}>
+            <div key={index} className=" opacity-0">
               <h3 className={`uppercase font-bold ${GetRaretyColor(feature.val.tier)}`}>{feature.val.type}</h3>
               <p className={`lowercase text-lg`}>{feature.val.name}</p>
               <p className={`capitalize text-sm leading-none ${GetRaretyColor(feature.val.tier)}`}>{feature.val.tier ?? '-'}</p>
