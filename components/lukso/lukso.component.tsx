8b3001a76b91470528ffbb226d43d08083223721
GabCh15 <info@thedac.info>
2024-08-16 16:14:24 -0500

    Fix for tokenId on avatar related files

--- components/lukso/lukso.component.tsx
+++ components/lukso/lukso.component.tsx
@@ -73,6 +73,7 @@ import ConnectWeb3Button from '../web3/connectWeb3.component'
 import { uploadMetadata } from '../../utils/metadata.util'
 import Toastify from 'toastify-js'
 import { SelectableCampaign } from '../../types/common.type'
+import { FileCampaignNameLabel } from '../../constants/lukso/labels.constant'
 
 const exportData: ExportInterface = { attributes: [] }
 let optionList: FeatureInterface[] | undefined
@@ -540,11 +541,12 @@ export default function LuksoComponent({
     console.log("EXPORTING VRM, GLB and image...")
     const modelVRM = modelVRMPromise.success ? modelVRMPromise.value : undefined;
     const modelGLB = modelGLBPromise.success ? modelGLBPromise.value : undefined;
+    const filesName = FileCampaignNameLabel[campaignParams?.campaign as Campaign] + selectedTokenId
     if (modelVRMPromise.success && modelGLBPromise.success) {
       const refinedModelVRM = await PostRequestVRMProcessFile(modelVRM as Blob)
-      await SaveFile(refinedModelVRM, `avatar.vrm`);
-      await SaveFile(modelGLB, `model.glb`);
-      await SaveFile(picturePromise, 'picture.png');
+      await SaveFile(refinedModelVRM, `${filesName}.vrm`);
+      await SaveFile(modelGLB, `${filesName}.glb`);
+      await SaveFile(picturePromise, `${filesName}.png`);
     }
   }
 
---
+++ constants/lukso/labels.constant.ts
@@ -0,0 +1,4 @@
+export const FileCampaignNameLabel = {
+    vrm_male: 'Lukso_Citizen_#',
+    vrm_female:'Lukso_Creator_#'
+}
