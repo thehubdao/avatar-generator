1f14ac98e926a0d6ed6d7898aa5933637a4324da
nan <ccmolinac@gmail.com>
2024-09-25 23:23:40 -0500

    Create SVG components

---
+++ ui/citizens/common/SVG/plusSVG.ui.tsx
@@ -0,0 +1,7 @@
+export default function PlusSVG() {
+  return (
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M14 6.22222L7.77778 6.22222L7.77778 -2.71982e-07L6.22222 -3.39977e-07L6.22222 6.22222L-2.71982e-07 6.22222L-3.39977e-07 7.77778L6.22222 7.77778L6.22222 14L7.77778 14L7.77778 7.77778L14 7.77778L14 6.22222Z" fill="#FFFFFF" />
+    </svg>
+  )
+}
