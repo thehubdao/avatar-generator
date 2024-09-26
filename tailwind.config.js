c20ba02cca0be628475fb6a93d210b048fda34fa
nan <ccmolinac@gmail.com>
2024-09-25 23:24:44 -0500

    Add shadow styles to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,8 @@
           }
         },
         boxShadow: {
+          'citizens-btn':'inset 0px 1px 0px 0px #FFFFFF1A, inset 0px -1px 0px 0px #FFFFFF0D',
+          'citizens-img':'16px 12px 32px 0px #0C0D0F, -12px -8px 32px 0px #24262B',
           'flat-soft':'-2.81481px -2.81481px 6.6296px #FFFFFF, 2.81481px 2.81481px 6.6296px #D3D9E6',
           'flat-medium':'-5.81481px -5.81481px 12.6296px #FFFFFF, 5.81481px 5.81481px 12.6296px #D3D9E6',
           'flat-hard':'-8.78261px -8.78261px 17.5652px #FFFFFF, 8.78261px 8.78261px 17.5652px #D3D9E6',
