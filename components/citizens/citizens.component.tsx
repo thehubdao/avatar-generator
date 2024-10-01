214884f3b8baeec127f72dd78e8c2986177db0c9
nan <ccmolinac@gmail.com>
2024-10-01 17:37:57 -0500

    Improve layout and state flow based on the Figma

--- components/citizens/citizens.component.tsx
+++ components/citizens/citizens.component.tsx
@@ -3,22 +3,56 @@ import { CampaignParameters } from "../../interfaces/common.interface";
 import MobileLayout from "../../layouts/mobile.layout";
 import { Campaign, TokenId } from "../../types/metadata.type";
 import LoginUI from "../../ui/citizens/sections/login.ui";
-import CampaignList from "../../ui/citizens/common/campaignList.ui";
 import Image from "next/image";
 import ConnectButton from "../../ui/citizens/common/connectButton.ui";
 import AvatarEditor from "../avatar/editor.component";
 import CitizensUI from "../../ui/citizens/citizens.ui";
 import { useConnectWallet } from "@web3-onboard/react";
 import { getCampaignsTokenIds } from "../../utils/web3/contract.util";
+import { CitizensCollection } from "../../interfaces/citizens.interface";
+import Button from "../../ui/citizens/common/button.ui";
+import ArrowLinkSVG from "../../ui/citizens/common/SVG/arrowLinkSVG.ui";
+import SocialDiscordSVG from "../../ui/citizens/common/SVG/socialDiscordSVG.ui";
+import Link from "next/link";
+import SocialInstagramSVG from "../../ui/citizens/common/SVG/socialInstagramSVG.ui";
+import SocialXSVG from "../../ui/citizens/common/SVG/socialXSVG.ui";
+
+const COLLECTIONS: CitizensCollection[] = [
+  {
+    name: 'Collection 01',
+    image: 'https://lipsum.app/id/24/280x300/'
+  },
+  {
+    name: 'Collection 02',
+    image: 'https://lipsum.app/id/25/280x300/'
+  },
+  {
+    name: 'Collection 03',
+    image: 'https://lipsum.app/id/26/280x300/'
+  }
+  // {
+  //   name: 'Collection 04',
+  //   image: 'https://lipsum.app/id/27/280x300/'
+  // },
+  // {
+  //   name: 'Collection 05',
+  //   image: 'https://lipsum.app/id/28/280x300/'
+  // },
+  // {
+  //   name: 'Collection 06',
+  //   image: 'https://lipsum.app/id/29/280x300/'
+  // }
+]
 
 interface CitizensComponentProps {
   campaignParams?: CampaignParameters;
   setCampaign: (campaign: Campaign | undefined) => void;
 }
 
-export default function CitizensComponent({ campaignParams, }: CitizensComponentProps) {
+export default function CitizensComponent({ campaignParams, setCampaign }: CitizensComponentProps) {
   const [isSigned, setIsSigned] = useState<boolean>(false); // false: log out, true: logged in
-  const [selectedCombination] = useState<string>('');
+  const [collectionList] = useState<CitizensCollection[] | null | undefined>(); // collections to show before login, it controls the view flow: undefined: loading state, null: error getting data, CitizensCollection[]: show collections
+  const [selectedCombination] = useState<string>('vrm_female');
 
 
   const [walletAddress, setWalletAddress] = useState<string | undefined>(undefined)
@@ -28,10 +62,10 @@ export default function CitizensComponent({ campaignParams, }: CitizensComponent
   const [, setTokenIdList] = useState<TokenId[]>()
 
 
-  
+
   useEffect(() => {
     console.log('Campaign params: ', campaignParams);
-    // setCampaign('vrm_female');
+    setCampaign('vrm_female');
   }, [])
 
   useEffect(() => {
@@ -40,16 +74,16 @@ export default function CitizensComponent({ campaignParams, }: CitizensComponent
     const address = wallet.accounts[0].address
 
     setWalletAddress(address)
-}, [isSigned])
+  }, [isSigned])
 
-useEffect(() => {
-  if (!walletAddress) return
-  const getTokensMetadataPromise = async () => {
+  useEffect(() => {
+    if (!walletAddress) return
+    const getTokensMetadataPromise = async () => {
       const tokenIds = await getCampaignsTokenIds(walletAddress)
       setTokenIdList(tokenIds)
-  }
-  void getTokensMetadataPromise()
-}, [walletAddress])
+    }
+    void getTokensMetadataPromise()
+  }, [walletAddress])
 
   async function onAvatarBuilderReady(
     currentCombination: string,
@@ -58,7 +92,7 @@ useEffect(() => {
     if (!campaign) return
 
     console.log('onAvatarBuilderReady');
-    
+
     // await Promise.all([
     //   getStageList(),
     //   getEnvironmentMapList(),
@@ -105,59 +139,49 @@ useEffect(() => {
   return (
     <MobileLayout>
       <div className="w-full min-h-screen bg-[#202020] font-work">
-
         {!isSigned ?
-          <LoginUI />
+          <LoginUI collections={collectionList} />
           :
-          !selectedCombination ?
-            <div className="pt-[25vh]">
-              <CampaignList />
-              <div className="grid justify-items-center py-20">
-                <h1 className="font-monument text-7xl text-white text-center">CITIZENS PORTAL</h1>
-                <p className="text-2xl text-white">The home of creators in the 3D Web</p>
+          campaignParams && campaignParams.campaign ?
+            <>
+              <div>
+                <AvatarEditor
+                  avatarBasePath={
+                    campaignParams.armature
+                  }
+                  editMode={false}
+                  lights={
+                    campaignParams.config.lights
+                  }
+                  defaultShadow={
+                    campaignParams.config
+                      .defShadow
+                  }
+                  defaultCamera={
+                    campaignParams.config.defCam
+                  }
+                  postProcessing={
+                    campaignParams.config
+                      .postProcessing
+                  }
+                  onReady={() =>
+                    onAvatarBuilderReady(
+                      selectedCombination,
+                      campaignParams.campaign
+                    )
+                  }
+                />
               </div>
-            </div>
+              <CitizensUI />
+            </>
             :
-            campaignParams && campaignParams.campaign ?
-              <>
-                <div>
-                  <AvatarEditor
-                    avatarBasePath={
-                      campaignParams.armature
-                    }
-                    editMode={false}
-                    lights={
-                      campaignParams.config.lights
-                    }
-                    defaultShadow={
-                      campaignParams.config
-                        .defShadow
-                    }
-                    defaultCamera={
-                      campaignParams.config.defCam
-                    }
-                    postProcessing={
-                      campaignParams.config
-                        .postProcessing
-                    }
-                    onReady={() =>
-                      onAvatarBuilderReady(
-                        selectedCombination,
-                        campaignParams.campaign
-                      )
-                    }
-                  />
-                </div>
-                <CitizensUI />
-              </>
-              :
-              <div className="w-full h-screen flex justify-center items-center">
-                <p className="font-monument text-white">Something is wrong!</p>
-              </div>
+            <div className="w-full h-screen flex justify-center items-center">
+              <p className="font-monument text-white">Something is wrong!</p>
+            </div>
         }
 
         {/* HEADER */}
-        <div className="fixed w-fit h-fit left-4 top-8">
+        <div className="fixed w-fit h-fit left-6 top-8">
           <Image
             src='/resources/images/the-hub-logo-white.svg'
             alt="the hub icon"
@@ -165,9 +189,29 @@ useEffect(() => {
             height={32}
           />
         </div>
-        <div className="fixed top-8 right-4">
-          <ConnectButton isSigned={isSigned} setIsSigned={(isSigned)=>{setIsSigned(isSigned)}} address={walletAddress}/>
-        </div>
+        {(collectionList || collectionList === null || isSigned) &&
+          <div className="fixed top-8 right-6 flex gap-4">
+            {!isSigned &&
+              <Button label="About" handleClick={() => { }} withIcon textStiles="text-start pl-2">
+                <ArrowLinkSVG />
+              </Button>
+            }
+            <ConnectButton isSigned={isSigned} setIsSigned={(isSigned) => { setIsSigned(isSigned) }} address={walletAddress} />
+          </div>
+        }
+        {!isSigned &&
+          <div className="flex gap-4 fixed bottom-8 right-6">
+            <Link href={'https://x.com/thehub_dao'}>
+              <SocialXSVG />
+            </Link>
+            <Link href={'https://www.instagram.com/thehub_dao/'}>
+              <SocialInstagramSVG />
+            </Link>
+            <Link href={'https://discord.gg/3KvUpQayxp'}>
+              <SocialDiscordSVG />
+            </Link>
+          </div>
+        }
       </div>
     </MobileLayout>
   )
--- ui/citizens/sections/login.ui.tsx
+++ ui/citizens/sections/login.ui.tsx
@@ -1,10 +1,39 @@
-export default function LoginUI() {
+import { CitizensCollection } from "../../../interfaces/citizens.interface";
+import CampaignList from "../common/campaignList.ui";
+
+interface LoginUIProps {
+  collections?: CitizensCollection[] | null;
+}
+
+export default function LoginUI({ collections }: LoginUIProps) {
   return (
-    <div className="font-work w-full h-screen flex justify-center items-center bg-citizens-dark">
-      <div className="grid justify-items-center">
-        <h1 className="font-monument text-7xl text-white text-center">CITIZENS<br /><span className="text-[83px]">PORTAL</span></h1>
-        <p className="text-2xl text-white">The home of creators in the 3D Web</p>
-      </div>
-    </div>
+    <>
+      {
+        collections ?
+          <div className="pt-[15vh] 2xl:pt-[25vh]">
+            <CampaignList collections={collections} />
+            <div className="grid justify-items-center pt-4 2xl:pt-10">
+              <h1 className="font-monument text-7xl text-white text-center">CITIZENS PORTAL</h1>
+              <p className="text-2xl text-white">The home of creators in the 3D Web</p>
+            </div>
+          </div>
+          :
+          <div className="font-work w-full h-screen flex justify-center items-center">
+            <div className="relative grid justify-items-center">
+              <h1 className="font-monument text-7xl text-white text-center">CITIZENS<br /><span className="text-[83px]">PORTAL</span></h1>
+              <p className="text-2xl text-white">The home of creators in the 3D Web</p>
+              <div className="absolute top-full pt-8">
+                {
+                  collections === null ?
+                    <p className="font-light text-xs text-white">:( Sorry, something is wrong, come back later!</p>
+                    :
+                    <div className="w-4 h-4 border-t rounded-full animate-spin"></div>
+                }
+              </div>
+            </div>
+          </div>
+      }
+
+    </>
   )
 }
