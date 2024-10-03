4fcf820dd0b30bef1e7e6c88133074043f8b1e29
nan <ccmolinac@gmail.com>
2024-10-02 23:04:52 -0500

    Add sections behavior on citicens UI

--- ui/citizens/citizens.ui.tsx
+++ ui/citizens/citizens.ui.tsx
@@ -1,21 +1,30 @@
-import Button from "./common/button.ui";
+import { CitizensSections } from "../../enums/citizens/common.enum";
 import DetailsUI from "./common/details.ui";
 import Notifications from "./common/notifications.ui";
+import Collection from "./sections/collection.ui";
 
-export default function CitizensUI() {
+interface CitizensUIProps {
+  currentSection: CitizensSections;
+}
+
+export default function CitizensUI({ currentSection }: CitizensUIProps) {
   return (
     <>
-      {/* nav */}
-      <div className="fixed top-8 left-1/2 -translate-x-1/2 flex gap-4">
-        <Button label="backpack" withIcon handleClick={() => { }} />
-        <Button label="collection" withIcon handleClick={() => { }} />
-        <Button label="leaderboard" withIcon handleClick={() => { }} />
-        <Button label="play" withIcon handleClick={() => { }} />
-      </div>
-      {/* details */}
-      <DetailsUI />
-      {/* notifications */}
-      <Notifications />
+      {
+        currentSection === CitizensSections.View &&
+        <>
+          {/* details */}
+          <DetailsUI />
+          {/* notifications */}
+          <Notifications />
+        </>
+      }
+      {
+        currentSection === CitizensSections.Collection &&
+        <>
+          <Collection />
+        </>
+      }
     </>
   )
 }
