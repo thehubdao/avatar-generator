b3fef04b482675536d789a4023fe06005606ab8f
nan <ccmolinac@gmail.com>
2024-10-02 23:04:00 -0500

    Add small and light props and behaviors to campaignCard

--- ui/citizens/common/campaignCard.ui.tsx
+++ ui/citizens/common/campaignCard.ui.tsx
@@ -4,16 +4,18 @@ interface CampaignCardProps {
   title: string;
   imgSrc: string;
   imgAlt: string;
+  light?: boolean;
+  small?: boolean;
 }
 
-export default function CampaignCard({ title, imgSrc, imgAlt }: CampaignCardProps) {
+export default function CampaignCard({ title, imgSrc, imgAlt, light = false, small = false }: CampaignCardProps) {
   return (
-    <div className="w-[280px] h-[388px] rounded-2xl overflow-hidden cursor-pointer">
-      <div className="relative w-full h-[300px]">
+    <div className={`${small ? 'w-[228px] h-[272px]':'w-[280px] h-[388px]'} rounded-2xl overflow-hidden cursor-pointer`}>
+      <div className={`relative w-full ${small ? 'h-[228px]':'h-[300px]'}`}>
         <Image src={imgSrc} alt={imgAlt} fill className="object-cover" />
       </div>
-      <div className="bg-citizens-dark h-[88px] flex justify-center items-center">
-        <p className="text-center text-white text-xl truncate px-4">{title}</p>
+      <div className={`${light ? 'bg-white/75':'bg-citizens-dark'} ${small ? 'h-[44px]':'h-[88px]'} flex justify-center items-center`}>
+        <p className={`${light ? 'text-citizens-dark':'text-white'} text-center ${small ? 'text-sm':'text-xl'} truncate px-4`}>{title}</p>
       </div>
     </div>
   )
