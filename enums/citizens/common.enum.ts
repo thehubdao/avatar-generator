bf6f49e063c443a1fbd8492bc44a94d11fcbe177
nan <ccmolinac@gmail.com>
2024-10-02 23:03:13 -0500

    create citizens common enums

---
+++ enums/citizens/common.enum.ts
@@ -0,0 +1,10 @@
+export enum CitizensSections {
+  View,
+  Collection
+}
+
+export enum TheHubSocialLinks {
+  SocialX = 'https://x.com/thehub_dao',
+  SocialInstagram = 'https://www.instagram.com/thehub_dao/',
+  Discord = 'https://discord.gg/3KvUpQayxp'
+}
