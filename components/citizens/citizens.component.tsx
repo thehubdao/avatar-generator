214f3e7a6c33693a5db286f093e16518d30e1650
nan <ccmolinac@gmail.com>
2024-10-05 16:27:26 -0500

    Add campaign setup, export model and loading campaign behaviors to citizen component

--- components/citizens/citizens.component.tsx
+++ components/citizens/citizens.component.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import { CampaignParameters } from "../../interfaces/common.interface";
+import { BasicData, CampaignParameters, ExportInterface } from "../../interfaces/common.interface";
 import MobileLayout from "../../layouts/mobile.layout";
-import { Campaign, TokenId } from "../../types/metadata.type";
+import { Campaign, CampaignDrops, TokenId, TokenMetadata } from "../../types/metadata.type";
 import LoginUI from "../../ui/citizens/sections/login.ui";
 import Image from "next/image";
 import ConnectButton from "../../ui/citizens/common/connectButton.ui";
-import AvatarEditor from "../avatar/editor.component";
+import AvatarEditor, { ChangeFeature, ChangeSkinColor, ChangeStartAnimation, GetAvatarGLB, SetEnvironment, SetFeaturesData } from "../avatar/editor.component";
 import CitizensUI from "../../ui/citizens/citizens.ui";
 import { useConnectWallet } from "@web3-onboard/react";
 import { getCampaignsTokenIds } from "../../utils/web3/contract.util";
@@ -17,6 +17,14 @@ import Link from "next/link";
 import SocialInstagramSVG from "../../ui/citizens/common/SVG/socialInstagramSVG.ui";
 import SocialXSVG from "../../ui/citizens/common/SVG/socialXSVG.ui";
 import { CitizensSections, TheHubSocialLinks } from "../../enums/citizens/common.enum";
+import { FetchBlob, GetAccessoryListByCampaign, GetAnimationByCampaignAndName, GetAssetsListByCampaign, GetAvatarSingleByCampaignCombination, GetAvatarSingleByCampaignCombinationString, GetEnvMapListByCampaign, GetStageListByCampaign } from "../../utils/api.util";
+import { EnvMapInterface, FeatureInterface, SingleInterface, StageInterface } from "../../interfaces/api.interface";
+import { FilterList, LogError, MixArrays } from "../../utils/common.util";
+import { Module } from "../../enums/common.enum";
+import { BodyPart } from "../../types/avatar.type";
+import { fileCampaignNameLabel } from "../../constants/lukso/labels.constant";
+import { SaveFile } from "../../utils/exporter.util";
+import { StorageLocation } from "../../enums/firebase.enum";
 
 const COLLECTIONS: CitizensCollection[] = [
   {
@@ -45,30 +53,90 @@ const COLLECTIONS: CitizensCollection[] = [
   // }
 ]
 
+const exportData: ExportInterface = { attributes: [] }
+let optionList: FeatureInterface[] | undefined
+let featureList: FeatureInterface[] | undefined
+let accessoryList: FeatureInterface[] | undefined
+let stageList: StageInterface[] | undefined
+let envMapList: EnvMapInterface[] | undefined
+let singleInitData: SingleInterface | undefined
+
+const tokenMetadata: TokenMetadata = {
+  name: '',
+  description: '',
+  GLBUrl: '',
+  body: {},
+  links: [],
+  assets: [],
+  tokenId: '',
+  campaign: '',
+  imageUrl: '',
+  combination: '',
+  images: [],
+  baseCombination: '',
+  fallbackImageUrl: '',
+}
+
 interface CitizensComponentProps {
   campaignParams?: CampaignParameters;
   setCampaign: (campaign: Campaign | undefined) => void;
 }
 
 export default function CitizensComponent({ campaignParams, setCampaign }: CitizensComponentProps) {
-  const [isSigned, setIsSigned] = useState<boolean>(false); // false: log out, true: logged in
+  const [isSigned, setIsSigned] = useState<boolean>(true); // false: log out, true: logged in
   const [collectionList] = useState<CitizensCollection[] | null | undefined>(COLLECTIONS); // collections to show before login, it controls the view flow: undefined: loading state, null: error getting data, CitizensCollection[]: show collections
-  const [selectedCombination] = useState<string>('vrm_female');
 
-  const [currentSection, setCurrentSection] = useState<CitizensSections>(CitizensSections.Collection);
-
+  const [currentSection, setCurrentSection] = useState<CitizensSections>(CitizensSections.View);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const [walletAddress, setWalletAddress] = useState<string | undefined>(undefined)
 
   const [{ wallet }] = useConnectWallet()
 
+  // Edit state
+  // const [optionListShow, setOptionListShow] = useState<FeatureInterface[]>()
+  // const [dropList, setDropList] = useState<CampaignDrops>({
+  //   vrm_male: [],
+  //   vrm_female: [],
+  // })
+  // const [isEditModeSelected, setIsEditModeSelected] = useState<boolean>(false)
+  // const [selectedOpc, setSelectedOpc] = useState<BasicData[]>(
+  //   exportData.attributes
+  // )
+  // const [skinColor, setSkinColor] = useState<string>(
+  //   campaignParams?.config.skin?.defColor ?? 'FFFFFF'
+  // )
+  // const [selectedCategory, setSelectedCategory] = useState<string>('head')
+  // const [tokenIdList, setTokenIdList] = useState<TokenId[]>()
+  // const [selectedCombination, setSelectedCombination] = useState<string>('vrm_male')
+  // const [selectedBaseCombination, setSelectedBaseCombination] = useState<
+  //   string
+  // >()
+  // const [selectedTokenId, setSelectedTokenId] = useState<number>()
+  // const [selectedMetadata, setSelectedMetadata] = useState<TokenMetadata>()
+
+  const [, setOptionListShow] = useState<FeatureInterface[]>()
+  const [dropList,] = useState<CampaignDrops>({
+    vrm_male: [],
+    vrm_female: [],
+  })
+  const [isEditModeSelected,] = useState<boolean>(false)
+  const [, setSelectedOpc] = useState<BasicData[]>(
+    exportData.attributes
+  )
+  const [selectedCategory,] = useState<string>('head')
   const [tokenIdList, setTokenIdList] = useState<TokenId[]>()
+  const [selectedCombination,] = useState<string>('vrm_male')
+  const [selectedBaseCombination,] = useState<string>()
+  const [selectedTokenId, ] = useState<number>()
+
+  const [combinationPictureUrl, ] = useState<string>('')
 
 
 
   useEffect(() => {
     console.log('Campaign params: ', campaignParams);
-    setCampaign('vrm_female');
+    setCampaign('vrm_male');
   }, [])
 
   useEffect(() => {
@@ -96,47 +164,222 @@ export default function CitizensComponent({ campaignParams, setCampaign }: Citiz
 
     console.log('onAvatarBuilderReady');
 
-    // await Promise.all([
-    //   getStageList(),
-    //   getEnvironmentMapList(),
-    //   getSingleInfo(),
-    //   getSingleData(campaign, currentCombination),
-    // ])
+    await Promise.all([
+      getStageList(),
+      getEnvironmentMapList(),
+      getSingleInfo(),
+      getSingleData(campaign, currentCombination),
+    ])
 
-    // await SetFeaturesData(campaignParams?.features ?? [])
+    await SetFeaturesData(campaignParams?.features ?? [])
 
-    // const bgMap = envMapList?.find(
-    //   (em) => em.name === campaignParams?.config.envMap?.defBgMap
-    // )
-    // const lightMap = envMapList?.find(
-    //   (em) => em.name === campaignParams?.config.envMap?.defLightMap
-    // )
-    // await SetEnvironment(
-    //   bgMap?.path,
-    //   lightMap?.path,
-    //   campaignParams?.config.envMap?.skyboxConfig
-    // )
+    const bgMap = envMapList?.find(
+      (em) => em.name === campaignParams?.config.envMap?.defBgMap
+    )
+    const lightMap = envMapList?.find(
+      (em) => em.name === campaignParams?.config.envMap?.defLightMap
+    )
+    await SetEnvironment(
+      bgMap?.path,
+      lightMap?.path,
+      campaignParams?.config.envMap?.skyboxConfig
+    )
 
     // Set features from single
-    // await loadSingleData()
+    await loadSingleData()
 
     // Set skin tone
-    // await ChangeSkinColor(campaignParams?.config.skin?.defColor ?? 'ffffff')
+    await ChangeSkinColor(campaignParams?.config.skin?.defColor ?? 'ffffff')
 
     // Set animation
-    // const result = await GetAnimationByCampaignAndName(
-    //   campaignParams?.campaign,
-    //   campaignParams?.config.defAnimation
-    // )
+    const result = await GetAnimationByCampaignAndName(
+      campaignParams?.campaign,
+      campaignParams?.config.defAnimation
+    )
 
-    // if (result.success) {
-    //   await ChangeStartAnimation(result.value.at(0)?.path)
-    // }
+    if (result.success) {
+      await ChangeStartAnimation(result.value.at(0)?.path)
+    }
 
-    // fade loader view
-    // await handleFadeLoader(loaderDivElement, () => {
-    //   setIsLoading(false)
-    // })
+    setIsLoading(false);
+  }
+
+  async function loadSingleData() {
+    setIsLoading(true)
+    // Iterate the features
+    // Place the features on the model
+    if (singleInitData === undefined)
+      return void LogError(Module.Lukso, 'Missing single data!!!!!')
+
+    for (const { val } of singleInitData.features) {
+      const { id, path, type, name } = val
+      const bodyIndex: keyof typeof tokenMetadata.body = val.type.toLowerCase() as keyof typeof tokenMetadata.body
+      tokenMetadata.body[bodyIndex] = val as BodyPart
+      // Set feature on model
+      await ChangeFeature(
+        id,
+        path,
+        name,
+        type,
+        campaignParams?.config.skin?.defColor ?? 'ffffff'
+      )
+    }
+  }
+
+  function addReplaceAttribute(addId: string, addValue: string) {
+    if (exportData && exportData.attributes.some((x) => x.id === addId)) {
+      const oldAttribute = exportData.attributes.find(
+        (x) => x.id === addId
+      )
+      if (oldAttribute) oldAttribute.val = addValue
+
+      setSelectedOpc([...exportData.attributes])
+      return
+    }
+
+    exportData?.attributes.push({ id: addId, val: addValue })
+    setSelectedOpc([...exportData.attributes])
+  }
+
+  async function getFeatureList() {
+    const result = await GetAssetsListByCampaign(campaignParams?.campaign)
+    featureList = result.success ? result.value : undefined
+    optionList = MixArrays(optionList, featureList)
+    optionList = MixArrays(optionList, accessoryList)
+
+    const combinationIndexes = selectedBaseCombination?.split('-')
+    let filteredOptionList: FeatureInterface[] = []
+    //This algorithm can be done in a better way, change it in the future.
+    // Get features from combination and make them visible on avatar edit mode.
+    combinationIndexes?.forEach((featureIndex: string, index) => {
+      const filteredArray = optionList?.filter((val) => {
+        const categoryIndex = campaignParams?.features?.find(
+          (category) => {
+            return category.displayName === val.type
+          }
+        )?.index
+
+        if (!categoryIndex) return
+
+        return (
+          categoryIndex - 1 === index &&
+          val.index.toString() === featureIndex
+        )
+      })
+
+      if (!filteredArray) return
+
+      filteredOptionList = filteredOptionList.concat(filteredArray)
+    })
+
+    const campaignDropList =
+      dropList[campaignParams?.campaign as keyof typeof dropList]
+    if (campaignDropList) {
+      const formattedDropList = campaignDropList
+        .map((val) => {
+          return optionList?.find(
+            (option) =>
+              option.type === val.type &&
+              val.index === option.index
+          ) as FeatureInterface
+        })
+        .filter((val) => {
+          const categoryIndex = campaignParams?.features?.find(
+            (category) => {
+              return category.displayName === val.type
+            }
+          )?.index
+
+          if (!categoryIndex || !combinationIndexes) return true
+
+          return !combinationIndexes[categoryIndex - 1]?.includes(
+            val.index.toString()
+          )
+        })
+      filteredOptionList = filteredOptionList.concat(formattedDropList)
+    }
+
+    optionList = filteredOptionList
+    const filteredList = FilterList(optionList, 'type', selectedCategory)
+    return setOptionListShow(filteredList)
+  }
+
+  async function getAccessoryList() {
+    const result = await GetAccessoryListByCampaign(
+      campaignParams?.campaign
+    )
+    accessoryList = result.success ? result.value : undefined
+  }
+
+  async function getStageList() {
+    const result = await GetStageListByCampaign(campaignParams?.campaign)
+    stageList = result.success ? result.value : undefined
+  }
+
+  async function getEnvironmentMapList() {
+    if (!campaignParams?.campaign) return
+    const result = await GetEnvMapListByCampaign(campaignParams?.campaign)
+    envMapList = result.success ? result.value : undefined
+  }
+
+  async function getSingleInfo() {
+    if (
+      campaignParams?.config.defAvatarCombination == undefined ||
+      !campaignParams?.campaign
+    )
+      return
+
+    const result = await GetAvatarSingleByCampaignCombination(
+      campaignParams?.campaign,
+      campaignParams.config.defAvatarCombination
+    )
+    singleInitData = result.success ? result.value : undefined
+  }
+
+  async function getSingleData(campaign: string, combination: string) {
+    const numResult = await GetAvatarSingleByCampaignCombinationString(
+      campaign,
+      combination
+    )
+    const result: SingleInterface | undefined = numResult.success
+      ? numResult.value
+      : undefined
+    singleInitData = result
+    await getAccessoryList()
+    await getFeatureList()
+
+    singleInitData?.features.forEach((feature) => {
+      addReplaceAttribute(feature.val.type, feature.val.name)
+    })
+  }
+
+  async function exportModel() {
+    exportData.attributesBase64 = window.btoa(
+      JSON.stringify(exportData.attributes)
+    )
+    const vrmStorageUrl = `https://firebasestorage.googleapis.com/v0/b/avatar-generator-e430b.appspot.com/o/${campaignParams?.campaign}%2F${StorageLocation.AvatarVrms}%2F${selectedCombination}.vrm?alt=media&token=ad2e1e79-6c26-4284-92c3-2e42f5166b42`
+    const [
+      picturePromise,
+      modelGLBPromise,
+      modelVRMPromise,
+    ] = await Promise.all([
+      FetchBlob(combinationPictureUrl),
+      GetAvatarGLB(),
+      FetchBlob(vrmStorageUrl),
+    ])
+    console.log('EXPORTING VRM, GLB and image...')
+    const modelVRM = modelVRMPromise
+    const modelGLB = modelGLBPromise.success
+      ? modelGLBPromise.value
+      : undefined
+    const filesName =
+      fileCampaignNameLabel[campaignParams?.campaign as Campaign] +
+      selectedTokenId
+    if (modelVRM && modelGLBPromise.success) {
+      await SaveFile(modelVRM, `${filesName}.vrm`)
+      await SaveFile(modelGLB, `${filesName}.glb`)
+      await SaveFile(picturePromise, `${filesName}.png`)
+    }
   }
 
   return (
@@ -152,7 +395,7 @@ export default function CitizensComponent({ campaignParams, setCampaign }: Citiz
                   avatarBasePath={
                     campaignParams.armature
                   }
-                  editMode={false}
+                  editMode={isEditModeSelected}
                   lights={
                     campaignParams.config.lights
                   }
@@ -175,11 +418,19 @@ export default function CitizensComponent({ campaignParams, setCampaign }: Citiz
                   }
                 />
               </div>
-              <CitizensUI currentSection={currentSection} tokenIdList={tokenIdList} />
+              {/* LOADER */}
+              {
+                isLoading &&
+                <div className="fixed inset-0 w-full h-screen flex flex-col justify-center items-center gap-4 bg-gradient-to-b from-[#151515] to-[#0C0C0C]">
+                  <p className=" text-white text-xl font-light">Loading Citizen</p>
+                  <div className="w-4 h-4 border-t rounded-full animate-spin"></div>
+                </div>
+              }
+              <CitizensUI currentSection={currentSection} tokenIdList={tokenIdList} features={singleInitData?.features} exportModel={() => exportModel()} />
             </>
             :
             <div className="w-full h-screen flex flex-col justify-center items-center gap-4">
-              <p className=" text-white text-xl font-light">Loading</p>
+              <p className=" text-white text-xl font-light">Loading Data</p>
               <div className="w-4 h-4 border-t rounded-full animate-spin"></div>
             </div>
         }
