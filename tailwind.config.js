d29737e920f62be4bc9d63a1daf113af24f64cff
nan <ccmolinac@gmail.com>
2024-10-10 19:33:39 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 ﻿module.exports = {
+    darkMode: 'class',
     plugins: [require('tailwindcss-neumorphism')],
     content: [
         './pages/**/*.{js,ts,jsx,tsx}',
@@ -38,6 +39,7 @@
           citizens: {
             dark: '#1b1b1d',
             gray: '#E6E6E6',
+            graylight: '#C6C6C7',
             blue: '#19B5F3',
             bluedark: '#1E293B' 
           }
@@ -47,8 +49,11 @@
           'citizens-img':'16px 12px 32px 0px #0C0D0F, -12px -8px 32px 0px #24262B',
           'citizens-input':'inset 0px 2px 2px 0px rgba(0,0,0,0.25), inset 0px -2px 2px 0px rgba(255,255,255,0.05)',
           'flat-soft':'-2.81481px -2.81481px 6.6296px #FFFFFF, 2.81481px 2.81481px 6.6296px #D3D9E6',
+          'flat-soft-dark':'-2.81481px -2.81481px 6.6296px #24262B, 2.81481px 2.81481px 6.6296px #0C0D0F',
           'flat-medium':'-5.81481px -5.81481px 12.6296px #FFFFFF, 5.81481px 5.81481px 12.6296px #D3D9E6',
+          'flat-medium-dark':'-5.81481px -5.81481px 12.6296px #24262B, 5.81481px 5.81481px 12.6296px #0C0D0F',
           'flat-hard':'-8.78261px -8.78261px 17.5652px #FFFFFF, 8.78261px 8.78261px 17.5652px #D3D9E6',
+          'flat-hard-dark':'-8.78261px -8.78261px 17.5652px #24262B, 8.78261px 8.78261px 17.5652px #0C0D0F',
           'inset-soft':'inset 1.3913px 1.3913px 2.78261px rgba(0,0,0,0.15), inset -1.3913px -1.3913px 2.78261px rgba(255,255,255,0.7)',
           'inset-medium':'inset 2.3913px 2.3913px 5.78261px rgba(0,0,0,0.15), inset -2.3913px -2.3913px 5.78261px rgba(255,255,255,0.7)',
           'inset-hard':'inset 4.3913px 4.3913px 8.78261px rgba(0,0,0,0.15), inset -4.3913px -4.3913px 8.78261px rgba(255,255,255,0.7)',
