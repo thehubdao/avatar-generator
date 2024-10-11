1f5af3a45d0ba95dfae743517e2265d8a1ace010
nan <ccmolinac@gmail.com>
2024-10-10 19:35:41 -0500

    Add dark styles to feature item selector

--- ui/avatar/selectors/featureItemSelector.ui.tsx
+++ ui/avatar/selectors/featureItemSelector.ui.tsx
@@ -21,8 +21,8 @@ export default function FeatureItemSelectorUI({ item, activeOpc, handleClick }: 
         selectFeature(event, item.displayName)
       }}
     >
-      <div className={`w-[84px] h-[84px] flex justify-center items-center rounded-xl cursor-pointer ${isActive ? 'bg-accent bg-opacity-80 shadow-inset-hard' : 'bg-bg shadow-flat-soft hover:shadow-flat-hard'} transition-all duration-500`} title={item.displayName}>
-        <p className="font-featuresIcons text-6xl text-gray-extralight">{item.iconUrl && item.iconUrl.length > 0 ? item.iconUrl : FeaturesIcons.Shirt}</p>
+      <div className={`w-[84px] h-[84px] flex justify-center items-center rounded-xl cursor-pointer ${isActive ? 'bg-accent dark:bg-citizens-dark bg-opacity-80 dark:bg-opacity-100 shadow-inset-hard dark:shadow-flat-soft-dark' : 'bg-bg dark:bg-citizens-dark shadow-flat-soft dark:shadow-flat-soft-dark hover:shadow-flat-hard hover:dark:shadow-flat-hard-dark'} transition-all duration-500`} title={item.displayName}>
+        <p className={`text-gray-extralight ${isActive ? 'dark:text-white':'dark:text-white/20'} font-featuresIcons text-6xl transition-colors duration-500`}>{item.iconUrl && item.iconUrl.length > 0 ? item.iconUrl : FeaturesIcons.Shirt}</p>
       </div>
     </div>
   )
