45fb8411e7adfc2fdaf39d13e44778480911a37f
nan <ccmolinac@gmail.com>
2024-10-16 15:04:14 -0500

    add modal styles prop to citizen modal

--- ui/citizens/common/modal.ui.tsx
+++ ui/citizens/common/modal.ui.tsx
@@ -3,13 +3,14 @@ import { ReactElement } from "react"
 interface ModalProps {
   children: ReactElement;
   handleClose: () => void;
+  modalStyles?: string;
 }
 
-export default function Modal({children, handleClose}: ModalProps) {
+export default function Modal({children, handleClose, modalStyles}: ModalProps) {
   return (
     <div className="fixed inset-0 w-full h-screen flex justify-center items-center">
       <div className="absolute w-full h-full bg-black/25 cursor-pointer" onClick={() => handleClose()}/>
-      <div className="w-[340px] min-h-[340px] rounded-2xl border-[2px] border-white/25 bg-black/25 backdrop-blur-sm px-8 py-12">
+      <div className={`w-[340px] min-h-[340px] ${modalStyles} rounded-2xl border-[2px] border-white/25 bg-black/25 backdrop-blur-sm px-8 py-12`}>
         {children}
       </div>
     </div>
