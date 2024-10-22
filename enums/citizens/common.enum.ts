b7ff5a8929b69fd122098a2bdde5e28a1e08201e
nan <ccmolinac@gmail.com>
2024-10-21 21:51:03 -0500

    Add citizens sections & create cardSize enum on citizens common enums

--- enums/citizens/common.enum.ts
+++ enums/citizens/common.enum.ts
@@ -1,10 +1,18 @@
 export enum CitizensSections {
   View,
-  Collection
+  Collection,
+  LeaderBoard,
+  Play
 }
 
 export enum TheHubSocialLinks {
   SocialX = 'https://x.com/thehub_dao',
   SocialInstagram = 'https://www.instagram.com/thehub_dao/',
   Discord = 'https://discord.gg/3KvUpQayxp'
+}
+
+export enum CardSize {
+  Small,
+  Medium,
+  Large
 }
