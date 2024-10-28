3b5e9c41c3a068f3aa2a2857e2d11e284cd6a5d9
nan <ccmolinac@gmail.com>
2024-10-28 15:33:05 -0500

    Create drop interface and delete unnecessary type declaration

--- interfaces/citizens.interface.ts
+++ interfaces/citizens.interface.ts
@@ -1,7 +1,20 @@
+import { PaymentType } from "../enums/citizens/common.enum";
 import { Campaign } from "../types/metadata.type";
 
 export interface CitizensCollection {
   name: string;
   image: string;
   campaign: Campaign;
+}
+
+export interface Drop {
+  id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  requiredXP: number;
+  paymentType: PaymentType;
+  price?: number;
+  requiredToken?: string;
+  contractAddress: string;
 }
