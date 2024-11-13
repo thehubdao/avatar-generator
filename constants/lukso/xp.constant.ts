4ffcff7a717bd46d30e8cef0f8d478e92abe89b8
DAC-info <info@thedac.info>
2024-11-13 12:16:58 -0500

    change citizens xp holding

--- constants/lukso/xp.constant.ts
+++ constants/lukso/xp.constant.ts
@@ -1,8 +1,8 @@
 export enum XPReward {
-    DailyLogin = 50,
-    WeeklyLoginStreak = 100,
-    NewFollower = 20,
-    NewFollowing = 10,
-    HeldCitizen = 101,
-    WearableHolding = 51
-  }
+  DailyLogin = 50,
+  WeeklyLoginStreak = 100,
+  NewFollower = 20,
+  NewFollowing = 10,
+  HeldCitizen = 12,
+  WearableHolding = 11
+}
