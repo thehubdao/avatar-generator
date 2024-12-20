30e8cfba078b1885ff8b03f0ca2e437770a1e14f
nan <ccmolinac@gmail.com>
2024-12-20 18:55:14 -0500

    Improve responsive layout in selector UI

--- ui/citizens/common/selector.ui.tsx
+++ ui/citizens/common/selector.ui.tsx
@@ -8,10 +8,11 @@ interface SelectorUIProps {
   list?: string[];
   label?: string;
   selection?: string;
+  dropDownPosition?: string;
   selectionHandler: (value?: string) => void;
 }
 
-export default function SelectorUI({ list, selection, label = 'CHOOSE', selectionHandler }: SelectorUIProps) {
+export default function SelectorUI({ list, selection, label = 'CHOOSE', dropDownPosition,  selectionHandler }: SelectorUIProps) {
   const menuDOM = useRef<HTMLDivElement>(null);
   
   const [isButtonHovered, setIsButtonHovered] = useState<boolean>(false);
@@ -27,15 +28,15 @@ export default function SelectorUI({ list, selection, label = 'CHOOSE', selectio
     <div ref={menuDOM} className="relative">
       <div onMouseEnter={() => setIsButtonHovered(true)}
         onMouseLeave={() => setIsButtonHovered(false)}>
-        <Button label={label} handleClick={() => setIsSelectorOpen(!isSelectorOpen)} withIcon textStiles="px-4">
-          <div className={`${isSelectorOpen ? 'rotate-180' : ''}`}>
+        <Button label={label} className="min-w-fit md:min-w-[214px]" handleClick={() => setIsSelectorOpen(!isSelectorOpen)} withIcon textStyles="sm:px-4 text-xs sm:text-lg" iconStyles={'w-[23px] sm:w-10 h-[16px] sm:h-[27px]'} >
+          <div className={`scale-75 sm:scale-100 ${isSelectorOpen ? 'rotate-180' : ''}`}>
             <ArrowSVG className="fill-white" />
           </div>
         </Button>
       </div>
-      <p className="absolute top-full right-0 px-2 mt-1 text-xs text-white/20">{campaignLabels[selection?.toLowerCase() as keyof typeof campaignLabels]?.dropdownName}</p>
+      <p className="absolute top-full left-0 sm:left-auto sm:right-0 px-2 sm:mt-1 text-xs text-white/20">{campaignLabels[selection?.toLowerCase() as keyof typeof campaignLabels]?.dropdownName}</p>
       {isSelectorOpen &&
-        <div className="absolute top-full w-full max-h-96 overflow-y-auto rounded-2xl bg-white mt-2 z-10">
+        <div className={`absolute top-full ${dropDownPosition} w-48 md:w-full max-h-96 overflow-y-auto rounded-2xl bg-white mt-2 z-10`}>
           {
             list?.map((el, index) => (
               <div key={index} className="py-2 px-4 cursor-pointer hover:bg-citizens-gray" onClick={() => {
