95651eea35c84001bf440d4eb0eb291b2128c649
nan <ccmolinac@gmail.com>
2024-12-20 00:19:30 -0500

    Improve responsive layout to button UI

--- ui/citizens/common/button.ui.tsx
+++ ui/citizens/common/button.ui.tsx
@@ -14,7 +14,7 @@ interface ButtonProps {
 export default function Button({ label = 'Button', className, textStiles, light = false, withIcon = false, children, handleClick }: ButtonProps) {
   return (
     <button className={`flex justify-between items-center gap-2  ${light ? 'bg-white/75 border-2 border-white rounded-2xl px-4 py-[10px]':'min-w-[214px] bg-citizens-dark rounded-[20px] p-2 shadow-citizens-btn'} ${className}`} onClick={() => handleClick()}>
-      <p className={` ${light ? 'text-xl':'font-light text-lg text-white text-center uppercase grow'} ${textStiles}`}>{label}</p>
+      <p className={`whitespace-nowrap ${light ? 'text-xl':'font-light text-lg text-white text-center uppercase grow'} ${textStiles}`}>{label}</p>
       {
         withIcon &&
         <div className={`${light ? '':'w-10 h-[27px] border border-white rounded-full flex justify-center items-center'}`}>
