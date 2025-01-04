1429bc43f27dd06cd64e70cb15a2a5abd2b4fb8f
DAC-info <info@thedac.info>
2025-01-03 21:51:52 -0500

    delete unused function and imports

--- server/api-handler/v1/auth.api-handler.ts
+++ server/api-handler/v1/auth.api-handler.ts
@@ -3,8 +3,6 @@ import { ApiResponse } from "../../../interfaces/api.interface";
 import { RequestResponse } from "../request.api-handler";
 import { DefaultApiResponse } from "../../enums/api.enum";
 import { GetUserXPAndLevel, UpdateLastLoginDate, TrackUserLogin } from "../../../utils/firebase.util";
-import { doc, getDoc } from "firebase/firestore";
-import { FirebaseUtil } from "../../../utils/firebase.util";
 
 export async function PostApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<{
   xp: number;
@@ -47,22 +45,5 @@ async function HandleXPReward(address: string): Promise<void> {
     }
   } catch (error) {
     console.error('Error handling XP reward:', error);
-  }
-}
-
-async function CheckUserDailyLogin(address: string): Promise<boolean> {
-  try {
-    const db = await FirebaseUtil.Instance().DB();
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-    const dateString = today.toISOString().split('T')[0];
-    
-    const userLoginRef = doc(db, 'statistics', 'dailyLogins', 'users', address, 'dates', dateString);
-    const userLoginDoc = await getDoc(userLoginRef);
-    
-    return userLoginDoc.exists();
-  } catch (error) {
-    console.error('Error checking user daily login:', error);
-    return false;
   }
 }
