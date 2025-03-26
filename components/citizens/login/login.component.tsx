fcd5dc59aefa4b60b3eb000b57485d1f74b7631c
Creador Labs <info@thedac.info>
2025-03-25 20:26:15 -0500

    add home redirect flow when authenticated

--- components/citizens/login/login.component.tsx
+++ components/citizens/login/login.component.tsx
@@ -1,19 +1,18 @@
-import { usePrivy } from "@privy-io/react-auth";
+import { useEffect } from "react";
+import { CitizensPageLocation } from "../../../enums/citizens/common.enum";
 import LoginUI from "../../../ui/citizens/login/login.ui";
+import { GoToPage } from "../../../utils/router.util";
+import { usePrivy } from "@privy-io/react-auth";
 
 export default function CitizensLoginComponent() {
-    const { login, logout } = usePrivy();
+    const { ready, authenticated } = usePrivy();
 
+    //User Auth Check
+    useEffect(() => {
+        if (ready && authenticated) {
+            GoToPage(CitizensPageLocation.HOME);
+        }
+    }, [ready, authenticated]);
 
-    const handleLogin = () => {
-        login();
-    }
-
-    const handleLogout = () => {
-        logout();
-    }
-
-
-
-    return <LoginUI handleLogin={handleLogin} handleLogout={handleLogout} />
+    return <LoginUI />
 }
