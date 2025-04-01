7426c1fb7c62fe99f4ecc46e1188b6dc74351a17
Creador Labs <info@thedac.info>
2025-04-01 13:44:02 -0500

    Update hook to encapsulate init data

--- hooks/useBlockchainWallet.ts
+++ hooks/useBlockchainWallet.ts
@@ -1,67 +1,89 @@
 import { usePrivy } from '@privy-io/react-auth';
 import { useWallets } from '@privy-io/react-auth';
-import { useSolanaWallets } from '@privy-io/react-auth/solana';
 import { BrowserProvider } from 'ethers';
-import { useEffect, useState } from 'react';
-
-export enum BlockchainType {
-  SOLANA = 'solana',
-  ETHEREUM = 'ethereum',
-  UNKNOWN = 'unknown'
-}
+import { useEffect, useState, useRef } from 'react';
+import { Blockchain } from '../enums/blockchain/common.enum';
+import { setCitizensMetadata, setSelectedCombination } from '../store/citizensMetadataSlice';
+import { getCampaignsTokensMetadata } from '../utils/web3/lukso/contract.util';
+import { CitizenMetadata } from '../interfaces/citizens.interface';
+import { getCollectionAssetByOwner } from '../utils/web3/solana/contract.util';
+import { LogError } from '../utils/common.util';
+import { Module } from '../enums/common.enum';
+import { useDispatch } from 'react-redux';
 
 export function useBlockchainWallet() {
+  const blockchainType = useRef<Blockchain>();
+  const dispatch = useDispatch();
   const { wallets: ethWallets } = useWallets();
-  const { wallets: solanaWallets } = useSolanaWallets();
-  const { ready, logout: privyLogout, user, authenticated, } = usePrivy();
-  const [blockchainType, setBlockchainType] = useState<BlockchainType>(BlockchainType.UNKNOWN);
-  const [provider, setProvider] = useState<any>(undefined);
+  const { ready, user, authenticated } = usePrivy();
+  const [provider, setProvider] = useState<BrowserProvider | undefined>(undefined);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | undefined>(undefined);
+  const [walletAddress, setWalletAddress] = useState<string | undefined>(undefined);
 
-  useEffect(() => {
-    if (!ready || provider) return;
-    if (user?.wallet?.chainType == BlockchainType.SOLANA) {
-      console.log(user, 'user')
-      setBlockchainType(BlockchainType.SOLANA);
-      setProvider({})
-    } else if (user?.wallet?.chainType == BlockchainType.ETHEREUM) {
-      console.log(user, 'user')
-      setBlockchainType(BlockchainType.ETHEREUM);
-
+  const setBlockchain = () => {
+    if (user?.wallet?.chainType == Blockchain.Solana) {
+      blockchainType.current = Blockchain.Solana;
+      setProvider(undefined)
+    } else if (user?.wallet?.chainType == Blockchain.Ethereum) {
+      blockchainType.current = Blockchain.Ethereum;
       ethWallets[0]?.getEthereumProvider().then(ethProvider => {
         const browserProvider = new BrowserProvider(ethProvider);
         setProvider(browserProvider);
       });
     }
-
-  }, [user]);
-
-  useEffect(() => {
-    console.log(blockchainType, 'blockchainType')
-  }, [ready])
-
-  const getWalletAddress = () => {
-    if (blockchainType === BlockchainType.SOLANA) {
-      return solanaWallets[0]?.address.toString();
-    }
-    return ethWallets[0]?.address;
-  };
-
-  const logout = () => {
-    privyLogout();
-    setProvider(undefined);
-    setBlockchainType(BlockchainType.UNKNOWN);
   }
 
+  const getSolanaTokensMetadataPromise = async (walletAddress: string) => {
+    const asset = await getCollectionAssetByOwner(walletAddress)
+    
+    if (asset.success) return [asset.value as unknown as CitizenMetadata]
+
+    return []
+  }
+
+  const getEthereumTokensMetadataPromise = async (walletAddress: string) => {
+    const tokensMetadata = await getCampaignsTokensMetadata(walletAddress)
+
+    if (tokensMetadata.success) return tokensMetadata.value
+
+    return []
+  }
+
+  const fetchCitizensMetadata = async (walletAddress: string) => {
+    if (blockchainType.current === Blockchain.Ethereum) {
+      const ethereumCitizensMetadata = await getEthereumTokensMetadataPromise(walletAddress);
+      dispatch(setCitizensMetadata(ethereumCitizensMetadata));
+      dispatch(setSelectedCombination(ethereumCitizensMetadata[0]?.combination)); // null in case of error. This means user has no avatars
+    } else if (blockchainType.current === Blockchain.Solana) {
+      const solanaCitizensMetadata = await getSolanaTokensMetadataPromise(walletAddress);
+      dispatch(setCitizensMetadata(solanaCitizensMetadata));
+      dispatch(setSelectedCombination(solanaCitizensMetadata[0]?.combination)); // null in case of error. This means user has no avatars
+    }
+
+
+  }
+
+  useEffect(() => {
+    if (ready && authenticated) {
+      if (!user?.wallet?.address) {
+        LogError(Module.Citizens, "User wallet address is undefined");
+        return //TODO: Add error handling
+      }
+      setBlockchain();
+      fetchCitizensMetadata(user?.wallet?.address);
+      setWalletAddress(user?.wallet?.address);
+      setIsLoggedIn(true);
+    }
+    if (ready && !authenticated) {
+      setIsLoggedIn(false);
+    }
+
+  }, [ready, authenticated]);
 
   return {
     blockchainType,
-    walletAddress: getWalletAddress(),
-    isSolana: blockchainType === BlockchainType.SOLANA,
-    isEthereum: blockchainType === BlockchainType.ETHEREUM,
-    solanaWallets,
-    ethWallets,
+    walletAddress,
     provider,
-    logout,
-    authenticated
+    isLoggedIn
   };
 } 
