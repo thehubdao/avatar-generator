3d9d4172b415e81df11c751edb5d09e0fc2e9c7f
Creador Labs <info@thedac.info>
2025-04-01 13:49:59 -0500

    Update store import

--- layouts/mobile.layout.tsx
+++ layouts/mobile.layout.tsx
@@ -1,5 +1,5 @@
 import { Provider } from "react-redux";
-import store from "../store/store";
+import store from "../store/citizensStore";
 import MobileBuildAlert from "../ui/admin/common/mobileBuildAlert.ui";
 
 interface MobileLayoutProps {
