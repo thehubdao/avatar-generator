0ea29965cce4d667b97738e88dacdd6b5cdf255b
Creador Labs <info@thedac.info>
2025-04-01 13:48:31 -0500

    Create citizensMetadataSlice.ts

---
+++ store/citizensMetadataSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { CitizenMetadata } from '../interfaces/citizens.interface';
+import { Campaign } from '../enums/citizens/common.enum';
+
+
+interface CitizensMetadataState {
+  citizensMetadata: CitizenMetadata[];
+  selectedCampaign: Campaign | null;
+  selectedCombination: string | null;
+}
+
+const initialState: CitizensMetadataState = {
+  citizensMetadata: [],
+  selectedCampaign: null,
+  selectedCombination: null, // Cannot be void string, only null or valid combination string
+}
+
+export const citizensMetadataSlice = createSlice({
+  name: 'citizensMetadata',
+  initialState,
+  reducers: {
+    resetCitizensMetadata: () => initialState,
+    setCitizensMetadata: (state, action: PayloadAction<CitizenMetadata[]>) => {
+      state.citizensMetadata = action.payload;
+    },
+    setSelectedCampaign: (state, action: PayloadAction<Campaign>) => {
+      state.selectedCampaign = action.payload;
+    },
+    setSelectedCombination: (state, action: PayloadAction<string>) => {
+      state.selectedCombination = action.payload;
+    }
+  }
+})
+
+export const { setCitizensMetadata, resetCitizensMetadata, setSelectedCampaign, setSelectedCombination } = citizensMetadataSlice.actions
+export default citizensMetadataSlice.reducer
