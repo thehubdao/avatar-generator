cf10bb7375fe126d13d4093d0a352b2182b9b10b
nan <ccmolinac@gmail.com>
2025-04-03 17:05:40 -0500

    Create new login UI

--- ui/citizens/login/login.ui.tsx
+++ ui/citizens/login/login.ui.tsx
@@ -4,15 +4,116 @@ import { BlockchainToWalletChainType } from "../../../utils/web3/web3.util";
 import { useDispatch } from "react-redux";
 import { Campaign } from "../../../enums/citizens/common.enum";
 import { setSelectedCampaign } from "../../../store/citizensMetadataSlice";
+import Image from "next/image";
+import Link from "next/link";
+import PrivacyPolicySVG from "../common/SVG/privacyPolicySVG.ui";
+import TermsOfServicesSVG from "../common/SVG/termsOfServicesSVG.ui";
+import SocialButtons from "../common/socialButtons.ui";
+import CampaignList from "./campaignList.ui";
+import { LOGIN_COLLECTIONS } from "../../../constants/citizens.constant";
+import NewsUI from "./news.ui";
+import FreshDropsUI from "./freshDrops.ui";
+import CommunityContent from "./communityContent.ui";
+import BackedBy from "./backedBy.ui";
+import HeaderUI from "../common/header.ui";
 
 export default function CitizensLoginUI() {
-    const dispatch = useDispatch();
-    const { login } = useLogin();
+	const dispatch = useDispatch();
+	const { login } = useLogin();
 
-    const handleLogin = (blockchain: Blockchain) => {
-        login({ walletChainType: BlockchainToWalletChainType(blockchain) }); // @NaN enviar dinámicamente el blockchain según la campaña seleccionada
-        dispatch(setSelectedCampaign(Campaign.Citizens)); //Settear la campaña seleccionada
-    }
+	/* Tanto el blockchain como la campaña se seleccionan manualmente en cada boton que llama esta función */
+	const handleLogin = (blockchain: Blockchain | undefined, campaign: Campaign | undefined) => {
+		login({ walletChainType: BlockchainToWalletChainType(blockchain) });
+		dispatch(setSelectedCampaign(campaign ?? Campaign.Citizens));
+	}
 
-    return <div onClick={() => handleLogin(Blockchain.Ethereum)}>Poner Login UI</div>
+	return (
+		<div className="w-full min-h-screen bg-gradient-to-b from-[#151515] to-[#0C0C0C] font-work">
+			<HeaderUI onLogin={() => handleLogin(undefined, undefined)}/>
+			<div className="pt-[8vh] 2xl:pt-[6vh] min-h-screen">
+				{/* LANDING CARD */}
+				<div className="relative w-full px-6">
+					{/* BACKGROUND */}
+					<div className="relative w-full h-[45vh] md:h-[72vh] overflow-hidden rounded-3xl md:rounded-[58px]">
+						<Image src={'/resources/images/citizens/login/background.jpg'} fill alt="" priority className="object-cover object-top md:object-[0%_15%] brightness-90" />
+					</div>
+					{/* CITIZEN IMAGE */}
+					<div className="hidden lg:block absolute bottom-0 right-0 xl:right-6 w-[75vh] xl:w-[80vh] h-[75vh] xl:h-[80vh] overflow-hidden rounded-[58px] xl:z-10">
+						<Image src={'/resources/images/citizens/login/background-citizen.png'} fill alt="" className="object-cover object-[0%_15%]" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
+					</div>
+					{/* CONTENT */}
+					<div className="absolute top-1/2 left-1/2 -translate-x-1/2 lg:-translate-x-0 lg:left-20 -translate-y-1/2 px-6">
+						{/* TITLE */}
+						<h1 className="font-monument text-[53px] md:text-[120px] 2xl:text-[170px] text-white text-center leading-[0.8]"><span className="text-[46px] md:text-[104px] 2xl:text-[150px]">CITIZENS</span><br /> PORTAL</h1>
+						{/* TEXT */}
+						<p className="text-[15px] lg:text-[34px] 2xl:text-[48px] text-white text-center leading-none">The home of creators in the 3D Web</p>
+						{/* BUTTON */}
+						<div className="w-full flex justify-center pt-6">
+							<button type="button" className="w-fit h-16 bg-white rounded-[28px] px-10 md:px-24 font-light text-2xl" onClick={() => handleLogin(undefined, undefined)}>
+								FIND OUT MORE
+							</button>
+						</div>
+					</div>
+				</div>
+				{/* CAMPAIGNS */}
+				<div>
+					<h2 className="font-monument text-3xl md:text-[64px] text-white text-center pb-12 pt-20">CAMPAIGNS</h2>
+					<CampaignList collections={LOGIN_COLLECTIONS} handleClick={handleLogin} />
+				</div>
+				{/* WHATS NEW */}
+				<div className="xl:pt-12 px-6">
+					{/* TABLE */}
+					<div className="shadow-citizens-btn bg-citizens-dark rounded-2xl my-8">
+						{/* TABLE TITLE */}
+						<h2 className="font-monument text-3xl md:text-[64px] text-white text-center pb-8 pt-10 border-b-[1px] border-white/10">WHATS NEW?</h2>
+						{/* NEWS LIST */}
+						<NewsUI />
+					</div>
+				</div>
+				{/* FRESH DROPS */}
+				<div className="xl:pt-12 px-6">
+					{/* TABLE */}
+					<div className="shadow-citizens-btn bg-citizens-dark rounded-2xl my-8">
+						{/* TABLE TITLE */}
+						<h2 className="font-monument text-3xl md:text-[64px] text-white text-center pb-8 pt-10 border-b-[1px] border-white/10">FRESH DROPS</h2>
+						{/* NEWS LIST */}
+						<FreshDropsUI setSelectedCampaign={(blockChain, campaign) => handleLogin(blockChain, campaign)} />
+					</div>
+				</div>
+				{/* COMMUNITY CONTENT */}
+				<div>
+					<h2 className="font-monument text-3xl md:text-[64px] text-white text-center pb-12 pt-20 leading-none">COMMUNITY CONTENT</h2>
+					<CommunityContent />
+				</div>
+				{/* BACKED BY */}
+				<div className="pb-[15vh]">
+					<h2 className="font-monument text-3xl md:text-[64px] text-white text-center pb-12 pt-48">BACKED BY</h2>
+					<BackedBy />
+				</div>
+				{/* FOOTER */}
+				<footer className="w-full border-t-[1px] border-white/10 flex flex-col md:flex-row justify-between items-center gap-8 md:gap-0 p-6">
+					<div className="flex gap-4 items-center">
+						<Link href={''} target="_blank" className="flex flex-col items-center gap-2">
+							<PrivacyPolicySVG />
+							<p className="font-light text-sm text-white text-center">
+								Privacy<br />Policy
+							</p>
+						</Link>
+						<Link href={''} target="_blank" className="flex flex-col items-center gap-2">
+							<TermsOfServicesSVG />
+							<p className="font-light text-sm text-white text-center">
+								Terms of<br />Service
+							</p>
+						</Link>
+					</div>
+					<div>
+						<p className="font-light text-sm text-white">Creador Labs UG. All rights reserved</p>
+					</div>
+					<div>
+						<SocialButtons className='flex gap-4' />
+					</div>
+				</footer>
+			</div>
+		</div>
+	)
 }
