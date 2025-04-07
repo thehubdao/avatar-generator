18f103629ece06e94f014faa8c1a43728fb81813
Creador Labs <info@thedac.info>
2025-04-07 15:03:54 -0500

    Add follow user data to redux

--- store/citizensMetadataSlice.ts
+++ store/citizensMetadataSlice.ts
@@ -1,17 +1,19 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { CitizenMetadata } from '../interfaces/citizens.interface';
+import { CitizenMetadata, FollowUserData } from '../interfaces/citizens.interface';
 import { Campaign } from '../enums/citizens/common.enum';
 
 interface CitizensMetadataState {
   citizensMetadata: CitizenMetadata[] | null;
   selectedCampaign: Campaign | null;
   selectedCombination: string | null;
+  followUserData: FollowUserData | null;
 }
 
 const initialState: CitizensMetadataState = {
   citizensMetadata: null,
   selectedCampaign: null,
   selectedCombination: null, // Cannot be void string, only null or valid combination string
+  followUserData: null,  // Both follower and following count are -1, meaning this blockchain does not support follow user data
 }
 
 export const citizensMetadataSlice = createSlice({
@@ -27,9 +29,13 @@ export const citizensMetadataSlice = createSlice({
     },
     setSelectedCombination: (state, action: PayloadAction<string>) => {
       state.selectedCombination = action.payload;
+    },
+    setFollowUserData: (state, action: PayloadAction<FollowUserData>) => {
+      console.log("action.payload", action.payload);
+      state.followUserData = action.payload;
     }
   }
 });
 
-export const { setCitizensMetadata, resetCitizensMetadata, setSelectedCampaign, setSelectedCombination } = citizensMetadataSlice.actions;
+export const { setCitizensMetadata, resetCitizensMetadata, setSelectedCampaign, setSelectedCombination, setFollowUserData } = citizensMetadataSlice.actions;
 export default citizensMetadataSlice.reducer;
