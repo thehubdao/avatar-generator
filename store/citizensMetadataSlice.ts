4b47fc8ed89fdbc1ecd6d55cdfc86c74e4411cbf
nan <ccmolinac@gmail.com>
2025-04-10 18:09:46 -0500

    Add campaignParameters & selectedCitizen to CItizensMetadata state
    - Remove selectedCombination, replaced by selectedCitizen.combination
    - Allow to set selectedCamppaign as null

--- store/citizensMetadataSlice.ts
+++ store/citizensMetadataSlice.ts
@@ -1,18 +1,21 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { CitizenMetadata, FollowUserData } from '../interfaces/citizens.interface';
 import { Campaign } from '../enums/citizens/common.enum';
+import { CampaignParameters } from '../interfaces/common.interface';
 
 interface CitizensMetadataState {
   citizensMetadata: CitizenMetadata[] | null;
   selectedCampaign: Campaign | null;
-  selectedCombination: string | null;
+  CampaignParameters: CampaignParameters | null;
+  selectedCitizen: CitizenMetadata | null;
   followUserData: FollowUserData | null;
 }
 
 const initialState: CitizensMetadataState = {
   citizensMetadata: null,
   selectedCampaign: null,
-  selectedCombination: null, // Cannot be void string, only null or valid combination string
+  CampaignParameters: null,
+  selectedCitizen: null, // Cannot be void string, only null or valid combination string
   followUserData: null,  // Both follower and following count are -1, meaning this blockchain does not support follow user data
 }
 
@@ -24,11 +27,14 @@ export const citizensMetadataSlice = createSlice({
     setCitizensMetadata: (state, action: PayloadAction<CitizenMetadata[]>) => {
       state.citizensMetadata = action.payload;
     },
-    setSelectedCampaign: (state, action: PayloadAction<Campaign>) => {
+    setSelectedCampaign: (state, action: PayloadAction<Campaign | null>) => {
       state.selectedCampaign = action.payload;
     },
-    setSelectedCombination: (state, action: PayloadAction<string>) => {
-      state.selectedCombination = action.payload;
+    setCampaignParameters: (state, action: PayloadAction<CampaignParameters>) => {
+      state.CampaignParameters = action.payload;
+    },
+    setSelectedCitizen: (state, action: PayloadAction<CitizenMetadata>) => {
+      state.selectedCitizen = action.payload;
     },
     setFollowUserData: (state, action: PayloadAction<FollowUserData>) => {
       state.followUserData = action.payload;
@@ -36,5 +42,5 @@ export const citizensMetadataSlice = createSlice({
   }
 });
 
-export const { setCitizensMetadata, resetCitizensMetadata, setSelectedCampaign, setSelectedCombination, setFollowUserData } = citizensMetadataSlice.actions;
+export const { setCitizensMetadata, resetCitizensMetadata, setSelectedCampaign, setCampaignParameters, setSelectedCitizen, setFollowUserData } = citizensMetadataSlice.actions;
 export default citizensMetadataSlice.reducer;
