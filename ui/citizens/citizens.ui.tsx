87d8fa3ef78d5be724a5db664ac238fecb64d337
nan <ccmolinac@gmail.com>
2025-04-10 18:20:03 -0500

    Add Avatar editor and loading behavior to citizens UI

--- ui/citizens/citizens.ui.tsx
+++ ui/citizens/citizens.ui.tsx
@@ -1,20 +1,52 @@
-import { usePrivy, useLogout } from "@privy-io/react-auth";
-import { useDispatch } from "react-redux";
-import { resetCitizensMetadata } from "../../store/citizensMetadataSlice";
+import AvatarEditor from "../../components/avatar/editor.component";
+import { useAppSelector } from "../../store/hooks";
 
-export default function CitizensUI() {
-    const dispatch = useDispatch();
-    const { user } = usePrivy();
-    const { logout } = useLogout();
+interface CitizensUIProps {
+	isReady: boolean;
+	handleReady: () => Promise<void>;
+}
 
-    const handleLogout = () => {
-        logout();
-        dispatch(resetCitizensMetadata()); // Reset the citizens metadata state
-    }
+export default function CitizensUI({ isReady, handleReady }: CitizensUIProps) {
+	const campaignParams = useAppSelector(state => state.citizensMetadata.CampaignParameters);
+	const selectedCitizen = useAppSelector(state => state.citizensMetadata.selectedCitizen);
 
-    return <div>
-        <h1>Poner Aquí Citizens UI</h1>
-        <p>{user?.wallet?.address}</p>
-        <button onClick={handleLogout}>Logout</button>
-    </div>
+	return (
+		<div className="w-full min-h-[calc(100dvh_-_96px)] grid items-center text-white">
+			{
+				campaignParams && campaignParams !== null && selectedCitizen !== null &&
+				<div className="fixed inset-0 w-full h-dvh flex justify-center items-center">
+					<AvatarEditor
+						avatarBasePath={
+							campaignParams.armature
+						}
+						editMode={false}
+						lights={
+							campaignParams.config.lights
+						}
+						defaultShadow={
+							campaignParams.config
+								.defShadow
+						}
+						defaultCamera={
+							campaignParams.config.defCam
+						}
+						postProcessing={
+							campaignParams.config
+								.postProcessing
+						}
+						onReady={() => {
+							return handleReady();
+						}
+						}
+					/>
+				</div>
+			}
+			{
+				!isReady &&
+				<div className="fixed inset-0 w-full h-dvh flex justify-center items-center bg-gradient-to-b from-[#151515] to-[#0C0C0C]">
+					<h1 className="text-white text-2xl">Loading Environment...</h1>
+				</div>
+			}
+		</div>
+	)
 }
