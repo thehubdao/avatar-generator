580d46e844bcda30d32e5c7b5c21b96f3f9c55e4
nan <ccmolinac@gmail.com>
2025-04-15 19:50:50 -0500

    Update leaderboard link in header

--- ui/citizens/common/header.ui.tsx
+++ ui/citizens/common/header.ui.tsx
@@ -72,7 +72,7 @@ export default function HeaderUI({onLogin, onLogout}: HeaderUIProps) {
                 handleNavbarClick(CitizensPageLocation.BACKPACK);
               }} />
               <Button label="leaderboard" withIcon className="w-full xl:min-w-min h-fit px-4 !shadow-none xl:!shadow-citizens-btn" textStyles="text-[32px] xl:!text-base 2xl:!text-lg text-start xl:text-center" handleClick={() => {
-                handleNavbarClick();
+                handleNavbarClick(CitizensPageLocation.LEADERBOARD);
               }} />
               <Button label="play" withIcon className="w-full xl:min-w-min h-fit px-4 !shadow-none xl:!shadow-citizens-btn" textStyles="text-[32px] xl:!text-base 2xl:!text-lg text-start xl:text-center" handleClick={() => {
                 handleNavbarClick(CitizensPageLocation.PLAY);
