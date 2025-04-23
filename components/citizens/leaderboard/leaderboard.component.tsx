00f5a288c3ffeb4e7a256206a343a58d4b762af3
Creador Labs <info@thedac.info>
2025-04-23 14:26:20 -0500

    Add follow and unfollow functions to leaderboard component

--- components/citizens/leaderboard/leaderboard.component.tsx
+++ components/citizens/leaderboard/leaderboard.component.tsx
@@ -1,5 +1,27 @@
 import CitizenLeaderBoardUI from "../../../ui/citizens/leaderboard/leaderboard.ui";
+import { FollowUser, UnfollowUser } from "../../../utils/web3/citizens.util";
+import { useAppSelector } from "../../../store/hooks";
 
 export default function CitizenLeaderBoardComponent() {
+  const provider = useAppSelector(state => state.citizensAuth.provider);
+
+  const handleFollowUser = async (address: string) => {
+    if (provider) {
+      const result = await FollowUser(address, provider);
+      if (result.success) {
+        // TODO: Update the leaderboard
+      }
+    }
+  };
+
+  const handleUnfollowUser = async (address: string) => {
+    if (provider) {
+      const result = await UnfollowUser(address, provider);
+      if (result.success) {
+        // TODO: Update the leaderboard
+      }
+    }
+  };
+
   return <CitizenLeaderBoardUI />;
 }
