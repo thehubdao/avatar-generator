d50c15bcdf22d80f783c185fd3db711029693483
Creador Labs <info@thedac.info>
2025-04-23 14:25:39 -0500

    Add provider to redux state

--- store/CitizensAuthSlice.ts
+++ store/CitizensAuthSlice.ts
@@ -1,31 +1,36 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Blockchain } from '../enums/blockchain/common.enum';
+import { BrowserProvider } from 'ethers';
 
 export interface AuthState {
   connected: boolean | null;
   address: string | null;
   blockchainType: Blockchain | null;
+  provider: BrowserProvider | null;
 }
 
 const initialState: AuthState = {
   connected: null,
   address: null,
   blockchainType: null,
+  provider: null,
 }
 
 export const citizensAuthSlice = createSlice({
   name: 'citizensAuth',
   initialState,
   reducers: {
-    connect: (state, action: PayloadAction<{address: string, blockchainType: Blockchain}>) => {
+    connect: (state, action: PayloadAction<{address: string, blockchainType: Blockchain, provider: BrowserProvider | null}>) => {
       state.connected = true;
       state.address = action.payload.address;
       state.blockchainType = action.payload.blockchainType;
+      state.provider = action.payload.provider;
     },
     disconnect: (state) => {
       state.connected = false;
       state.address = null;
       state.blockchainType = null;
+      state.provider = null;
     },
     setAddress: (state, action: PayloadAction<string>) => {
       state.address = action.payload;
