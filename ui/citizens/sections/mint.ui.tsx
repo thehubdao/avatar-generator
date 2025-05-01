b73b9653f6ba17fce17f996e977dafe61cdc1bc6
Creador Labs <info@thedac.info>
2025-05-01 10:43:14 -0500

    Add doomie kumi base combination

--- ui/citizens/sections/mint.ui.tsx
+++ ui/citizens/sections/mint.ui.tsx
@@ -16,6 +16,7 @@ interface MintUIProps {
   campaignDescription?: string;
   price?: number;
   supply?: number;
+  baseCombination?: string;
   mintRedirect: () => void;
 }
 
@@ -24,6 +25,7 @@ export default function MintUI({
   avatarDescription = '"Kum Kum" inspired the nickname of the football legend Kun Agüero. Today, we honor his nickname and celebrate his career by presenting "KUMI," a FREE-to-claim, 3D interoperable avatar that showcases the future of Web3 gaming and the internet',
   campaignName = 'KUMI CITIZENS',
   campaignDescription = 'Minting a KUMI unlocks the gateway to the Fitchin Universe. It’s your chance to own a unique visual identity that’s truly yours. Dive in, create epic content, flex your avatar, climb the leaderboard, and snag exclusive wearables to stand out in style.',
+  baseCombination='0-0-0-0-0-0-0-0-0-0',
   price,
   supply,
   mintRedirect }: MintUIProps) {
