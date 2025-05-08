18bfafaa2d9ef9c1b7babe38124a30782da06d05
nan <ccmolinac@gmail.com>
2025-05-08 13:28:08 -0500

    Add snackBar when follow or unfollow is success in follow button UI

--- ui/citizens/common/followButton.ui.tsx
+++ ui/citizens/common/followButton.ui.tsx
@@ -17,11 +17,10 @@ export default function FollowButton({ user, onFollowUser, onUnfollowUser }: Fol
   const followUserHandler = async () => {
     setIsLoading(true);
     const isSuccess = await onFollowUser(user.address);
-
     if (!isSuccess) {
-      showSnackbar(
-        <p>Follow failed, try again later.</p>
-      )
+      showSnackbar(<p>Follow failed, try again later.</p>);
+    } else {
+      showSnackbar(<p>Followed {user.name}</p>);
     }
     setIsLoading(false);
   }
@@ -29,48 +28,52 @@ export default function FollowButton({ user, onFollowUser, onUnfollowUser }: Fol
   const unfollowUserHandler = async () => {
     setIsLoading(true);
     const isSuccess = await onUnfollowUser(user.address);
-    console.log('isSuccess', isSuccess);
     if (!isSuccess) {
-
-      showSnackbar(
-        <p>Unfollow failed, try again later.</p>
-      )
+      showSnackbar(<p>Unfollow failed, try again later.</p>);
+    } else {
+      showSnackbar(<p>unfollowed {user.name}</p>);
     }
     setIsLoading(false);
   }
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  return !user.isFollowing ? <div
-    className={`h-7 sm:h-11 w-8 sm:w-16 flex justify-center items-center rounded-[20px] cursor-pointer mr-2 lg:mr-8 bg-white`}
-    onClick={() => {
-      if (!user.isFollowing) {
-        setIsLoading(true);
-        followUserHandler();
+  return (
+    <div>
+      {
+        !user.isFollowing ? <div
+          className={`h-7 sm:h-11 w-8 sm:w-16 flex justify-center items-center rounded-[20px] cursor-pointer mr-2 lg:mr-8 bg-white`}
+          onClick={() => {
+            if (!user.isFollowing) {
+              setIsLoading(true);
+              followUserHandler();
+            }
+          }}
+        >
+          {isLoading ?
+            <div className="w-2 sm:w-2 h-2 sm:h-4 border-t border-citizens-dark rounded-full animate-spin" />
+            :
+            <div className="scale-75">
+              <AddUserSVG />
+            </div>
+          }
+        </div> : <div
+          className={`h-7 sm:h-11 w-8 sm:w-16 flex justify-center items-center rounded-[20px] cursor-pointer mr-2 lg:mr-8 bg-white`}
+          onClick={() => {
+            if (user.isFollowing) {
+              setIsLoading(true);
+              unfollowUserHandler();
+            }
+          }}
+        >
+          {isLoading ?
+            <div className="w-2 sm:w-4 h-2 sm:h-4 border-t border-citizens-dark rounded-full animate-spin" />
+            :
+            <div className="scale-75">
+              <RemoveUserSVG />
+            </div>
+          }
+        </div>
       }
-    }}
-  >
-    {isLoading ?
-      <div className="w-2 sm:w-2 h-2 sm:h-4 border-t border-citizens-dark rounded-full animate-spin" />
-      :
-      <div className="scale-75">
-        <AddUserSVG />
-      </div>
-    }
-  </div> : <div
-    className={`h-7 sm:h-11 w-8 sm:w-16 flex justify-center items-center rounded-[20px] cursor-pointer mr-2 lg:mr-8 bg-white`}
-    onClick={() => {
-      if (user.isFollowing) {
-        setIsLoading(true);
-        unfollowUserHandler();
-      }
-    }}
-  >
-    {isLoading ?
-      <div className="w-2 sm:w-4 h-2 sm:h-4 border-t border-citizens-dark rounded-full animate-spin" />
-      :
-      <div className="scale-75">
-        <RemoveUserSVG  />
-      </div>
-    }
-  </div>
+    </div>
+  )
 }
