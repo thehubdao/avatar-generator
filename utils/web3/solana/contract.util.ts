ace83e31255148a7400c45567627f2082978b99a
Creador Labs <info@thedac.info>
2025-05-15 09:55:37 -0500

    Add kumi minting functions

--- utils/web3/solana/contract.util.ts
+++ utils/web3/solana/contract.util.ts
@@ -1,12 +1,41 @@
-import { publicKey } from '@metaplex-foundation/umi';
-import { AssetV1, fetchAssetsByOwner } from '@metaplex-foundation/mpl-core';
+import { generateSigner, PublicKey, publicKey, signerIdentity, some, transactionBuilder } from '@metaplex-foundation/umi';
+import { AssetV1, fetchAssetsByOwner, mplCore } from '@metaplex-foundation/mpl-core';
 import { Result } from '../../../types/common.type';
 import { LogError } from '../../../utils/common.util';
 import { CommonErrorCode, Module } from '../../../enums/common.enum';
-import { UMI, COLLECTION_ID } from '../../../constants/solana/contract.constant';
-import { PublicKey } from '@solana/web3.js';
+import { UMI, COLLECTION_ID, KUMI_CANDY_MACHINE_ID, KUMI_CANDY_MACHINE_TREASURY } from '../../../constants/solana/contract.constant';
 import { CitizenMetadata } from '../../../interfaces/citizens.interface';
 import { GetSolanaImageUrl, GetSolanaIPFSData } from '../citizens.util';
+import { ConnectedSolanaWallet } from '@privy-io/react-auth';
+import { GuardSetMintArgs, mintV1, mplCandyMachine } from '@metaplex-foundation/mpl-core-candy-machine';
+import {
+    fromWeb3JsTransaction,
+    toWeb3JsTransaction
+} from '@metaplex-foundation/umi-web3js-adapters';
+import { CandyMachineGroup } from '../../../enums/citizens/common.enum';
+import { setComputeUnitLimit } from '@metaplex-foundation/mpl-toolbox';
+
+export async function InitializeUmi(wallet: ConnectedSolanaWallet) {
+    UMI.use(signerIdentity({
+        publicKey: publicKey(wallet.address),
+        signMessage: async (message: Uint8Array) =>
+            await wallet.signMessage(message),
+        signTransaction: async (transaction) => {
+            const web3JsTransaction = toWeb3JsTransaction(transaction);
+            const signedTx = await wallet.signTransaction(web3JsTransaction);
+            return fromWeb3JsTransaction(signedTx);
+        },
+        signAllTransactions: async (transactions) => {
+            const web3JsTransactions = transactions.map(toWeb3JsTransaction);
+            const signedTxs = await Promise.all(
+                web3JsTransactions.map(tx => wallet.signTransaction(tx))
+            );
+            return signedTxs.map(fromWeb3JsTransaction);
+        }
+    }));
+    UMI.use(mplCore());
+    UMI.use(mplCandyMachine());
+}
 
 export async function GetAssetsByOwner(walletAddress: string): Promise<Result<AssetV1[]>> {
     try {
@@ -73,4 +102,48 @@ export async function GetCollectionAssetByOwner(
         value: [assetMetadata] //Temporarily return as array while we add more campaigns to Solana
     };
 
+}
+
+export async function getKumiCandyMachineGuardGroup(walletAddress: string, collectionId: PublicKey): Promise<Result<{group: CandyMachineGroup, mintArgs: GuardSetMintArgs}>> {
+    const assetsResult = await GetCollectionAssetByOwner(walletAddress, collectionId);
+    if (!assetsResult.success) return {
+        success: false,
+        errMessage: assetsResult.errMessage,
+        errCode: CommonErrorCode.GetNoData
+    };
+
+    if (assetsResult.value.length > 0) return {
+        success: true,
+        value: {group: CandyMachineGroup.Holder, mintArgs: {solPayment:some({destination:KUMI_CANDY_MACHINE_TREASURY}), assetGate: some({requiredCollection:COLLECTION_ID})}}
+    };
+
+    return {
+        success: true,
+        value: {group: CandyMachineGroup.Public, mintArgs: {solPayment:some({destination:KUMI_CANDY_MACHINE_TREASURY}) }}
+    };
+}
+
+export async function mintKumiCitizen() : Promise<Result<boolean>> {
+    const nftAccount = generateSigner(UMI); //Generate a new NFT account address
+    const groupResult = await getKumiCandyMachineGuardGroup(UMI.identity.publicKey, COLLECTION_ID);
+
+    if (!groupResult.success) return {
+        success: false,
+        errMessage: groupResult.errMessage,
+        errCode: CommonErrorCode.GetNoData
+    };
+console.log(groupResult.value)
+    const mintTx = mintV1(UMI, {
+        candyMachine: KUMI_CANDY_MACHINE_ID, asset: nftAccount, collection: COLLECTION_ID, group: some(groupResult.value.group), mintArgs: groupResult.value.mintArgs
+    });
+
+    await transactionBuilder()
+        .add(setComputeUnitLimit(UMI, { units: 300_000 }))
+        .add(mintTx)
+        .sendAndConfirm(UMI);
+
+    return {
+        success: true,
+        value: true
+    };
 }
