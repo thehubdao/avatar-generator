47cc28183e09ac18cd95f59ee9637f689b4bee22
Creador Labs <info@thedac.info>
2025-05-15 09:56:35 -0500

    Add constants and get from env

--- constants/solana/contract.constant.ts
+++ constants/solana/contract.constant.ts
@@ -1,5 +1,8 @@
 import { PublicKey } from "@solana/web3.js";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { publicKey } from "@metaplex-foundation/umi";
 
-export const COLLECTION_ID = new PublicKey('GbwhRDb6Xwe1ZMG6Ei6ttPpCqe3JKkuv7a1zViuxz9RR');
+export const KUMI_CANDY_MACHINE_ID = publicKey(process.env.NEXT_PUBLIC_KUMI_CANDY_MACHINE_ID || '');
+export const COLLECTION_ID = publicKey(process.env.NEXT_PUBLIC_COLLECTION_ID || '');
+export const KUMI_CANDY_MACHINE_TREASURY = publicKey(process.env.NEXT_PUBLIC_KUMI_CANDY_MACHINE_TREASURY || '');
 export const UMI = createUmi(process.env.NEXT_PUBLIC_SOLANA_RPC_URL || '');
