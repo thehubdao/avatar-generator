5596ab581faeeb792e9455b88fcd06a0b4bebfe2
Creador Labs <info@thedac.info>
2025-05-15 11:04:43 -0500

    add xpData to auth redux

--- store/CitizensAuthSlice.ts
+++ store/CitizensAuthSlice.ts
@@ -1,11 +1,13 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Blockchain } from '../enums/blockchain/common.enum';
+import { UserXPData } from '../interfaces/citizens.interface';
 
 export interface AuthState {
   connected: boolean | null;
   address: string | null;
   walletName: string | null;
   blockchainType: Blockchain | null;
+  xpData: UserXPData | null;
 }
 
 const initialState: AuthState = {
@@ -13,22 +15,26 @@ const initialState: AuthState = {
   address: null,
   walletName: null,
   blockchainType: null,
+  xpData: null,
 }
 
 export const citizensAuthSlice = createSlice({
   name: 'citizensAuth',
   initialState,
   reducers: {
-    connect: (state, action: PayloadAction<{address: string, walletName: string | null, blockchainType: Blockchain}>) => {
+    connect: (state, action: PayloadAction<{address: string, walletName: string | null, blockchainType: Blockchain, xpData: UserXPData | null}>) => {
       state.connected = true;
       state.address = action.payload.address;
       state.walletName = action.payload.walletName;
       state.blockchainType = action.payload.blockchainType;
+      state.xpData = action.payload.xpData;
     },
     disconnect: (state) => {
       state.connected = false;
       state.address = null;
+      state.walletName = null;
       state.blockchainType = null;
+      state.xpData = null;
     },
     setAddress: (state, action: PayloadAction<string>) => {
       state.address = action.payload;
