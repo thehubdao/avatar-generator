f5901f888e0616e65c2a910ffea12e2c3448796d
Creador Labs <gabch15@hotmail.com>
2025-05-16 14:29:15 -0500

    Add blockchain property to burn route

--- pages/api/v1/burns/index.ts
+++ pages/api/v1/burns/index.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { ApiResponse } from '../../../../interfaces/api.interface';
 import { RequestResponse } from '../../../../server/api-handler/request.api-handler';
 import { HandleBurnXPReward } from '../../../../server/api-handler/v1/burns.api-handler';
+import { Blockchain } from '../../../../enums/blockchain/common.enum';
 
 export default async function Handler(
   req: NextApiRequest,
@@ -15,7 +16,7 @@ export default async function Handler(
     }
 
     try {
-      await HandleBurnXPReward(address, burns);
+      await HandleBurnXPReward(address, burns, Blockchain.Ethereum );
       return RequestResponse(res, "Successful", true, "Burns processed successfully");
     } catch (error) {
       console.error('Error processing burns:', error);
