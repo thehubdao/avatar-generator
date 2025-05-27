9b3cbb8f3f028f12e2a2b6f22cb30b6066b713ad
Creador Labs <gabch15@hotmail.com>
2025-05-27 09:32:28 -0500

    Remove free to claim from label

--- constants/mint.constant.ts
+++ constants/mint.constant.ts
@@ -4,7 +4,7 @@ import { MintingUiData } from "../interfaces/citizens.interface";
 export const MINTING_UI_DATA: Record<Campaign, MintingUiData | undefined> = {
     [Campaign.Kumi]: {
         imgUrl: '/resources/images/campaings/kumi_collection.jpg',
-        avatarDescription: '"Kum Kum" inspired the nickname of the football legend Kun Agüero. Today, we honor his nickname and celebrate his career by presenting "KUMI," a FREE-to-claim, 3D interoperable avatar that showcases the future of Web3 gaming and the internet',
+        avatarDescription: '"Kum Kum" inspired the nickname of the football legend Kun Agüero. Today, we honor his nickname and celebrate his career by presenting "KUMI", 3D interoperable avatar that showcases the future of Web3 gaming and the internet',
         campaignName: 'KUMI CITIZENS',
         campaignDescription: 'Minting a KUMI unlocks the gateway to the Fitchin Universe. It’s your chance to own a unique visual identity that’s truly yours. Dive in, create epic content, flex your avatar, climb the leaderboard, and snag exclusive wearables to stand out in style.',
     },
