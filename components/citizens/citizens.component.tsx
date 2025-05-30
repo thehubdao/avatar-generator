9e04a38b42dddfad3f534cf598e0861228ccc8bd
Creador Labs <gabch15@hotmail.com>
2025-05-29 21:31:17 -0500

    Implement set of new combination on root

--- components/citizens/citizens.component.tsx
+++ components/citizens/citizens.component.tsx
@@ -17,8 +17,8 @@ import { setCitizensMetadata, setSelectedCitizen } from "../../store/citizensMet
 import { GetCampaignCitizensMetadata, MintKumiCitizen, SetNewCombination } from "../../utils/web3/solana/contract.util";
 import { GetVrmUrl } from "../../utils/web3/citizens.util";
 import { ModelExtension } from "../../enums/export.enum";
-import { GetRootAssetsMetadata, MintRootAsset } from "../../utils/web3/root/contract.util";
-import { Drop, LuksoMetadata, RootMetadata, SolanaAttribute, SolanaMetadata } from "../../interfaces/citizens.interface";
+import { GetRootAssetsMetadata, MintRootAsset, SetRootNewCombination } from "../../utils/web3/root/contract.util";
+import { Drop, LuksoMetadata, RootDrop, RootMetadata, SolanaAttribute, SolanaMetadata } from "../../interfaces/citizens.interface";
 import { StoreAssetData } from "../../utils/firebase.util";
 import { AssetData } from "../../interfaces/firebase.interface";
 
@@ -489,6 +489,7 @@ export default function CitizensComponent() {
     const newCombinationArray = newCombination.split('-');
     const oldAttributes: SolanaAttribute[] = []; //This array will contain the attributes to be unequipped
     const newAttributes: SolanaAttribute[] = []; //This array will contain the attributes to be equipped
+
     if (userFeatures && userFeatures[selectedCampaign as string] != null) newCombinationArray.forEach((featureIndex, index) => {
       const indexType = campaignParams?.features?.[index]?.displayName; //Get the type of the feature
       if (!indexType) return undefined;
@@ -535,11 +536,16 @@ export default function CitizensComponent() {
 
     newCitizenMetadata.imageUrl = metadataObject.value.imageUrl;
 
-    const result = await SetNewCombination((selectedCitizen.rawMetadata as SolanaMetadata).asset_address, metadataObject.value.uri, newAttributes, oldAttributes);
+    const setNewCombinationResult = await SetNewCombination((selectedCitizen.rawMetadata as SolanaMetadata).asset_address, metadataObject.value.uri, newAttributes, oldAttributes);
+
+    if (!setNewCombinationResult.success) {
+      LogError(Module.Citizens, 'Failed to set new combination on saveSolanaCombination', setNewCombinationResult.errCode);
+      return false;
+    }
 
     const isFetchSuccess = await fetchSolanaMetadata(walletAddress);
 
-    return isFetchSuccess && result.success;
+    return isFetchSuccess && setNewCombinationResult.value;
 
   }
 
@@ -549,42 +555,76 @@ export default function CitizensComponent() {
       .join('-') as string;
 
     if (!campaignParams) {
-      LogError(Module.Citizens, 'Campaign params is undefined in Kumi saveCombination');
+      LogError(Module.Citizens, 'Campaign params is undefined in Root saveCombination');
       return false;
     }
     if (!singleInitData.current) {
-      LogError(Module.Citizens, 'Single init data is undefined in Kumi saveCombination');
+      LogError(Module.Citizens, 'Single init data is undefined in Root saveCombination');
       return false;
     }
     if (!selectedCitizen) {
-      LogError(Module.Citizens, 'Selected citizen is undefined in Kumi saveCombination');
+      LogError(Module.Citizens, 'Selected citizen is undefined in Root saveCombination');
       return false;
     }
     if (!walletAddress) {
-      LogError(Module.Citizens, 'Wallet address is undefined in Kumi saveCombination');
+      LogError(Module.Citizens, 'Wallet address is undefined in Root saveCombination');
       return false;
     }
     if (!citizensMetadata) {
-      LogError(Module.Citizens, 'Citizens metadata is undefined in Kumi saveCombination');
+      LogError(Module.Citizens, 'Citizens metadata is undefined in Root saveCombination');
       return false;
     }
 
     const currentCampaign = selectedCampaign;
 
     if (!currentCampaign) {
-      LogError(Module.Citizens, 'Current campaign is undefined in Kumi saveCombination');
+      LogError(Module.Citizens, 'Current campaign is undefined in Root saveCombination');
       return false;
     }
 
-    await StoreAssetData({
+    const newCombinationArray = newCombination.split('-');
+    const oldCombinationArray = selectedCitizen.combination.split('-');
+    const newAttributes: RootDrop[] = [];
+    const oldAttributes: RootDrop[] = [];
+    
+    if (userFeatures && userFeatures[selectedCampaign] != null) newCombinationArray.forEach((newIndex, index) => {
+      const indexType = campaignParams?.features?.[index]?.displayName; //Get the type of the feature
+      const oldIndex = oldCombinationArray[index]; //Get the type of the feature
+
+      if (!indexType) return undefined;
+
+      if(oldIndex == newIndex) return undefined;
+
+      const newAttribute = userFeatures[currentCampaign].find((feature: { type: string; index: number; }) => feature.type === indexType && feature.index === parseInt(newIndex)) as RootDrop; //Get the new feature to be equipped
+      const oldAttribute = userFeatures[currentCampaign].find((feature: { type: string; index: number; }) => feature.type === indexType && feature.index === parseInt(oldIndex)) as RootDrop; //Get the old feature to be unequipped
+
+      if(newAttribute) newAttributes.push(newAttribute);
+      if(oldAttribute) oldAttributes.push(oldAttribute);
+    });
+
+    const setNewCombinationResult = await SetRootNewCombination(walletAddress, selectedCitizen.tokenId, newAttributes, oldAttributes);
+
+    if(!setNewCombinationResult.success) {
+      LogError(Module.Citizens, 'Failed to set new combination on saveRootCombination', setNewCombinationResult.errCode);
+      return false;
+    }
+
+    const storeAssetDataResult = await StoreAssetData({
       imageUrl: selectedCitizen.imageUrl,
       tokenId: selectedCitizen.tokenId,
       collectionId: (selectedCitizen.rawMetadata as RootMetadata).collectionId,
       campaign: currentCampaign,
+      combination: newCombination
     } as AssetData);
 
-    return true;
+    if(!storeAssetDataResult.success) {
+      LogError(Module.Citizens, 'Failed to store asset data on saveRootCombination', storeAssetDataResult.errCode);
+      return false;
+    }
 
+    const isFetchSuccess = await fetchRootMetadata(walletAddress);
+
+    return isFetchSuccess;
   }
 
   async function handleSaveCombination() {
--- constants/root/contract.constant.ts
+++ constants/root/contract.constant.ts
@@ -4,6 +4,7 @@ import { getApiOptions } from '@therootnetwork/api';
 import { LogError } from '../../utils/common.util';
 import { Module } from '../../enums/common.enum';
 import { Signer } from '@futureverse/signer';
+import { AssetRegister } from '@futureverse/asset-register/v2'
 
 const ROOT_NETWORK_WS_URL = process.env.NEXT_PUBLIC_ROOT_NETWORK_WS_URL;
 
@@ -17,7 +18,14 @@ export const ADMIN_SIGNER = KEYRING.addFromSeed(SEED_U8A);
 export const MINT_AMOUNT = 1;
 
 export const NFT_COLLECTION_ID = process.env.NEXT_PUBLIC_NFT_COLLECTION_ID;
-export const NFT_COLLECTION_ADDRESS = process.env.NEXT_PUBLIC_NFT_COLLECTION_ADDRESS;
+export const NFT_COLLECTION_ADDRESS = process.env.NEXT_PUBLIC_NFT_COLLECTION_ADDRESS as string;
+
+export const ROOT_GQL_API_URL = process.env.NEXT_PUBLIC_ROOT_GQL_API_URL as string;
+export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN as string;
+export const ORIGIN = process.env.NEXT_PUBLIC_ORIGIN as string;
+export const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID as string;
+
+export let ASSET_REGISTER_SDK: AssetRegister;
 
 export let API: ApiPromise;
 export let SIGNER: Signer;
@@ -30,4 +38,23 @@ export async function InitializeContractEssentialData(_signer: Signer) {
 
     if (SIGNER) return LogError(Module.RootContractConstant, 'Attempted to initialize in Singleton Pattern. Signer already initialized');
     SIGNER = _signer;
+
+    if (ASSET_REGISTER_SDK) return LogError(Module.RootContractConstant, 'Attempted to initialize in Singleton Pattern. Asset Register SDK already initialized');
+    ASSET_REGISTER_SDK = new AssetRegister({
+        url: ROOT_GQL_API_URL,
+        auth: {
+          sign: async (message) => {
+            return await SIGNER.signMessage({ raw: message as `0x${string}` });
+          },
+          storage: {
+            set: (key: string, value: string) => localStorage.setItem(key, value),
+            get: (key: string) => localStorage.getItem(key),
+          },
+          domain: DOMAIN,
+          origin: ORIGIN,
+          chainId: Number(CHAIN_ID),
+          walletAddress: await SIGNER.getAddress() as `0x${string}`,
+        },
+      });
+      
 }
