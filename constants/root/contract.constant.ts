ef56d5f7af21baeb80c4febb12355640ce6248a9
Creador Labs <gabch15@hotmail.com>
2025-05-30 11:42:48 -0500

    Remove dangerous code

--- constants/root/contract.constant.ts
+++ constants/root/contract.constant.ts
@@ -10,14 +10,9 @@ const ROOT_NETWORK_WS_URL = process.env.NEXT_PUBLIC_ROOT_NETWORK_WS_URL;
 
 export const PROVIDER = new WsProvider(ROOT_NETWORK_WS_URL);
 
-const KEYRING = new Keyring({ type: "ethereum" });
-const SEED_U8A = hexToU8a(process.env.NEXT_PUBLIC_SIGNER_PK);
-
-export const ADMIN_SIGNER = KEYRING.addFromSeed(SEED_U8A);
-
 export const MINT_AMOUNT = 1;
 
-export const NFT_COLLECTION_ID = process.env.NEXT_PUBLIC_NFT_COLLECTION_ID;
+export const NFT_COLLECTION_ID = process.env.NEXT_PUBLIC_NFT_COLLECTION_ID as string;
 export const NFT_COLLECTION_ADDRESS = process.env.NEXT_PUBLIC_NFT_COLLECTION_ADDRESS as string;
 
 export const ROOT_GQL_API_URL = process.env.NEXT_PUBLIC_ROOT_GQL_API_URL as string;
@@ -56,5 +51,4 @@ export async function InitializeContractEssentialData(_signer: Signer) {
           walletAddress: await SIGNER.getAddress() as `0x${string}`,
         },
       });
-      
 }
