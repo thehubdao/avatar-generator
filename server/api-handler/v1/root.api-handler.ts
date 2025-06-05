19834ef575c98f0af2749f2b522ee9c4ac09add7
Creador Labs <gabch15@hotmail.com>
2025-06-05 13:46:21 -0500

    fix events getting

--- server/api-handler/v1/root.api-handler.ts
+++ server/api-handler/v1/root.api-handler.ts
@@ -5,8 +5,10 @@ import { RequestResponse } from "../request.api-handler";
 
 
 export async function  PostApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<string[]>>) {
-    const {data, events} = req.body;
-    console.log(events, events[0], events[0].args);
+    const {data} = req.body;
+    const {events} = data;
+    console.log(events);
+    console.log(events[0], events[0].args);
   
     if (!data)
       return RequestResponse(res, "BadRequest", false,  DefaultApiResponse.MissingInfo);
