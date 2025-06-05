b2b73d97af7529d7f62819dff6c246104125d432
Creador Labs <gabch15@hotmail.com>
2025-06-05 12:22:09 -0500

    Add new route

---
+++ server/api-handler/v1/root.api-handler.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { ApiResponse } from "../../../interfaces/api.interface";
+import { DefaultApiResponse } from "../../enums/api.enum";
+import { RequestResponse } from "../request.api-handler";
+
+
+export async function  PostApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<string[]>>) {
+    const data = req.query;
+  
+    if (!data)
+      return RequestResponse(res, "BadRequest", false,  DefaultApiResponse.MissingInfo);
+
+    console.log(data);
+    
+    return RequestResponse(res, "ServerError", false, DefaultApiResponse.ErrorProcessingInfo);
+  }
