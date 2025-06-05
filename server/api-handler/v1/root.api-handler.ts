202b624a182994a1aeeca4d3ad92737a45dabc87
Creador Labs <gabch15@hotmail.com>
2025-06-05 13:39:54 -0500

    read events data

--- server/api-handler/v1/root.api-handler.ts
+++ server/api-handler/v1/root.api-handler.ts
@@ -6,8 +6,7 @@ import { RequestResponse } from "../request.api-handler";
 
 export async function  PostApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<string[]>>) {
     const {data, events} = req.body;
-    console.log(data);
-    console.log(events);
+    console.log(events, events[0], events[0].args);
   
     if (!data)
       return RequestResponse(res, "BadRequest", false,  DefaultApiResponse.MissingInfo);
