e49540b5528204e1de721d8acc7618cc7f1a0495
Creador Labs <gabch15@hotmail.com>
2025-06-05 21:23:25 -0500

    Add new util function to post request on root endpoint

--- server/api-handler/v1/root.api-handler.ts
+++ server/api-handler/v1/root.api-handler.ts
@@ -2,18 +2,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { ApiResponse } from "../../../interfaces/api.interface";
 import { DefaultApiResponse } from "../../enums/api.enum";
 import { RequestResponse } from "../request.api-handler";
+import { UpdateRootAsset } from "../../../utils/web3/root/registry.util";
 
 
-export async function  PostApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<string[]>>) {
+export async function PostApiHandler(req: NextApiRequest, res: NextApiResponse<ApiResponse<string[]>>) {
     const {data} = req.body;
     const {events} = data;
-    console.log(events);
-    console.log(events[0], events[0].args);
+    const event = events[0];
+    const [,,,, collectionId, tokenId] = event.args[1].split(':');
   
     if (!data)
       return RequestResponse(res, "BadRequest", false,  DefaultApiResponse.MissingInfo);
 
-    console.log(data);
+    const updateRootAssetResult = await UpdateRootAsset(event.args[0], collectionId, tokenId);
+
+    if (!updateRootAssetResult.success) return RequestResponse(res, "ServerError", false, DefaultApiResponse.ErrorProcessingInfo);
+
+    return RequestResponse(res, "Successful", true, DefaultApiResponse.PostSuccess);
     
-    return RequestResponse(res, "ServerError", false, DefaultApiResponse.ErrorProcessingInfo);
   }
