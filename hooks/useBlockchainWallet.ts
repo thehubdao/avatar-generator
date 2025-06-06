d09d90187aeecd35dd1a7536301ce7f57d2859a6
Creador Labs <gabch15@hotmail.com>
2025-06-06 10:26:22 -0500

    Remove commented code for Root

--- hooks/useBlockchainWallet.ts
+++ hooks/useBlockchainWallet.ts
@@ -423,26 +423,6 @@ export function useBlockchainWallet() {
     }
   }, [ready, authenticated, isEthereumReady, isSolanaReady]);
 
-  /*   // Root Logic
-    useEffect(() => {
-      const connectPromise = async () => {
-        if (!isFetchingSession && userSession && signer) {
-          const eoa = userSession.linked[0].eoa;
-  
-          await InitializeContractEssentialData(signer);
-  
-          dispatch(connect({ address: eoa, walletName: null, blockchainType: Blockchain.Root, xpData: null, followUserData: { followerCount: -1, followingCount: -1 } }));
-          setLoginLibraryFlags(prev => ({ ...prev, [LoginLibrary.Pass]: true }));
-        }
-        if (!isFetchingSession && !userSession) { //We don't need the blockchain type as use effect dependency because to be connected, blockchain type must be defined
-          if (blockchainType === Blockchain.Root) dispatch(disconnect());
-          else setLoginLibraryFlags(prev => ({ ...prev, [LoginLibrary.Pass]: false }));
-          console.log(new Date().toISOString(), "disconnecting", blockchainType)
-        }
-      }
-      connectPromise();
-    }, [isFetchingSession, userSession]); */
-
   useEffect(() => {
     if (isConnected === true) {
       if (citizensMetadata === null) {
