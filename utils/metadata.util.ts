6e55c569304a7c2db07c8c5d6cc9d67eebc28d39
Creador Labs <gabch15@hotmail.com>
2025-06-06 14:33:44 -0500

    Change constant

--- utils/metadata.util.ts
+++ utils/metadata.util.ts
@@ -1,16 +1,16 @@
 import { CitizenMetadata, LuksoMetadata, SolanaMetadata } from "../interfaces/citizens.interface";
 import { PinataSDK } from "pinata-web3";
-import { tempCampaignSwitch } from "./web3/lukso/contract.util[deprecated]";
 import { Campaign } from "../enums/citizens/common.enum";
 import { Result } from "../types/common.type";
 import { CommonErrorCode, Module } from "../enums/common.enum";
 import { LogError } from "./common.util";
+import { TEMP_CAMPAIGN_SWITCH } from "../constants/lukso/contract.constant";
 
 
 const pinata = new PinataSDK({ pinataGateway: 'lukso.mypinata.cloud', pinataJwt: process.env.NEXT_PUBLIC_PINATA_JWT })
 
 export async function GetImageUrl(campaign: Campaign, combination: string): Promise<string> {
-    return `https://firebasestorage.googleapis.com/v0/b/avatar-generator-e430b.appspot.com/o/${tempCampaignSwitch[campaign]}%2Favatar_images%2F${combination}.png?alt=media&token=d6808b15-0859-4025-8397-f3137bb170cb`
+    return `https://firebasestorage.googleapis.com/v0/b/avatar-generator-e430b.appspot.com/o/${TEMP_CAMPAIGN_SWITCH[campaign]}%2Favatar_images%2F${combination}.png?alt=media&token=d6808b15-0859-4025-8397-f3137bb170cb`
 }
 
 export async function GetVrmUrl(campaign: Campaign, combination: string): Promise<string> {
