abb08d2592a7bb70949fcc25193d6a9c1d815b64
Creador Labs <gabch15@hotmail.com>
2025-06-06 14:35:18 -0500

    Add get handler

--- pages/api/v1/root/index.ts
+++ pages/api/v1/root/index.ts
@@ -1,10 +1,11 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {ApiResponse} from "../../../../interfaces/api.interface";
 import {RequestApiHandler} from "../../../../server/api-handler/request.api-handler";
-import {PostApiHandler} from "../../../../server/api-handler/v1/root.api-handler";
+import {GetApiHandler, PostApiHandler} from "../../../../server/api-handler/v1/root.api-handler";
 
-export default async function Handler(req: NextApiRequest, res: NextApiResponse<ApiResponse<string[]>>) {
+export default async function Handler(req: NextApiRequest, res: NextApiResponse<ApiResponse<string | string[]>>) {
   return RequestApiHandler(req, res, {
-    Post: PostApiHandler
+    Post: PostApiHandler,
+    Get: GetApiHandler
   })
 }
