d9cf02c9a1983d010ddf672023fb6750c118114d
nan <16200227+nan-ser0@users.noreply.github.com>
2025-06-22 13:48:15 -0500

    Add marketplace behavior on header

--- ui/citizens/common/header.ui.tsx
+++ ui/citizens/common/header.ui.tsx
@@ -7,7 +7,7 @@ import Button from "./button.ui";
 import SocialButtons from "./socialButtons.ui";
 import { Campaign, CitizensPageLocation } from "../../../enums/citizens/common.enum";
 import { GoToPage } from "../../../utils/router.util";
-import { setEditMode } from "../../../store/citizensMetadataSlice";
+import { setEditMode, setMarketplaceMode } from "../../../store/citizensMetadataSlice";
 import AboutButton from "./aboutButton.ui";
 import AboutModal from "./aboutModal.ui";
 
@@ -22,6 +22,7 @@ export default function HeaderUI({onLogin, onLogout}: HeaderUIProps) {
   const didMintingMode = useAppSelector(state => state.citizensMetadata.mintingMode);
   const didSavingMode = useAppSelector(state => state.citizensMetadata.savingMode);
   const didEditMode = useAppSelector(state => state.citizensMetadata.editMode);
+  const didMarketplaceMode = useAppSelector(state => state.citizensMetadata.marketplaceMode);
   const selectedCampaign = useAppSelector(state => state.citizensMetadata.selectedCampaign);
 
   const [isBurguerOpen, setIsBurguerOpen] = useState(false);
@@ -58,7 +59,7 @@ export default function HeaderUI({onLogin, onLogout}: HeaderUIProps) {
           )}
         </div>
       {/* NAVBAR */}
-      {isLoggedIn && !didMintingMode && !didSavingMode && !didEditMode &&
+      {isLoggedIn && !didMintingMode && !didSavingMode && !didEditMode && !didMarketplaceMode &&
         <>
           <div className={`fixed z-50 xl:relative inset-6 xl:inset-0 bg-citizens-dark xl:bg-inherit h-fit ${isBurguerOpen ? 'block rounded-3xl' : 'hidden xl:block'}`}>
             <div className='w-full px-4 pt-4 pb-24 flex justify-between xl:hidden'>
@@ -81,7 +82,8 @@ export default function HeaderUI({onLogin, onLogout}: HeaderUIProps) {
                 handleNavbarClick(CitizensPageLocation.HOME);
               }} />
               <Button label="Backpack" withIcon className="w-full xl:min-w-min h-fit px-4 !shadow-none xl:!shadow-citizens-btn" textStyles="text-[32px] xl:!text-base 2xl:!text-lg text-start xl:text-center" handleClick={() => {
-                handleNavbarClick(CitizensPageLocation.BACKPACK);
+                dispatch(setMarketplaceMode(true));
+                handleNavbarClick(CitizensPageLocation.HOME);
               }} />
               {selectedCampaign !== Campaign.Kumi && <Button label="leaderboard" withIcon className="w-full xl:min-w-min h-fit px-4 !shadow-none xl:!shadow-citizens-btn" textStyles="text-[32px] xl:!text-base 2xl:!text-lg text-start xl:text-center" handleClick={() => {
                 handleNavbarClick(CitizensPageLocation.LEADERBOARD);
