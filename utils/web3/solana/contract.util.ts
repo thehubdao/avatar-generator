a8c87274bfce9414f466c02eb7084ca55173ceb1
Creador Labs <gabch15@hotmail.com>
2025-06-23 10:04:26 -0500

    change function name

--- utils/web3/solana/contract.util.ts
+++ utils/web3/solana/contract.util.ts
@@ -20,8 +20,7 @@ import { CampaignDrops } from '../../../types/citizens.type';
 import { GetCollectionDocs } from '../../firebase.util';
 import { GetSolanaSetNewCombinationSerializedTransaction } from '../../api.util';
 
-
-export async function InitializeClientUmi(wallet: ConnectedSolanaWallet): Promise<Result<boolean>> {
+export async function InitializeUmi(wallet: ConnectedSolanaWallet): Promise<Result<boolean>> {
     try {
         UMI.use(signerIdentity({
             publicKey: publicKey(wallet.address),
