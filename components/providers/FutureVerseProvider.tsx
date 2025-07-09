32b952e13978c45eea3375586f41f5e77a3826d7
Creador Labs <gabch15@hotmail.com>
2025-07-09 13:11:33 -0500

    change to staging

--- components/providers/FutureVerseProvider.tsx
+++ components/providers/FutureVerseProvider.tsx
@@ -11,7 +11,7 @@ export default function FutureVerseProvider({ children }: { children: React.Reac
 
     const authClient = new FutureverseAuthClient({
         clientId: CLIENT_ID,
-        environment: 'production',
+        environment: 'staging',
         redirectUri: ORIGIN,
         signInFlow: 'redirect',
     });
