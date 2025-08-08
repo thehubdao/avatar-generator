7bdb2a4b0a924cd9228ebf326409e1cd536939e5
Creador Labs <gabch15@hotmail.com>
2025-08-08 14:37:44 -0500

    Refactor api routes and logic

--- pages/api/v1/drops/burnDrops.ts
+++ pages/api/v1/drops/burnDrops.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ApiResponse } from "../../../../interfaces/api.interface";
 import { RequestApiHandler } from "../../../../server/api-handler/request.api-handler";
-import {PostApiHandler} from "../../../../server/api-handler/v1/drops.api-handler";
+import { PostBurnDropsApiHandler } from "../../../../server/api-handler/v1/drops.api-handler";
 import { DropToClaim } from "../../../../interfaces/citizens.interface";
 
 export default async function Handler(req: NextApiRequest, res: NextApiResponse<ApiResponse<DropToClaim[] | boolean>>) {
-  return RequestApiHandler(req, res, {
-    Post: PostApiHandler,
-  });
+    return RequestApiHandler(req, res, {
+        Post: PostBurnDropsApiHandler,
+    });
 }
