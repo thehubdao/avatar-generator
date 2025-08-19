b022f372060b8e3572283768f818ada2b84125c5
nan <16200227+nan-ser0@users.noreply.github.com>
2025-08-18 23:12:45 -0500

    Improve text color to light variant in button

--- ui/citizens/common/button.ui.tsx
+++ ui/citizens/common/button.ui.tsx
@@ -15,7 +15,7 @@ interface ButtonProps {
 export default function Button({ label = 'Button', className, textStyles, iconStyles, light = false, withIcon = false, children, handleClick }: ButtonProps) {
   return (
     <button className={`flex justify-between items-center gap-2  ${light ? 'bg-white/75 border-2 border-white rounded-2xl px-4 py-[10px]':'min-w-[214px] bg-citizens-dark rounded-[20px] p-2 shadow-citizens-btn'} ${className}`} onClick={() => handleClick()}>
-      <p className={`whitespace-nowrap ${light ? 'text-xl':'font-light text-lg text-white text-center uppercase grow'} ${textStyles}`}>{label}</p>
+      <p className={`whitespace-nowrap ${light ? 'text-xl text-citizens-dark':'font-light text-lg text-white text-center uppercase grow'} ${textStyles}`}>{label}</p>
       {
         withIcon &&
         <div className={`${light ? '':'w-10 h-[27px] border border-white rounded-full flex justify-center items-center'} ${iconStyles}`}>
