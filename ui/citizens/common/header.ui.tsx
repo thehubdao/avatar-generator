919d7271fc5f0312003170def1520b96a9deb088
Creador Labs <gabch15@hotmail.com>
2025-09-22 11:47:06 -0500

    Remove leaderboard from polygon

--- ui/citizens/common/header.ui.tsx
+++ ui/citizens/common/header.ui.tsx
@@ -84,7 +84,7 @@ export default function HeaderUI({onLogin, onLogout}: HeaderUIProps) {
               <Button label="Collection" withIcon className="w-full xl:min-w-min h-fit px-4 !shadow-none xl:!shadow-citizens-btn" textStyles="text-[32px] xl:!text-base 2xl:!text-lg text-start xl:text-center" handleClick={() => {
                 handleNavbarClick(CitizensPageLocation.BACKPACK);
               }} />
-              {selectedCampaign !== Campaign.Kumi && selectedCampaign !== Campaign.Based && <Button label="leaderboard" withIcon className="w-full xl:min-w-min h-fit px-4 !shadow-none xl:!shadow-citizens-btn" textStyles="text-[32px] xl:!text-base 2xl:!text-lg text-start xl:text-center" handleClick={() => {
+              {selectedCampaign !== Campaign.Kumi && selectedCampaign !== Campaign.Based && selectedCampaign !== Campaign.Polygon && <Button label="leaderboard" withIcon className="w-full xl:min-w-min h-fit px-4 !shadow-none xl:!shadow-citizens-btn" textStyles="text-[32px] xl:!text-base 2xl:!text-lg text-start xl:text-center" handleClick={() => {
                 handleNavbarClick(CitizensPageLocation.LEADERBOARD);
               }} />}
               <Button label="play" withIcon className="w-full xl:min-w-min h-fit px-4 !shadow-none xl:!shadow-citizens-btn" textStyles="text-[32px] xl:!text-base 2xl:!text-lg text-start xl:text-center" handleClick={() => {
