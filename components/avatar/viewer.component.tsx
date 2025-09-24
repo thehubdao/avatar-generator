ced90d5fef5d9f5c2e4dd3425905ce9d89b8a366
nan <16200227+nan-ser0@users.noreply.github.com>
2025-09-23 21:40:41 -0500

    Improve GetCanvasImageUrl to get image from renderTarget

--- components/avatar/viewer.component.tsx
+++ components/avatar/viewer.component.tsx
@@ -9,7 +9,8 @@ import {
   Scene,
   Texture,
   Vector3,
-  WebGLRenderer
+  WebGLRenderer,
+  WebGLRenderTarget
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Delay, LogError, LogWarning } from "../../utils/common.util";
@@ -29,6 +30,7 @@ import { ConfigPostProcessing } from "../../interfaces/postProcessing.interface"
 import SceneUtil from "../../utils/scene.utils";
 
 //#region Logic
+let _canvas: HTMLCanvasElement | undefined;
 let _scene: Scene | undefined;
 let _camera: PerspectiveCamera | undefined;
 let _renderer: WebGLRenderer | undefined;
@@ -65,6 +67,67 @@ function DoAnimation(delta: number) {
     mixer.update(delta);
   }
 }
+
+function Animate() {
+    if (_scene == undefined) return void LogError(Module.Viewer, "Missing Scene for rendering!");
+    if (_renderer == undefined) return void LogError(Module.Viewer, "Missing Renderer on viewer!");
+    if (_camera == undefined) return void LogError(Module.Viewer, "Missing Camera for rendering!");
+    if (_controls == undefined) return void LogError(Module.Viewer, "Missing camera controls!");
+
+    _animReq = requestAnimationFrame(Animate);
+
+    const delta = _clock.getDelta();
+
+    DoAnimation(delta);
+
+    _controls.update();
+
+    if (_camera.position.distanceTo(_cameraPos) < 0.1) {
+      _willCameraMove = false;
+    }
+
+    if (_controls.target.distanceTo(_cameraLookAt) < 0.1) {
+      _willCameraLookAt = false;
+    }
+
+    if (_willCameraMove) _camera.position.lerp(_cameraPos, delta);
+    if (_willCameraLookAt) _controls.target.lerp(_cameraLookAt, delta);
+
+    if (_composer) {
+      _composer.render();
+    } else {
+      _renderer.render(_scene, _camera);
+    }
+  }
+
+// function ResizeCanvas(windowSize: boolean = false) {
+//   if (_canvas == undefined) return void LogError(Module.Viewer, "Missing canvas on resize!");
+//     if (_scene == undefined) return void LogError(Module.Viewer, "Missing scene on resize!");
+//     if (_renderer == undefined) return void LogError(Module.Viewer, "Missing Renderer on resize!");
+//     if (_camera == undefined) return void LogError(Module.Viewer, "Missing Camera for resize!");
+//     if (!(_tanFOV && _windowHeight))
+//       return void LogError(Module.AvatarGenerator, "Missing fov/windowHeight on resize!"); 
+
+//     console.log(windowSize ? 'Resizing to window' : 'Resizing to canvas');
+
+
+//     const width = (windowSize ? window.innerWidth : _canvas.width);
+//     const height = (windowSize ? window.innerHeight : _canvas.height);
+
+//     _camera.aspect = width / height;
+//     _camera.fov = (360 / Math.PI) * Math.atan(_tanFOV * (height / height));
+//     _camera.updateProjectionMatrix();
+
+//     if (_composer) {
+//       _composer.setSize(width, height);
+//       console.log(`Resizing composer to ${width}x${height}`);
+
+//       _composer.render();
+//     } else {
+//       _renderer.setSize(width, height);
+//       _renderer.render(_scene, _camera);
+//     }
+// }
 
 export function AddComposer(postProcessing: ConfigPostProcessing[]) {
   if (_scene == undefined) return void LogError(Module.Viewer, "Missing Scene to create composer!");
@@ -158,6 +221,11 @@ export function TakeCanvasPicture(mimeType = 'image/png') {
 
 export async function GetCanvasImageUrl(pos?: Vector3, target?: Vector3): Promise<string | null> {
   if (_renderer == undefined) {
+    void LogError(Module.Viewer, 'Missing renderer');
+    return null;
+  }
+
+  if (_scene === undefined) {
     void LogError(Module.Viewer, 'Missing scene');
     return null;
   }
@@ -174,14 +242,61 @@ export async function GetCanvasImageUrl(pos?: Vector3, target?: Vector3): Promis
     const lastTarget = new Vector3(_controls?.target.x, _controls?.target.y, _controls?.target.z);
     _camera?.position.set(pos.x, pos.y, pos.z);
     _controls?.target.set(target?.x ?? 0, target?.y ?? 1.65, target?.z ?? 0);
+
+    const imageSize = window.innerWidth < 1024 ? 512 : 1024;
+    const pixelBuffer = new Uint8Array(imageSize * imageSize * 4);
+    const renderTarget = new WebGLRenderTarget(imageSize, imageSize);
+
+    cancelAnimationFrame(_animReq);
     await Delay(100);
-    shot = _renderer.domElement.toDataURL();
+
+    if (_composer) {
+      _composer.render(); 
+      _composer.renderTarget1 = renderTarget;
+      const renderFromComposer = _composer.renderTarget1;
+      console.log(_composer.readBuffer);
+      
+      _renderer.readRenderTargetPixels(renderFromComposer, 0, 0, imageSize, imageSize, pixelBuffer);
+    } else {
+      _renderer.setRenderTarget(renderTarget);
+      _renderer.render(_scene, _camera);
+      _renderer.setRenderTarget(null);
+      _renderer.readRenderTargetPixels(renderTarget, 0, 0, imageSize, imageSize, pixelBuffer);
+    }
+
+    // Convert the image data to a base64-encoded PNG
+    const canvas = document.createElement('canvas');
+    canvas.width = imageSize;
+    canvas.height = imageSize;
+    const context = canvas.getContext('2d');
+    const imageDataObject = context!.createImageData(imageSize, imageSize);
+
+    context!.putImageData(imageDataObject, 0, 0);
+
+    const finalCanvas = document.createElement('canvas');
+    finalCanvas.width = imageSize;
+    finalCanvas.height = imageSize;
+    const finalContext = finalCanvas.getContext('2d');
+    
+    finalContext!.scale(1, -1); // Flip vertically
+    finalContext!.translate(0, -imageSize); // Move origin back
+    finalContext!.drawImage(canvas, 0, 0);
+
+    finalContext!.putImageData(imageDataObject, 0, 0);
+
+    shot = finalCanvas.toDataURL('image/png');
+
+
+    // await Delay(100);
+    // shot = _renderer.domElement.toDataURL();
     _camera.position.set(lastPos.x, lastPos.y, lastPos.z);
     _controls?.target.set(lastTarget.x, lastTarget.y, lastTarget.z);
   } else {
     shot = _renderer.domElement.toDataURL();
   }
-  return(shot)
+
+  // Animate();
+  return (shot)
 }
 
 //#endregion
@@ -208,11 +323,11 @@ export default function AvatarViewer({ onReady, defaultCamPos, defaultCamLookAt,
       .catch(err => console.error(err));
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    return () => { 
+    return () => {
       window.removeEventListener('resize', onWindowResize, false);
       cancelAnimationFrame(_animReq);
       disposeScene();
-      
+
       threeCanvas.current?.remove();
     }
   }, []);
@@ -233,6 +348,8 @@ export default function AvatarViewer({ onReady, defaultCamPos, defaultCamLookAt,
 
     // mount scene
     threeCanvas.current.appendChild(_renderer.domElement);
+
+    _canvas = _renderer.domElement;
 
     _cameraPos = _camera.position.clone();
     _cameraLookAt = _controls.target.clone();
@@ -257,8 +374,8 @@ export default function AvatarViewer({ onReady, defaultCamPos, defaultCamLookAt,
   }
 
   function disposeScene() {
-    if(_renderer === undefined) return LogWarning(Module.Viewer, "Error disposing renderer, renderer is undefined");
-    if(_scene === undefined) return LogWarning(Module.Viewer, "Error disposing scene, scene is undefined");
+    if (_renderer === undefined) return LogWarning(Module.Viewer, "Error disposing renderer, renderer is undefined");
+    if (_scene === undefined) return LogWarning(Module.Viewer, "Error disposing scene, scene is undefined");
 
     const cleanMaterial = (material: Material) => {
       material.dispose();
@@ -310,38 +427,6 @@ export default function AvatarViewer({ onReady, defaultCamPos, defaultCamLookAt,
     _renderer.setSize(window.innerWidth, window.innerHeight);
     _renderer.render(_scene, _camera);
   }
-
-  const Animate = () => {
-    if (_scene == undefined) return void LogError(Module.Viewer, "Missing Scene for rendering!");
-    if (_renderer == undefined) return void LogError(Module.Viewer, "Missing Renderer on viewer!");
-    if (_camera == undefined) return void LogError(Module.Viewer, "Missing Camera for rendering!");
-    if (_controls == undefined) return void LogError(Module.Viewer, "Missing camera controls!");
-
-    _animReq = requestAnimationFrame(Animate);
-
-    const delta = _clock.getDelta();
-
-    DoAnimation(delta);
-
-    _controls.update();
-
-    if (_camera.position.distanceTo(_cameraPos) < 0.1) {
-      _willCameraMove = false;
-    }
-
-    if (_controls.target.distanceTo(_cameraLookAt) < 0.1) {
-      _willCameraLookAt = false;
-    }
-
-    if (_willCameraMove) _camera.position.lerp(_cameraPos, delta);
-    if (_willCameraLookAt) _controls.target.lerp(_cameraLookAt, delta);
-
-    if (_composer) {
-      _composer.render();
-    } else {
-      _renderer.render(_scene, _camera);
-    }
-  };
 
   return (
     <div className={`relative w-full ${editMode ? 'xl:w-[42%] h-[calc(100vh-256px)] xl:h-full' : 'h-full'} overflow-hidden flex justify-center items-center transition-all duration-300`} ref={threeCanvas} onMouseDown={() => stopCamMovement()} />
