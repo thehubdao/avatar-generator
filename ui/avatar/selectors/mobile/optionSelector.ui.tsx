de9bacba94d9019c5eaf5e13f89db361029ec730
nan <16200227+nan-ser0@users.noreply.github.com>
2025-09-26 21:50:30 -0500

    Use Option card UI instead old option item UI & add 'no option' option in mobile option selector

--- ui/avatar/selectors/mobile/optionSelector.ui.tsx
+++ ui/avatar/selectors/mobile/optionSelector.ui.tsx
@@ -1,7 +1,9 @@
 import { FeatureInterface } from "../../../../interfaces/api.interface";
 import 'swiper/css';
 import { BasicData } from '../../../../interfaces/common.interface';
-import { OptionItemUI } from "./optionItem.ui";
+import { MouseEvent } from "react";
+import OptionCardUI from "../optionCard.ui";
+import { RandomTier } from "../../../../enums/common.enum";
 
 interface OptionSelectorUIProps {
   list: FeatureInterface[];
@@ -10,11 +12,28 @@ interface OptionSelectorUIProps {
 }
 
 export default function OptionSelectorUI({ list, activeOption, handleClick }: OptionSelectorUIProps) {
+  const selectFeature = (e: MouseEvent<HTMLDivElement>, opt: FeatureInterface) => {
+    e.preventDefault();
+    handleClick(opt.id, opt.path, opt.name);
+  }
   return <>
     <div className='w-full px-2 flex flex-wrap overflow-y-scroll gap-2 justify-center h-fit items-center'>
-      {list.map((opt, index) => (
-        <OptionItemUI key={index} opt={opt} isActive={activeOption?.val === opt.name} handleClick={(id, path, name) => handleClick(id, path, name)} />
-      ))}
+      {list.length > 0 ?
+        list.map((opt, index) => (
+          <div key={opt.id + index} onClick={event => selectFeature(event, opt)}>
+            <OptionCardUI option={opt} isActive={activeOption && activeOption.val === opt.name} />
+          </div>
+        ))
+        :
+        <OptionCardUI option={{
+          id: 'empty',
+          index: 0,
+          name: 'No options',
+          type: 'empty',
+          tier: RandomTier.Common,
+          path: '',
+        }} />
+      }
     </div>
   </>
 }
